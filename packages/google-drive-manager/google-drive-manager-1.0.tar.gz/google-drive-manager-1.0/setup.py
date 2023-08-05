9a267fa293aa35f98ff35a604f0064a8afe18f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:41 +0000

    Add package google-drive-manager/1.0/google-drive-manager-1.0.tar.gz

---
+++ packages/google-drive-manager/google-drive-manager-1.0.tar.gz/google-drive-manager-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='google-drive-manager',
+    version='1.0',
+    description='Un gestionnaire pour Google Drive',
+    packages=find_packages(),
+    install_requires=[
+        "google-api-python-client==2.64.0",
+        "google-auth-oauthlib==0.5.0"
+    ],
+    python_requires='>=3.8'
+)
