c40c181240300622b7ae7ce07e4328cc5bb57d80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:09 +0000

    Add package distributions-abcdefg/0.124/distributions_abcdefg-0.124.tar.gz

---
+++ packages/distributions-abcdefg/distributions_abcdefg-0.124.tar.gz/distributions_abcdefg-0.124/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_abcdefg',
+      version='0.124',
+      description='Gaussian distributions_abcdefg',
+      packages=['distributions_abcdefg'],
+      zip_safe=False)
