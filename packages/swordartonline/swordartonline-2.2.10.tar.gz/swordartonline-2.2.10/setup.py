7279c742b3f84950fa3b267ec90f5afdd8ca8aae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:28 +0000

    Add package swordartonline/2.2.10/swordartonline-2.2.10.tar.gz

---
+++ packages/swordartonline/swordartonline-2.2.10.tar.gz/swordartonline-2.2.10/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+import urllib.request
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="swordartonline",
+    version="2.2.10",
+    author="Orange Alice",
+    author_email="author@example.com",
+    description="Sample package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
---
+++ packages/swordartonline/swordartonline-2.2.10.tar.gz/swordartonline-2.2.10/src/swordartonline/__init__.py
@@ -0,0 +1,2 @@
+FLAG = "inno{dont_call_SAO_a_bad_anime}"
+print('https://www.youtube.com/watch?v=MO60ahwc8FI')
