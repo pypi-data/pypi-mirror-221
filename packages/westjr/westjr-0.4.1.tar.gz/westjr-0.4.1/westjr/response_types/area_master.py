b957f311b695db2187db992331b7f546fcb3aaf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:39 +0000

    Add package westjr/0.4.1/westjr-0.4.1.tar.gz

---
+++ packages/westjr/westjr-0.4.1.tar.gz/westjr-0.4.1/westjr/response_types/area_master.py
@@ -0,0 +1,55 @@
+# [/api/v3/area_{AREA}_master.json]
+from typing import Dict, List, Optional, Union
+
+from pydantic import BaseModel, Field
+
+
+class Dest(BaseModel):
+    upper: str
+    lower: str
+
+
+class Line(BaseModel):
+    name: str
+    range: str
+    relatelines: Optional[List[str]] = None
+    st: str
+    pos: str
+    index: int
+    dest: Dest
+
+
+class TrafficInfo(BaseModel):
+    url: str
+
+
+class DelayTextItemFrom(BaseModel):
+    from_: int = Field(..., alias="from")
+    display: int
+
+
+class DelayTextItemTo(BaseModel):
+    to: int
+    display: int
+
+
+class NoUpdateAlert(BaseModel):
+    currentTime: str
+    thresholdMinutes: int
+
+
+class TrainmonitorinfoLine(BaseModel):
+    info: str
+    currentTime: str
+    thresholdSeconds: int
+
+
+TrainmonitorinfoLines = Dict[str, TrainmonitorinfoLine]
+
+
+class AreaMaster(BaseModel):
+    lines: Dict[str, Line]
+    trafficInfo: TrafficInfo
+    delayText: List[Union[DelayTextItemFrom, DelayTextItemTo]]
+    noUpdateAlert: NoUpdateAlert
+    trainmonitorinfoLines: TrainmonitorinfoLines
