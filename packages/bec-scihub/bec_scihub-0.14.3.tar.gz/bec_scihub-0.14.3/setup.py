76d79128597f753ce3f3546213b2fa38675815a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:44 +0000

    Add package bec-scihub/0.14.3/bec_scihub-0.14.3.tar.gz

---
+++ packages/bec-scihub/bec_scihub-0.14.3.tar.gz/bec_scihub-0.14.3/setup.py
@@ -0,0 +1,21 @@
+import pathlib
+import subprocess
+
+from setuptools import setup
+
+current_path = pathlib.Path(__file__).parent.resolve()
+
+utils = f"{current_path}/../bec_lib/"
+
+__version__ = "0.14.3"
+
+if __name__ == "__main__":
+    setup(
+        install_requires=["msgpack", "pyyaml", "fastjsonschema", "python-dotenv"],
+        version=__version__,
+        entry_points={"console_scripts": ["bec-scihub = scihub:main"]},
+        extras_require={"dev": ["pytest", "pytest-random-order", "coverage", "black", "pylint"]},
+    )
+    local_deps = [utils]
+    for dep in local_deps:
+        subprocess.run(f"pip install -e {dep}", shell=True, check=True)
