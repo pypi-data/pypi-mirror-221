0c8405ad928ce3b77133ed31c8399b1bd67d36ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:56 +0000

    Add package arun-test/0.1.0/arun_test-0.1.0.tar.gz

---
+++ packages/arun-test/arun_test-0.1.0.tar.gz/arun_test-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='arun_test',
+    version='0.1.0',
+    packages=find_packages(),
+    author='Arun Uday',
+    description='A Python SDK for Testing sdk',
+)
