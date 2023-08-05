75dfcdfa33ed0befa73875822800d476aaab35fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:02 +0000

    Add package yargy/0.16.0/yargy-0.16.0.tar.gz

---
+++ packages/yargy/yargy-0.16.0.tar.gz/yargy-0.16.0/setup.py
@@ -0,0 +1,34 @@
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as file:
+    description = file.read()
+
+
+setup(
+    name='yargy',
+    version='0.16.0',
+
+    description='Rule-based facts extraction for Russian language',
+    long_description=description,
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/natasha/yargy',
+    author='Yargy contributors',
+    author_email='d.a.veselov@yandex.ru, alex@alexkuk.ru',
+    license='MIT',
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+    ],
+    keywords='natural language processing, russian morphology, glr, parser',
+
+    packages=find_packages(
+        exclude=['tests']
+    ),
+    install_requires=[
+        'pymorphy2'
+    ]
+)
