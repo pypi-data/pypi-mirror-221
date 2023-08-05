eb48c02b1c91da014cf37e9417f7d7dafe24bf9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:40 +0000

    Add package minet/1.0.0a45/minet-1.0.0a45-py3-none-any.whl

---
+++ packages/minet/minet-1.0.0a45-py3-none-any.whl/minet/types.py
@@ -0,0 +1,25 @@
+import sys
+from typing import Union
+from os import PathLike
+from io import FileIO
+from bs4 import BeautifulSoup
+
+# NOTE: yes this is repetitive, but mypy only understands this, don't ask questions...
+if sys.version_info >= (3, 8):
+    from typing import Literal
+else:
+    from typing_extensions import Literal
+
+if sys.version_info >= (3, 10):
+    from typing import TypeGuard, Concatenate, ParamSpec
+else:
+    from typing_extensions import TypeGuard, Concatenate, ParamSpec
+
+if sys.version_info >= (3, 11):
+    from typing import TypedDict, Required, NotRequired
+else:
+    from typing_extensions import TypedDict, Required, NotRequired
+
+AnyPath = Union[str, PathLike]
+AnyFileTarget = Union[AnyPath, FileIO]
+AnyScrapableTarget = Union[str, BeautifulSoup]
