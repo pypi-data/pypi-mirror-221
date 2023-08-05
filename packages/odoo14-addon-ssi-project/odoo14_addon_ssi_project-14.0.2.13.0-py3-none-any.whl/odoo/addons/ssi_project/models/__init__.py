6c9ea732fcc9288968394b497617561c028cb498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:38 +0000

    Add package odoo14-addon-ssi-project/14.0.2.13.0/odoo14_addon_ssi_project-14.0.2.13.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-project/odoo14_addon_ssi_project-14.0.2.13.0-py3-none-any.whl/odoo/addons/ssi_project/models/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import (
+    task_dependency,
+    task_instruction,
+    project_task,
+    project_project,
+)
