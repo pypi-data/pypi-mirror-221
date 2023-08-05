def644d75ba3f2c8130b1b22a3cb99499a6d44c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:44 +0000

    Add package bec-file-writer/0.14.3/bec_file_writer-0.14.3.tar.gz

---
+++ packages/bec-file-writer/bec_file_writer-0.14.3.tar.gz/bec_file_writer-0.14.3/setup.py
@@ -0,0 +1,21 @@
+import pathlib
+import subprocess
+
+from setuptools import setup
+
+current_path = pathlib.Path(__file__).parent.resolve()
+
+utils = f"{current_path}/../bec_lib/"
+
+__version__ = "0.14.3"
+
+if __name__ == "__main__":
+    setup(
+        install_requires=["numpy", "pyyaml", "h5py", "xmltodict"],
+        version=__version__,
+        entry_points={"console_scripts": ["bec-file-writer = file_writer:main"]},
+        extras_require={"dev": ["pytest", "pytest-random-order", "coverage", "black", "pylint"]},
+    )
+    local_deps = [utils]
+    for dep in local_deps:
+        subprocess.run(f"pip install -e {dep}", shell=True, check=True)
