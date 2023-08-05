70d7a1184ed57b5110c563093693c668ade3927a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:20 +0000

    Add package botiverse/0.3.0/botiverse-0.3.0.tar.gz

---
+++ packages/botiverse/botiverse-0.3.0.tar.gz/botiverse-0.3.0/botiverse/__init__.py
@@ -0,0 +1,2 @@
+"""Entry point for the botiverse package."""
+from botiverse.gui.gui import chat_gui
---
+++ packages/botiverse/botiverse-0.3.0.tar.gz/botiverse-0.3.0/botiverse/bots/__init__.py
@@ -0,0 +1,6 @@
+from botiverse.bots.BasicBot.BasicBot import BasicBot
+from botiverse.bots.WhizBot.WhizBot import WhizBot
+from botiverse.bots.basic_TODS.basic_TODS import BasicTODS
+from botiverse.bots.deep_TODS.deep_TODS import DeepTODS
+from botiverse.bots.VoiceBot.SpeechClassifier import SpeechClassifier
+from botiverse.bots.VoiceBot.VoiceBot import VoiceBot
