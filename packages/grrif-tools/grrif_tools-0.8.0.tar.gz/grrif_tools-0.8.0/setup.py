7b8329c3e6c13dbbd5e9c1baf65d7aae08e142a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:42 +0000

    Add package grrif-tools/0.8.0/grrif_tools-0.8.0.tar.gz

---
+++ packages/grrif-tools/grrif_tools-0.8.0.tar.gz/grrif_tools-0.8.0/setup.py
@@ -0,0 +1,28 @@
+import os
+from setuptools import setup, find_packages
+
+with open(
+    os.path.join(os.path.abspath(os.path.dirname(__file__)), "README.md"),
+    encoding="utf-8",
+) as f:
+    long_description = f.read()
+
+setup(
+    name="grrif_tools",
+    description="An unofficial set of tools for Cool Cats™.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Julien 'fetzu' Bono",
+    url="https://github.com/fetzu/grrif-tools",
+    version="0.8.0",
+    download_url="https://github.com/fetzu/grrif-tools/releases/latest",
+    packages=find_packages(include=["grrif_tools", "grrif_tools.*"]),
+    license="License :: OSI Approved :: MIT License",
+    install_requires=[
+        "Requests==2.31.0",
+        "beautifulsoup4==4.12.2",
+        "titlecase==2.4",
+        "miniaudio==1.57",
+    ],
+    entry_points={"console_scripts": ["grrif_tools=grrif_tools.cli:main"]},
+)
