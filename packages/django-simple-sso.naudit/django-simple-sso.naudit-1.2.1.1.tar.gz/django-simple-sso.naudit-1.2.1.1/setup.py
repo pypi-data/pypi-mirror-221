033e5bf408cbfd5a207f7f6aacc52335c1eff26c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:00 +0000

    Add package django-simple-sso.naudit/1.2.1.1/django-simple-sso.naudit-1.2.1.1.tar.gz

---
+++ packages/django-simple-sso.naudit/django-simple-sso.naudit-1.2.1.1.tar.gz/django-simple-sso.naudit-1.2.1.1/setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+from simple_sso import __version__
+
+
+REQUIREMENTS = [
+    'Django>=2.2',
+    'itsdangerous~=2.0',
+    'webservices.naudit[django]',
+]
+
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Framework :: Django',
+    'Framework :: Django :: 2.2',
+    'Framework :: Django :: 3.0',
+    'Framework :: Django :: 3.1',
+    'Framework :: Django :: 4.0',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries',
+]
+
+
+setup(
+    name='django-simple-sso.naudit',
+    version=__version__,
+    author='Rafael Leira',
+    author_email='rafael.leira@naudit.es',
+    url='http://github.com/naudit/django-simple-sso',
+    license='BSD-3-Clause',
+    description='Simple SSO for Django',
+    long_description=open('README.rst').read(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+    test_suite='tests.settings.run',
+)
