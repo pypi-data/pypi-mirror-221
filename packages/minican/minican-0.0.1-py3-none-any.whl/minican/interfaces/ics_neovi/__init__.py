fcae02e25134045604955bce8fe39c64b6890ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:03 +0000

    Add package minican/0.0.1/minican-0.0.1-py3-none-any.whl

---
+++ packages/minican/minican-0.0.1-py3-none-any.whl/minican/interfaces/ics_neovi/__init__.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+"""
+"""
+
+from minican.interfaces.ics_neovi.neovi_bus import NeoViBus
---
+++ packages/minican/minican-0.0.1-py3-none-any.whl/minican/interfaces/kvaser/__init__.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+"""
+"""
+
+from minican.interfaces.kvaser.canlib import *
