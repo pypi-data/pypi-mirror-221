0fcc80b70c9c4f7210274e513c9a629f8ef068a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:58 +0000

    Add package kistipy/0.0.5/kistipy-0.0.5.tar.gz

---
+++ packages/kistipy/kistipy-0.0.5.tar.gz/kistipy-0.0.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='kistipy',
+    version='0.0.5',
+    author='prosopher',
+    author_email='prosopher@gmail.com',
+    description='kistipy',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/prosopher/kistipy',
+    install_requires=['datasets', 'numpy', 'pandas'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9.12',
+)
