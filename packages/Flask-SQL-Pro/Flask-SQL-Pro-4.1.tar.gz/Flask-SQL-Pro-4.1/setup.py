89bff3a27d391fcd39175fbb09576f1ac86c6101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:22 +0000

    Add package Flask-SQL-Pro/4.1/Flask-SQL-Pro-4.1.tar.gz

---
+++ packages/Flask-SQL-Pro/Flask-SQL-Pro-4.1.tar.gz/Flask-SQL-Pro-4.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Flask-SQL-Pro',
+    version='4.1',
+    description='基于Flask-SQLAlchemy, 抽离SQL语句, 使用Jinja2语法实现动态SQL, 支持上下文事务, 支持分页',
+    long_description=open('README.rst').read(),
+    author='miaokela',
+    author_email='2972799448@qq.com',
+    maintainer='miaokela',
+    maintainer_email='2972799448@qq.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'flask_sqlalchemy',
+        'pyyaml',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+    ],
+)
