57538e1333450830de828f41919dc3c2b9ac90c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:00 +0000

    Add package autopack-tools/0.3.2/autopack_tools-0.3.2.tar.gz

---
+++ packages/autopack-tools/autopack_tools-0.3.2.tar.gz/autopack_tools-0.3.2/autopack/selection.py
@@ -0,0 +1,88 @@
+import re
+from typing import Callable, Union, Optional
+
+from langchain.chat_models.base import BaseChatModel
+
+from autopack.api import pack_search
+from autopack.get_pack import try_get_pack
+from autopack.installation import install_pack
+from autopack.prompts import GET_MORE_TOOLS_TEMPLATE, TOOL_SELECTION_TEMPLATE
+from autopack.utils import functions_bulleted_list, call_llm
+
+
+def select_packs_prompt(task_description: str, function_request: Optional[str] = None, installed_only=False) -> str:
+    """
+    Generate a prompt for the pack selection process based on the task description and an optional function request.
+
+    Args:
+        task_description (str): A description of the task to be used when selecting tools.
+        function_request (Optional[str]): A specific type of function asked for (e.g. a `get_more_tools` function).
+        installed_only (Optional[bool]): If True will only use already-installed pack. If False will install the packs.
+
+    Returns:
+        str: A prompt that can be fed to the LLM for pack selection.
+    """
+
+    pack_ids = [pack.pack_id for pack in pack_search("")]
+    if installed_only:
+        fetched_packs = [try_get_pack(pack_id) for pack_id in pack_ids]
+        installed_packs = [pack for pack in fetched_packs if pack is not None]
+    else:
+        fetched_packs = [install_pack(pack_id, force_dependencies=True) for pack_id in pack_ids]
+        installed_packs = [pack for pack in fetched_packs if pack is not None]
+
+    if function_request:
+        return TOOL_SELECTION_TEMPLATE.format(task=task_description, functions=functions_bulleted_list(installed_packs))
+
+    return GET_MORE_TOOLS_TEMPLATE.format(
+        task=task_description,
+        functions=functions_bulleted_list(installed_packs),
+        functions_request=function_request,
+    )
+
+
+def select_packs(
+    task_description: str,
+    llm: Union[BaseChatModel, Callable],
+    function_request: Optional[str] = None,
+    installed_only=False,
+) -> list[str]:
+    """Given a user input describing the task they wish to accomplish, return a list of Pack IDs that the given LLM
+    thinks will be suitable for this task.
+
+    This is good for a "pre-processing" step after receiving the task but before trying to solve it. This allows you
+    to benefit from the wide tool selection while keeping your token usage low.
+
+    You can then further filter, install the packs if desired, and then fetch them using get_pack().
+
+    # TODO: Include user-provided packs into selection
+
+    Args:
+        task_description (str): A description of the task to be used when selecting tools
+        llm (BaseChatModel): An LLM which will be used to evaluate the selection
+        function_request (Optional[str]): A specific type of function asked for (e.g. a `get_more_tools` function)
+        installed_only (Optional[bool]): If True will only use already-installed pack. If False will install the packs.
+
+    Returns:
+        list[str]: A list of selected Pack IDs
+    """
+    prompt = select_packs_prompt(task_description, function_request, installed_only=installed_only)
+
+    response = call_llm(prompt, llm)
+
+    return parse_selection_response(response)
+
+
+def parse_selection_response(response: str) -> list[str]:
+    """
+    Parse the response from the LLM and extract pack IDs.
+
+    The response is split by commas and newlines, and any arguments provided in the response are removed.
+
+    Args:
+        response (str): The response from the LLM.
+
+    Returns:
+        list[str]: A list of parsed pack IDs.
+    """
+    return [r.split("(")[0].strip() for r in re.split(r"(?<=\w),|\n", response)]
