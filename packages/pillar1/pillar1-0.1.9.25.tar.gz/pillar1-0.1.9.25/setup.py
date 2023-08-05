3de4c39993c11c558409b586a4be961223a62374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:05 +0000

    Add package pillar1/0.1.9.25/pillar1-0.1.9.25.tar.gz

---
+++ packages/pillar1/pillar1-0.1.9.25.tar.gz/pillar1-0.1.9.25/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pillar1',
+    version='0.1.9.25',
+    packages=find_packages(),
+    description='Official package for Pillar1 company',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Ayman Hajja',
+    author_email='amhajja@gmail.com',
+    url='https://github.com/amhajja/pillar1',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.9',
+    install_requires=[
+        'ipywidgets',
+        'google-cloud-aiplatform',
+        'boto3',
+        'pyspark'
+    ]
+)
