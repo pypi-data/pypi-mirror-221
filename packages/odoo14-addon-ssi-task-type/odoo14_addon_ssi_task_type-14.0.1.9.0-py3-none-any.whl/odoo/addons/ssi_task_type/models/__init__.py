f519b6c78c2b2dc37ff909be93618c8305263509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:40 +0000

    Add package odoo14-addon-ssi-task-type/14.0.1.9.0/odoo14_addon_ssi_task_type-14.0.1.9.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-task-type/odoo14_addon_ssi_task_type-14.0.1.9.0-py3-none-any.whl/odoo/addons/ssi_task_type/models/__init__.py
@@ -0,0 +1,11 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import (
+    task_type,
+    project_task,
+    task_type_category,
+    task_type_instruction,
+    task_type_sucessor,
+)
