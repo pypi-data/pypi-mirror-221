7abea792da8ad8376cce4b80777ac88143b65c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:05 +0000

    Add package marketwatchdata/0.0.13/marketwatchdata-0.0.13-py3-none-any.whl

---
+++ packages/marketwatchdata/marketwatchdata-0.0.13-py3-none-any.whl/tests/test_func.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+Date: 2023/7/21 18:16
+Desc: To test intention, just write test code here!
+"""
+import sys
+
+sys.path.insert(0, "../marketwatchdata")
+#print(sys.path)
+
+# from marketwatchdata.MarketWatch import *
+import marketwatchdata.MarketWatch as mw
+
+
+def test_ohlc():
+    """
+    just for test aim
+    :return: assert result
+    :rtype: assert
+    """
+    df = mw.ohlc('TMUBMUSD03M', 'P1D', 'P1Y')
+    print(df)
+    assert df.shape[0] > 0
+
+
+if __name__ == "__main__":
+    test_ohlc()
