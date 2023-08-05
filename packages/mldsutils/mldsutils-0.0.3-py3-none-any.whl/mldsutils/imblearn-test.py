dd0f0c10db8b416fe85b165c0cec44732647976e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:23 +0000

    Add package mldsutils/0.0.3/mldsutils-0.0.3-py3-none-any.whl

---
+++ packages/mldsutils/mldsutils-0.0.3-py3-none-any.whl/mldsutils/imblearn-test.py
@@ -0,0 +1,37 @@
+from pipeline import Pipeline
+from sklearn import pipeline as pp
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.datasets import make_regression
+from sklearn.model_selection import cross_val_score
+from preprocessing import QresPlsOutlierElim, TsqPlsOutlierElim
+from mldsutils.metrics import outlier_rmse_scorer, outlier_r2_scorer
+
+X, y = make_regression(n_samples=100, n_features=50, n_informative=40, random_state=42)
+
+pipelinezz = Pipeline([
+    ("sc", StandardScaler()),
+    ("qres", QresPlsOutlierElim()),
+    ("tsq", TsqPlsOutlierElim()),
+    ("lr", LinearRegression())
+    ]
+)
+
+# pipe = pipeline.make_pipeline(StandardScaler(), LinearRegression())
+# pipe = pp.make_pipeline(StandardScaler(), LinearRegression())
+
+cv_results = cross_val_score(pipelinezz, X, y, scoring=outlier_r2_scorer, cv=3)
+print(cv_results)
+
+# pipelinezz.fit(X, y)
+# y_pred = pipelinezz.predict(X)
+# print(y_pred)
+# print(pipelinezz.indices_retained)
+
+# qres = QresPlsOutlierElim(conf_lev=0)
+# qres.fit_resample(X, y)
+# X = qres.resample(X)
+# for i in X:
+#     print(i)
+
+# print(cv_results)
