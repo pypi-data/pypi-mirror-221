4c0500d17aab3c0b19c9e9aa4fed04abe4fad115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:38 +0000

    Add package labvirtual/0.3.21/labvirtual-0.3.21-py3-none-any.whl

---
+++ packages/labvirtual/labvirtual-0.3.21-py3-none-any.whl/labvirtual/simulador_maglev/__init__.py
@@ -0,0 +1,4 @@
+from .compensador_maglev import Compensador  # noqa: F401
+from .modelo_maglev import Maglev  # noqa: F401
+from .animacao_maglev import Simulacao  # noqa: F401
+from .grafico_maglev import Grafico  # noqa: F401
