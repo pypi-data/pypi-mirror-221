a8402e0c040a8d28d9e9d1e65e5a76188c4bd90e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:21 +0000

    Add package odoo14-addon-ssi-product-sale/14.0.1.1.2/odoo14_addon_ssi_product_sale-14.0.1.1.2-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-product-sale/odoo14_addon_ssi_product_sale-14.0.1.1.2-py3-none-any.whl/odoo/addons/ssi_product_sale/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Product App - sale Integration",
+    "version": "14.0.1.1.2",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": False,
+    "auto_install": True,
+    "depends": [
+        "ssi_product",
+        "sale",
+    ],
+    "data": [
+        "menu.xml",
+    ],
+    "demo": [],
+}
