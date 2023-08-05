420e2ae94463df5702fbf7a36a1a1bf9dc748321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:49 +0000

    Add package sum-arun-test/0.1.2/sum_arun_test-0.1.2.tar.gz

---
+++ packages/sum-arun-test/sum_arun_test-0.1.2.tar.gz/sum_arun_test-0.1.2/test_sdk/__init__.py
@@ -0,0 +1,4 @@
+from test_sdk.sum_arun_test import SumData
+from test_sdk.test_functions import TestFunctions
+
+__all__ = ['SumData', 'TestFunctions']
---
+++ packages/sum-arun-test/sum_arun_test-0.1.2.tar.gz/sum_arun_test-0.1.2/test_sdk/sum_arun_test.py
@@ -0,0 +1,4 @@
+class SumData:
+    @staticmethod
+    def sum(num1, num2):
+        return num1 + num2
