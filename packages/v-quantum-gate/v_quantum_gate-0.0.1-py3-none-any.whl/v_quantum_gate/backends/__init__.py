f6e617a6f9ac0b00afaae1730c4f8b08fb87d8b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:15 +0000

    Add package v-quantum-gate/0.0.1/v_quantum_gate-0.0.1-py3-none-any.whl

---
+++ packages/v-quantum-gate/v_quantum_gate-0.0.1-py3-none-any.whl/v_quantum_gate/backends/__init__.py
@@ -0,0 +1,26 @@
+from .numpy_backend import NumPyBackend
+from .numba_backend import numba_backend_lazy
+from .qasm_output_backend import QasmOutputBackend
+from .ibmq_backend import ibmq_backend
+from .sympy_backend import SympyBackend
+from .onequbitgate_transpiler import OneQubitGateCompactionTranspiler
+from .twoqubitgate_transpiler import TwoQubitGateDecomposingTranspiler
+from .draw_backend import DrawCircuit
+from .quimb import Quimb
+from .cusv import cuSV
+from .cutn import cuTN
+
+BACKENDS = {
+    "numpy": NumPyBackend,
+    "numba": numba_backend_lazy,
+    "qasm_output": QasmOutputBackend,
+    "ibmq": ibmq_backend,
+    "sympy_unitary": SympyBackend,
+    "2q_decomposition": TwoQubitGateDecomposingTranspiler,
+    "1q_compaction": OneQubitGateCompactionTranspiler,
+    "draw": DrawCircuit,
+    "quimb": Quimb,
+    "cusv": cuSV,
+    "cutn": cuTN,
+}
+DEFAULT_BACKEND_NAME = "quimb"
---
+++ packages/v-quantum-gate/v_quantum_gate-0.0.1-py3-none-any.whl/v_quantum_gate/typing.py
@@ -0,0 +1,21 @@
+"""This module provides for type hinting."""
+
+import typing
+from typing import Generic, TypeVar, Union
+
+if typing.TYPE_CHECKING:
+    from v_quantum_gate import Circuit
+
+T = TypeVar('T')
+C = TypeVar('C')
+Targets = Union[int, slice, tuple]
+
+class GeneralCircuitOperation(Generic[C, T]):
+    """Type definition of dynamic method."""
+    def __call__(self, *args) -> T:
+        ...
+
+    def __getitem__(self, targets: Targets) -> C:
+        ...
+
+CircuitOperation = GeneralCircuitOperation['Circuit', T]
