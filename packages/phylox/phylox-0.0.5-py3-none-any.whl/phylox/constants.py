538178a1f7788de55e34add32a17e38a5b7e2e75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:03 +0000

    Add package phylox/0.0.5/phylox-0.0.5-py3-none-any.whl

---
+++ packages/phylox/phylox-0.0.5-py3-none-any.whl/phylox/constants.py
@@ -0,0 +1,8 @@
+"""
+This module contains some constants used throughout the package.
+"""
+
+#: The node attribute used to store the label of a node.
+LABEL_ATTR = "label"
+#: The edge attribute used to store the length of an edge.
+LENGTH_ATTR = "length"
