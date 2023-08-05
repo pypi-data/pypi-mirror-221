304fc2530491cc4dabda18940afbe4b3816fb34c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:10 +0000

    Add package AZMusicAPI/0.9.2/AZMusicAPI-0.9.2.tar.gz

---
+++ packages/AZMusicAPI/AZMusicAPI-0.9.2.tar.gz/AZMusicAPI-0.9.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='AZMusicAPI',
+    version='0.9.2',
+    author='AZ Studio',
+    description='Easy access to music information and song audio links',
+    long_description = open('readme.md', encoding='utf-8').read(),
+    packages=['MusicAPI'],
+    install_requires=[],
+)
