c58d83d06b41d53b8e5a2c9ca3b35d7145dfac45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:59 +0000

    Add package zhei-init/0.0.6/zhei_init-0.0.6.tar.gz

---
+++ packages/zhei-init/zhei_init-0.0.6.tar.gz/zhei_init-0.0.6/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='zhei_init',
+    version='0.0.6',
+    author='deng1fan',
+    author_email='dengyifan@iie.ac.cn',
+    url='https://github.com/deng1fan',
+    description=u'Init zhei environment.',
+    long_description=open("README.md", "r", encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        'bs4',
+        'requests',
+    ],
+    exclude=["*.tests", "*.tests.*", "tests"],
+    include_package_data=True,
+    python_requires='>=3.6',
+    entry_points = {
+        'console_scripts' : [
+            # 这一行是安装到命令行运行的关键
+            'init = zhei_init.env:init'
+        ]
+    }
+)
