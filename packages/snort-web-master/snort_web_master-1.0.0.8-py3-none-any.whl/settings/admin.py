c5a80536a733185d69520d0098637f118c9a6a45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:19 +0000

    Add package snort-web-master/1.0.0.8/snort_web_master-1.0.0.8-py3-none-any.whl

---
+++ packages/snort-web-master/snort_web_master-1.0.0.8-py3-none-any.whl/settings/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from settings.models import Setting
+from settings.models import attackGroup
+from settings.models import keyword
+# Register your models here.
+
+@admin.register(Setting)
+class SettingAdmin(admin.ModelAdmin):
+    list_display = ['name', 'value']
+
+@admin.register(attackGroup)
+class attack_groupAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+
+
+@admin.register(keyword)
+class keywords(admin.ModelAdmin):
+    list_display = ['name', 'stage', "options", "description", "available"]
