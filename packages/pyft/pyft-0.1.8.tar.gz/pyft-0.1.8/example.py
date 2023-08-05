2c98f67c3878e624ad433e9fdf7f6e48a806e1d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:20 +0000

    Add package pyft/0.1.8/pyft-0.1.8.tar.gz

---
+++ packages/pyft/pyft-0.1.8.tar.gz/pyft-0.1.8/example.py
@@ -0,0 +1,22 @@
+import pyft
+import tqdm
+
+bam_f = "../tmp.bam"
+fiberbam = pyft.Fiberbam(bam_f)
+for fiber in tqdm.tqdm(fiberbam.fetch("chr20", 0, 10_000_000)): 
+    # the number of ccs passes
+    fiber.ec
+    # the mps start positions
+    fiber.msp.starts
+    # print the nuc reference starts
+    fiber.nuc.reference_starts
+    # lift query (fiber) positions to reference positions
+    fiber.lift_query_positions([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    # lift reference positions to query (fiber) positions
+    fiber.lift_reference_positions([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    # aligned blocks between query (fiber) and reference 
+    fiber.get_aligned_blocks_as_ranges()
+
+for fiber in fiberbam.center("chr20", start=20_000_000, end=20_000_001, strand="-"):
+    # returns the same fiber object as above; however, all the positions have been modified to be relative to the region be fetched
+    print(fiber.msp.starts)
