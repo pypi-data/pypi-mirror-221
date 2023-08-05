d6e161d67652564e168487fe24d52059c808fbb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:49 +0000

    Add package autometrics/0.8/autometrics-0.8-py3-none-any.whl

---
+++ packages/autometrics/autometrics-0.8-py3-none-any.whl/autometrics/tracker/test_tracker.py
@@ -0,0 +1,89 @@
+from prometheus_client.exposition import generate_latest
+import pytest
+
+from .opentelemetry import OpenTelemetryTracker
+from .prometheus import PrometheusTracker
+
+from .tracker import default_tracker, init_tracker, TrackerType
+
+
+def test_default_tracker(monkeypatch):
+    """Test the default tracker type."""
+
+    monkeypatch.delenv("AUTOMETRICS_TRACKER", raising=False)
+    tracker = default_tracker()
+    assert isinstance(tracker, OpenTelemetryTracker)
+
+    monkeypatch.setenv("AUTOMETRICS_TRACKER", "prometheus")
+    tracker = default_tracker()
+    assert isinstance(tracker, PrometheusTracker)
+    monkeypatch.setenv("AUTOMETRICS_TRACKER", "PROMETHEUS")
+    tracker = default_tracker()
+    assert isinstance(tracker, PrometheusTracker)
+
+    # Should use open telemetry when the tracker is not recognized
+    monkeypatch.setenv("AUTOMETRICS_TRACKER", "something_else")
+    tracker = default_tracker()
+    assert isinstance(tracker, OpenTelemetryTracker)
+
+
+def test_init_prometheus_tracker_set_build_info(monkeypatch):
+    """Test that init_tracker (for a Prometheus tracker) calls set_build_info using env vars."""
+
+    commit = "d6abce3"
+    version = "1.0.1"
+    branch = "main"
+
+    monkeypatch.setenv("AUTOMETRICS_COMMIT", commit)
+    monkeypatch.setenv("AUTOMETRICS_VERSION", version)
+    monkeypatch.setenv("AUTOMETRICS_BRANCH", branch)
+
+    prom_tracker = init_tracker(TrackerType.PROMETHEUS)
+    assert isinstance(prom_tracker, PrometheusTracker)
+
+    blob = generate_latest()
+    assert blob is not None
+    data = blob.decode("utf-8")
+
+    prom_build_info = (
+        f"""build_info{{branch="{branch}",commit="{commit}",version="{version}"}} 1.0"""
+    )
+    assert prom_build_info in data
+
+    monkeypatch.delenv("AUTOMETRICS_VERSION", raising=False)
+    monkeypatch.delenv("AUTOMETRICS_COMMIT", raising=False)
+    monkeypatch.delenv("AUTOMETRICS_BRANCH", raising=False)
+
+
+def test_init_otel_tracker_set_build_info(monkeypatch):
+    """
+    Test that init_tracker (for an OTEL tracker) calls set_build_info using env vars.
+    Note that the OTEL collector translates metrics to Prometheus.
+    """
+    pytest.skip(
+        "Skipping test because OTEL collector does not create a gauge when it translates UpDownCounter to Prometheus"
+    )
+
+    commit = "a29a178"
+    version = "0.0.1"
+    branch = "main"
+
+    monkeypatch.setenv("AUTOMETRICS_COMMIT", commit)
+    monkeypatch.setenv("AUTOMETRICS_VERSION", version)
+    monkeypatch.setenv("AUTOMETRICS_BRANCH", branch)
+
+    otel_tracker = init_tracker(TrackerType.OPENTELEMETRY)
+    assert isinstance(otel_tracker, OpenTelemetryTracker)
+
+    blob = generate_latest()
+    assert blob is not None
+    data = blob.decode("utf-8")
+
+    prom_build_info = (
+        f"""build_info{{branch="{branch}",commit="{commit}",version="{version}"}} 1.0"""
+    )
+    assert prom_build_info in data
+
+    monkeypatch.delenv("AUTOMETRICS_VERSION", raising=False)
+    monkeypatch.delenv("AUTOMETRICS_COMMIT", raising=False)
+    monkeypatch.delenv("AUTOMETRICS_BRANCH", raising=False)
---
+++ packages/autometrics/autometrics-0.8-py3-none-any.whl/autometrics/tracker/tracker.py
@@ -0,0 +1,105 @@
+from typing import Protocol, Optional
+from enum import Enum
+import os
+
+
+from ..objectives import Objective
+
+
+class Result(Enum):
+    """Result of the function call."""
+
+    OK = "ok"
+    ERROR = "error"
+
+
+class TrackMetrics(Protocol):
+    """Protocol for tracking metrics."""
+
+    def set_build_info(self, commit: str, version: str, branch: str):
+        """Observe the build info. Should only be called once per tracker instance"""
+
+    def start(
+        self, function: str, module: str, track_concurrency: Optional[bool] = False
+    ):
+        """Start tracking metrics for a function call."""
+
+    def finish(
+        self,
+        start_time: float,
+        function: str,
+        module: str,
+        caller: str,
+        result: Result = Result.OK,
+        objective: Optional[Objective] = None,
+        track_concurrency: Optional[bool] = False,
+    ):
+        """Finish tracking metrics for a function call."""
+
+    def initialize_counters(
+        self,
+        function: str,
+        module: str,
+        objective: Optional[Objective] = None,
+    ):
+        """Initialize (counter) metrics for a function at zero."""
+
+
+class TrackerType(Enum):
+    """Type of tracker."""
+
+    OPENTELEMETRY = "opentelemetry"
+    PROMETHEUS = "prometheus"
+
+
+def init_tracker(tracker_type: TrackerType) -> TrackMetrics:
+    """Create a tracker"""
+
+    tracker_instance: TrackMetrics
+    if tracker_type == TrackerType.OPENTELEMETRY:
+        # pylint: disable=import-outside-toplevel
+        from .opentelemetry import OpenTelemetryTracker
+
+        tracker_instance = OpenTelemetryTracker()
+    elif tracker_type == TrackerType.PROMETHEUS:
+        # pylint: disable=import-outside-toplevel
+        from .prometheus import PrometheusTracker
+
+        tracker_instance = PrometheusTracker()
+
+    # NOTE - Only set the build info when the tracker is initialized
+    tracker_instance.set_build_info(
+        commit=os.getenv("AUTOMETRICS_COMMIT") or os.getenv("COMMIT_SHA") or "",
+        version=os.getenv("AUTOMETRICS_VERSION") or "",
+        branch=os.getenv("AUTOMETRICS_BRANCH") or os.getenv("BRANCH_NAME") or "",
+    )
+
+    return tracker_instance
+
+
+def get_tracker_type() -> TrackerType:
+    """Get the tracker type."""
+    tracker_type = os.getenv("AUTOMETRICS_TRACKER") or "opentelemetry"
+    if tracker_type.lower() == "prometheus":
+        return TrackerType.PROMETHEUS
+    return TrackerType.OPENTELEMETRY
+
+
+def default_tracker():
+    """Setup the default tracker."""
+    preferred_tracker = get_tracker_type()
+    return init_tracker(preferred_tracker)
+
+
+tracker: TrackMetrics = default_tracker()
+
+
+def get_tracker() -> TrackMetrics:
+    """Get the tracker type."""
+    return tracker
+
+
+def set_tracker(tracker_type: TrackerType):
+    """Set the tracker type."""
+    global tracker
+    tracker = init_tracker(tracker_type)
