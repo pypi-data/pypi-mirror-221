7d3dc5e4be731b3a707da6cfc3cbdad0e1676d23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:28 +0000

    Add package arclet-alconna/1.7.14/arclet_alconna-1.7.14-py3-none-any.whl

---
+++ packages/arclet-alconna/arclet_alconna-1.7.14-py3-none-any.whl/arclet/alconna/__init__.py
@@ -0,0 +1,28 @@
+"""Alconna 概览"""
+
+from nepattern import AllParam as AllParam, AnyOne as AnyOne  # noqa
+from tarina import Empty as Empty # noqa
+from .config import config, namespace, Namespace
+from .typing import MultiVar, KeyWordVar, Kw, Nargs, CommandMeta
+from .args import Args, Field, ArgFlag, Arg
+from .base import Option, Subcommand
+from .completion import CompSession
+from .exceptions import ParamsUnmatched, NullMessage, InvalidParam
+from .argv import Argv, set_default_argv_type, argv_config
+from .core import Alconna
+from .arparma import Arparma, ArparmaBehavior
+from .manager import command_manager, ShortcutArgs
+
+from .action import store_value, store_true, store_false, append, count, append_value
+from .builtin import set_default
+from .model import OptionResult, SubcommandResult, HeadResult
+from .output import output_manager
+from .formatter import TextFormatter
+from .duplication import Duplication
+from .stub import ArgsStub, OptionStub, SubcommandStub
+
+__version__ = "1.7.14"
+
+# backward compatibility
+Arpamar = Arparma
+DataCollectionContainer = Argv
