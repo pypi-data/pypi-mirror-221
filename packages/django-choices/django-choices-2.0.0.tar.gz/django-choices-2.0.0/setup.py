d07a1eaa0661f3b5709371ff5952dd2627c35f82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:56 +0000

    Add package django-choices/2.0.0/django-choices-2.0.0.tar.gz

---
+++ packages/django-choices/django-choices-2.0.0.tar.gz/django-choices-2.0.0/setup.py
@@ -0,0 +1,35 @@
+from os import path
+
+from setuptools import find_packages, setup
+
+with open(path.join(path.dirname(__file__), "README.rst")) as f:
+    readme = f.read()
+
+setup(
+    name="django-choices",
+    version="2.0.0",
+    license="MIT",
+    description="Sanity for the django choices functionality.",
+    long_description=readme,
+    install_requires=["Django>=3.2"],
+    url="https://github.com/bigjason/django-choices",
+    author="Jason Webb",
+    author_email="bigjasonwebb@gmail.com,sergeimaertens@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Framework :: Django",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.1",
+        "Framework :: Django :: 4.2",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: PyPy",
+    ],
+)
