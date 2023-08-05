925e977e4a36e403c166cb7ff183076414e0f002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:44 +0000

    Add package pyvisonicalarm/0.1.0b5/pyvisonicalarm-0.1.0b5.tar.gz

---
+++ packages/pyvisonicalarm/pyvisonicalarm-0.1.0b5.tar.gz/pyvisonicalarm-0.1.0b5/pyvisonicalarm/device_definitions.py
@@ -0,0 +1,29 @@
+"""Device type definitions"""
+
+from .devices import (
+    CameraDevice,
+    ContactDevice,
+    GSMDevice,
+    GenericDevice,
+    KeyFobDevice,
+    MotionDevice,
+    PGMDevice,
+    SmokeDevice,
+)
+
+
+DEVICE_TYPES = {
+    "GSM": GSMDevice,
+    "PGM": PGMDevice,
+}
+
+DEVICE_SUBTYPES = {
+    "CONTACT": ContactDevice,
+    "MC303_VANISH": ContactDevice,
+    "MOTION_CAMERA": CameraDevice,
+    "SMOKE": SmokeDevice,
+    "BASIC_KEYFOB": KeyFobDevice,
+    "KEYFOB_ARM_LED": KeyFobDevice,
+    "FLAT_PIR_SMART": MotionDevice,
+    "WL_SIREN": GenericDevice,
+}
