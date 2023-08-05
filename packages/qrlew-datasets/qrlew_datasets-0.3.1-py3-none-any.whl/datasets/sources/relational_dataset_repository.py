1d80e3c09c795f257a52925cb495bae09cc90aba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:05:57 +0000

    Add package qrlew-datasets/0.3.1/qrlew_datasets-0.3.1-py3-none-any.whl

---
+++ packages/qrlew-datasets/qrlew_datasets-0.3.1-py3-none-any.whl/datasets/sources/relational_dataset_repository.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Engine, create_engine
+from datasets.database import Database
+
+class RelationalDatasetRepository(Database):
+    def __init__(self, name: str) -> None:
+        super().__init__()
+        self._schema = name
+
+    def engine(self) -> Engine:
+        return create_engine(f'mysql+pymysql://guest:relational@relational.fit.cvut.cz:3306/{self.schema()}')
+    
+    def url(self) -> str:
+        return f'mysql://guest:relational@relational.fit.cvut.cz:3306/{self.schema()}'
+    
+    def schema(self) -> str:
+        return self._schema
