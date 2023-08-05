00abf4c7b0e5089685284022a46cede250fb2f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:01:31 +0000

    Add package mubind/0.1.1/mubind-0.1.1-py3-none-any.whl

---
+++ packages/mubind/mubind-0.1.1-py3-none-any.whl/mubind/models/__init__.py
@@ -0,0 +1,1 @@
+from mubind.models.models import DinucMulti, Mubind, BMPrediction, ProteinDNABinding, Decoder, BMCollection, BindingModesSimple
---
+++ packages/mubind/mubind-0.1.1-py3-none-any.whl/mubind/pl/__init__.py
@@ -0,0 +1,15 @@
+from mubind.pl.plotting import (
+    logo,
+    logo_mono,
+    logo_di,
+    create_heatmap,
+    create_logo,
+    kmer_enrichment,
+    activities,
+    loss,
+    scatter,
+    alignment_protein,
+    R2_per_protein,
+    R2_calculation,
+    set_rcParams,
+)
