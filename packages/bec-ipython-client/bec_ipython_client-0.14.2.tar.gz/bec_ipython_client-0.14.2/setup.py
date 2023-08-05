e0011964cd132a38e78c171e93e3fca2472c97fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:07 +0000

    Add package bec-ipython-client/0.14.2/bec_ipython_client-0.14.2.tar.gz

---
+++ packages/bec-ipython-client/bec_ipython_client-0.14.2.tar.gz/bec_ipython_client-0.14.2/setup.py
@@ -0,0 +1,40 @@
+import pathlib
+import subprocess
+
+from setuptools import setup
+
+current_path = pathlib.Path(__file__).parent.resolve()
+
+bec_lib = f"{current_path}/../bec_lib/"
+
+
+__version__ = "0.14.2"
+
+
+if __name__ == "__main__":
+    setup(
+        install_requires=[
+            "numpy",
+            "requests",
+            "typeguard<3.0",
+            "ipython",
+            "rich",
+            "pyepics",
+            "h5py",
+        ],
+        scripts=["bec_client/bin/bec"],
+        version=__version__,
+        extras_require={
+            "dev": [
+                "pytest",
+                "pytest-random-order",
+                "pytest-asyncio",
+                "coverage",
+                "black",
+                "pylint",
+            ]
+        },
+    )
+    local_deps = [bec_lib]
+    for dep in local_deps:
+        subprocess.run(f"pip install -e {dep}", shell=True, check=True)
