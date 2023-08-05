727c1b02220e7537ee3e32f97a80d2495ecb0786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:59 +0000

    Add package middleware-apm/0.2.0/middleware-apm-0.2.0.tar.gz

---
+++ packages/middleware-apm/middleware-apm-0.2.0.tar.gz/middleware-apm-0.2.0/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+try:
+    with open("requirements.txt", "r") as f:
+        requirements = f.read().splitlines()
+except IOError:
+    requirements = []
+
+
+setuptools.setup(
+
+    name="middleware-apm",
+    version="0.2.0",
+    install_requires=requirements,
+    author="middleware-dev",
+    description="This package is use to check the RAM and CPU Usage of Current Device.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/plain',
+    packages=["mw_tracker"],
+    url = "https://github.com/middleware-labs/agent-apm-python.git",
+    entry_points={
+        'console_scripts': [
+            'middleware-instrument = opentelemetry.instrumentation.auto_instrumentation:run',
+            'middleware-bootstrap = opentelemetry.instrumentation.bootstrap:run',
+        ],
+    },
+)
