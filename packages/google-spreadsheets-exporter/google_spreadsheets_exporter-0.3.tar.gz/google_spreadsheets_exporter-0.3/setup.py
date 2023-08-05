d87a15d3e9265705f412b1590d27fa7458bfe689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:37 +0000

    Add package google-spreadsheets-exporter/0.3/google_spreadsheets_exporter-0.3.tar.gz

---
+++ packages/google-spreadsheets-exporter/google_spreadsheets_exporter-0.3.tar.gz/google_spreadsheets_exporter-0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='google_spreadsheets_exporter',
+    version='0.3',
+    packages=['google_spreadsheets_exporter'],
+    install_requires=[
+        'google-api-python-client',
+        'google-auth-httplib2',
+        'google-auth-oauthlib',
+    ],
+    exclude_package_data={'google_spreadsheets_exporter': ['creds.json', 'test.py']}
+)
