cb12e7295b68fe73b49ac37552c2b528f8eb7de1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:47 +0000

    Add package urlincode/0.3.0/urlincode-0.3.0.tar.gz

---
+++ packages/urlincode/urlincode-0.3.0.tar.gz/urlincode-0.3.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='urlincode',
+    version='0.3.0',
+    packages=find_packages(include=['urlincode']),
+    url='https://www.wikipedia.com',
+    project_urls={
+        'Documentation': 'https://www.apple.com',
+        'Source Code': 'https://www.facebook.com',
+        'Bug Tracker': 'https://www.9gag.com'
+    }
+)
+
