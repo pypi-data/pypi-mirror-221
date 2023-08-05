ab8ecfa461d5df9825384750e9d259ad6153fcb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:52 +0000

    Add package dvas/1.0.0/dvas-1.0.0.tar.gz

---
+++ packages/dvas/dvas-1.0.0.tar.gz/dvas-1.0.0/README.md
@@ -0,0 +1,11 @@
+
+# dvas: Data Visualization and Analysis Software
+
+The compiled code documentation [lives here](https://meteoswiss.github.io/dvas).
+
+The contributing guidelines are [here](CONTRIBUTING.md).
+
+Although the code documentation is already public, the dvas repository will remain private until we reach v1.0. This will happen, at the latest, when the dvas outcomes will be shared beyond the UAII 2022 project team.
+
+## License & Copyright
+dvas is distributed under the terms of the GNU General Public License v3.0 or later. The dvas copyright belongs to MeteoSwiss. See [here](https://meteoswiss.github.io/dvas/license.html) for details.
---
+++ packages/dvas/dvas-1.0.0.tar.gz/dvas-1.0.0/setup.py
@@ -0,0 +1,78 @@
+"""
+Copyright (c) 2020-2022 MeteoSwiss, contributors listed in AUTHORS.
+
+Distributed under the terms of the GNU General Public License v3.0 or later.
+
+SPDX-License-Identifier: GPL-3.0-or-later
+
+"""
+
+# Import from python packages
+from pathlib import Path
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+
+# Run the version file
+with open(Path('.') / 'src' / 'dvas' / 'version.py') as fid:
+    version = next(line.split("'")[1] for line in fid.readlines() if 'VERSION' in line)
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    dependency_links=[],
+    name="dvas",
+    version=version,
+    license='GNU General Public License v3 or later (GPLv3+)',
+
+    # Include all packages under src
+    packages=find_packages("src"),
+    # Tell setuptools packages are under src
+    package_dir={"": "src"},
+
+    url="https://github.com/MeteoSwiss/dvas",
+    author="MeteoSwiss",
+    author_email="",
+    description="Data Visualisation and Analysis Software for meteorological radiosounding",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires='>=3.10.0',
+    install_requires=[
+        "astroid>=2.13",
+        "jsonschema",
+        "matplotlib",
+        "netcdf4",
+        'numpy',
+        "pandas>=1.3.0",
+        "peewee",
+        "pytz",
+        "ruamel-yaml",
+        "scipy",
+        "sre_yield",
+    ],
+    extras_require={
+        'dev': ['sphinx', 'sphinx-rtd-theme', 'plantweb', 'pylint', 'pytest', 'pytest-data']
+    },
+    # Setup entry points to use dvas from a terminal
+    entry_points={
+        'console_scripts': ['dvas_init_arena=dvas_recipes.__main__:dvas_init_arena',
+                            'dvas_optimize=dvas_recipes.__main__:dvas_optimize',
+                            'dvas_run_recipe=dvas_recipes.__main__:dvas_run_recipe']
+    },
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Atmospheric Science',
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 3.10',
+    ],
+
+    include_package_data=True,  # So that non .py files make it onto pypi, and then back !
+)
