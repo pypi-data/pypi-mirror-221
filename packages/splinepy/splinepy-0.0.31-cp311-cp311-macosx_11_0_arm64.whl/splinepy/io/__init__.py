0f9413b573843b0b136e9292e2eff69b8101fc79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:37 +0000

    Add package splinepy/0.0.31/splinepy-0.0.31-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/splinepy/splinepy-0.0.31-cp311-cp311-macosx_11_0_arm64.whl/splinepy/io/__init__.py
@@ -0,0 +1,13 @@
+from splinepy.io import gismo, iges, ioutils, irit, json, mfem, npz, vtk, xml
+
+__all__ = [
+    "gismo",
+    "iges",
+    "ioutils",
+    "irit",
+    "json",
+    "mfem",
+    "npz",
+    "vtk",
+    "xml",
+]
