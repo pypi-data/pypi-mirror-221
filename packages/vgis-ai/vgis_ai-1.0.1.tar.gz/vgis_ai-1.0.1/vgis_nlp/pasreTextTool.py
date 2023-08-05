b4fcc59d8b5e4f46e21591250804d44ea8350138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:49 +0000

    Add package vgis-ai/1.0.1/vgis_ai-1.0.1.tar.gz

---
+++ packages/vgis-ai/vgis_ai-1.0.1.tar.gz/vgis_ai-1.0.1/vgis_nlp/pasreTextTool.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3.9
+# -*- coding: utf-8 -*-
+# @Time    : 2021/1/18 10:24
+# @Author  : chenxw
+# @Email   : gisfanmache.@gmail.com
+# @File    : pasreTextTool.py
+# @Descr   : 从文本信息里识别出有用资料
+# @Software: PyCharm
+
+from aip import AipNlp
+from vgis_nlp.parseTextSettings import Setting
+import binascii
+
+
+class TextParser:
+    # 初始化
+    def __init__(self):
+        pass
+
+    def parse_address_in_text(self, pasre_text):
+        setting = Setting()
+        auth_str = setting.get_AIP_AUTH_STR()
+        decode_str = binascii.a2b_hex(auth_str).decode("utf-8")
+        key_list = decode_str.split(setting.get_AUTH_SPLIT_STR())
+        client = AipNlp(key_list[0], key_list[1], key_list[2])
+        result = client.address(pasre_text)
+        obj = {}
+        if "error_code" not in result:
+            obj["is_success"] = True
+            obj["province"] = result.get("province")
+            obj["city"] = result.get("city")
+            obj["county"] = result.get("county")
+            obj["town"] = result.get("town")
+        else:
+            obj["is_success"] = False
+            obj["error_code"] = result.get("error_code")
+            obj["error_msg"] = result.get("error_msg")
+        return obj
