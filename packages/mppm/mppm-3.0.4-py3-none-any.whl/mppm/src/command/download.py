fb65a5f39def6e9edb2bd17b0ab32c4092f109f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:05:57 +0000

    Add package mppm/3.0.4/mppm-3.0.4-py3-none-any.whl

---
+++ packages/mppm/mppm-3.0.4-py3-none-any.whl/mppm/src/command/download.py
@@ -0,0 +1,28 @@
+from src.common.utility import *
+from src.config.pip_conf import *
+import os
+
+
+class DownloadCmd():
+    def __init__(self, args):
+        self.args = args
+
+    def exec(self, pip_path):
+        if self.args.output != "./":
+            base_output = self.args.output
+        else:
+            if self.args.module:
+                base_output = f"{self.args.output}{self.args.module}"
+            else:
+                base_output = f"{self.args.output}{ARG_DOWNLOAD_REQUIREMENT}"
+        create_dir(base_output)
+        if self.args.module:
+            download_pip_pkg_cmd = "{} download {} -d {}".format(pip_path, self.args.module, base_output)
+        else:
+            download_pip_pkg_cmd = "{} download -r {} -d {}".format(pip_path, self.args.requirement,
+                                                                    base_output)
+        cmd_result = exec_cmd(download_pip_pkg_cmd)
+        if cmd_result is None or ignore_errors[self.args.sub_cmd] in cmd_result:
+            print_colored(f"the module and dependencies have been downloaded. Please check the {base_output} path", "green")
+        else:
+            print_colored(cmd_result, "red")
