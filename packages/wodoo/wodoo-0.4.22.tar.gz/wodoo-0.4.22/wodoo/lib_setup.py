d441cf27eb546105aba9b128f78dfcf6f6fe09ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:59 +0000

    Add package wodoo/0.4.22/wodoo-0.4.22.tar.gz

---
+++ packages/wodoo/wodoo-0.4.22.tar.gz/wodoo-0.4.22/wodoo/lib_setup.py
@@ -0,0 +1,91 @@
+import click
+import sys
+import subprocess
+from .tools import _askcontinue
+from .tools import remove_webassets
+from .cli import cli, pass_config, Commands
+from .lib_clickhelpers import AliasedGroup
+
+
+@cli.group(cls=AliasedGroup)
+@pass_config
+def setup(config):
+    pass
+
+
+@setup.command()
+@pass_config
+@click.pass_context
+def show_effective_settings(ctx, config):
+    from . import MyConfigParser
+
+    config = MyConfigParser(config.files["settings"])
+    for k in sorted(config.keys()):
+        click.echo("{}={}".format(k, config[k]))
+
+
+@setup.command(name="remove-web-assets")
+@pass_config
+@click.pass_context
+def remove_web_assets(ctx, config):
+    """
+    if odoo-web interface is broken (css, js) then purging the web-assets helps;
+    they are usually recreated when admin login
+    """
+    from .odoo_config import current_version
+
+    _askcontinue(config)
+    conn = config.get_odoo_conn().clone(dbname=config.dbname)
+    remove_webassets(conn)
+    if current_version() <= 10.0:
+        click.echo("Please login as admin, so that assets are recreated.")
+
+
+@setup.command()
+@pass_config
+def status(config):
+    _status(config)
+
+
+def _status(config):
+    color = "yellow"
+    click.secho("projectname: ", nl=False)
+    click.secho(config.project_name, fg=color, bold=True)
+    click.secho("version: ", nl=False)
+    click.secho(config.odoo_version, fg=color, bold=True)
+    click.secho("db: ", nl=False)
+    click.secho(config.dbname, fg=color, bold=True)
+    if config.PROXY_PORT:
+        click.secho("url: ", nl=False)
+        click.secho(f"http://localhost:{config.PROXY_PORT}", fg=color, bold=True)
+
+    for key in [
+        "DEFAULT_DEV_PASSWORD",
+        "ODOO_DEMO",
+        "ODOO_QUEUEJOBS_CHANNELS",
+        "ODOO_QUEUEJOBS_CRON_IN_ONE_CONTAINER",
+        "ODOO_CRON_IN_ONE_CONTAINER",
+        "RUN_ODOO_CRONJOBS",
+        "RUN_ODOO_QUEUEJOBS",
+    ]:
+        click.secho(f"{key}:", nl=False, fg=color)
+        click.secho(getattr(config, key))
+
+
+@setup.command(help="Upgrade wodoo")
+def upgrade():
+    cmd = [sys.executable, "-mpip", "install", "wodoo", "-U"]
+    subprocess.check_call(cmd)
+
+
+@setup.command()
+@click.argument("lines")
+def produce_test_lines(lines):
+    import lorem
+
+    lines = int(lines)
+    for i in range(lines):
+        click.secho(lorem.paragraph())
+
+
+Commands.register(status)
