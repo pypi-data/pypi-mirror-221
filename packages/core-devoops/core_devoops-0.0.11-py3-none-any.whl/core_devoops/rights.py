f8738c61ddf1b9d96722419b4abe908c8ae80dd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:19 +0000

    Add package core-devoops/0.0.11/core_devoops-0.0.11-py3-none-any.whl

---
+++ packages/core-devoops/core_devoops-0.0.11-py3-none-any.whl/core_devoops/rights.py
@@ -0,0 +1,24 @@
+"""
+Module implementing the sqlmodel orm part of the right table
+"""
+from typing import Optional
+from typing import TYPE_CHECKING
+
+from sqlmodel import Field
+from sqlmodel import Relationship
+from sqlmodel import SQLModel
+
+
+if TYPE_CHECKING:
+    from core_devoops.user import User
+
+
+class Right(SQLModel, table=True):  # type: ignore
+    """
+    Simple right class: listing all app_services that a particular user can access to
+    """
+    __tablename__ = 'application_right'
+    id: Optional[int] = Field(default=None, primary_key=True)
+    app_service: str
+    user_id: Optional[int] = Field(default=None, foreign_key='user.id')
+    user: Optional['User'] = Relationship(back_populates='rights')
