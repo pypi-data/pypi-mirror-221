fb46ded3b0cea1449ddfd7b801598805dee2dd99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:41 +0000

    Add package djangocms-aldryn-categories/2.0.1/djangocms_aldryn_categories-2.0.1-py3-none-any.whl

---
+++ packages/djangocms-aldryn-categories/djangocms_aldryn_categories-2.0.1-py3-none-any.whl/aldryn_categories/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from django.utils.translation import gettext
+
+from parler.admin import TranslatableAdmin
+
+from treebeard.admin import TreeAdmin
+
+from .forms import CategoryAdminForm
+from .models import Category
+
+
+class CategoryAdmin(TranslatableAdmin, TreeAdmin):
+    form = CategoryAdminForm
+
+    fieldsets = (
+        (None, {
+            'fields': (
+                'name',
+                'slug',
+            )
+        }),
+        (' ', {
+            'fields': (
+                '_position',
+                '_ref_node_id',
+            )
+        }),
+    )
+
+    def get_form(self, request, obj=None, **kwargs):
+        FormClass = super().get_form(request, obj, **kwargs)
+        # Workaround for missing translations on treebeard
+        FormClass.base_fields['_position'].label = gettext('Position')
+        FormClass.base_fields['_ref_node_id'].label = gettext('Relative to')
+        return FormClass
+
+
+admin.site.register(Category, CategoryAdmin)
