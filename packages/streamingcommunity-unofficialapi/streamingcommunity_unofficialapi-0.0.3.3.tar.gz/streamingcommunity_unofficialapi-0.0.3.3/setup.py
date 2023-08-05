44b1ca7f51576d375153a2883163b098726ffcae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:07 +0000

    Add package streamingcommunity-unofficialapi/0.0.3.3/streamingcommunity_unofficialapi-0.0.3.3.tar.gz

---
+++ packages/streamingcommunity-unofficialapi/streamingcommunity_unofficialapi-0.0.3.3.tar.gz/streamingcommunity_unofficialapi-0.0.3.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="streamingcommunity_unofficialapi",
+    author="Beqir Stafa",
+    author_email="beqirstafa@gmail.com",
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Blu-Tiger/streamingcommunity-unofficialapi",
+    description="A simple unofficial api for the italian StreamingCommunity website.",
+    version="0.0.3.3",
+    packages=find_packages(),
+    install_requires=[
+        "setuptools>=61.0",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Utilities",
+    ],
+    python_requires='>= 3.10',
+)
