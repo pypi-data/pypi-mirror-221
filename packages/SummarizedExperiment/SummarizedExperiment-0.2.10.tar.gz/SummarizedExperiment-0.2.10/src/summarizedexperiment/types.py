698859847b87b4280f8fb7dd07292d8e9d1cdc4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:46 +0000

    Add package SummarizedExperiment/0.2.10/SummarizedExperiment-0.2.10.tar.gz

---
+++ packages/SummarizedExperiment/SummarizedExperiment-0.2.10.tar.gz/SummarizedExperiment-0.2.10/src/summarizedexperiment/types.py
@@ -0,0 +1,22 @@
+from typing import Sequence, Tuple, Union
+
+import numpy as np
+import pandas as pd
+from biocframe import BiocFrame
+from filebackedarray import H5BackedDenseData, H5BackedSparseData
+from scipy import sparse as sp
+
+from collections import namedtuple
+
+__author__ = "jkanche"
+__copyright__ = "jkanche"
+__license__ = "MIT"
+
+MatrixTypes = Union[np.ndarray, sp.spmatrix, H5BackedSparseData, H5BackedDenseData]
+BiocOrPandasFrame = Union[pd.DataFrame, BiocFrame]
+MatrixSlicerTypes = Union[Sequence[int], Sequence[bool], slice]
+SlicerTypes = Union[Sequence[int], Sequence[bool], Sequence[str], slice]
+SlicerArgTypes = Union[Tuple[SlicerTypes], Sequence[SlicerTypes], slice]
+SlicerResult = namedtuple(
+    "SlicerResult", ["rowData", "colData", "assays", "rowIndices", "colIndices"]
+)
