5597e4115fcaaf714b58ab58716707b16ed8a10d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:31 +0000

    Add package examon-core/1.0.10/examon_core-1.0.10.tar.gz

---
+++ packages/examon-core/examon_core-1.0.10.tar.gz/examon_core-1.0.10/examon_core/code_metrics.py
@@ -0,0 +1,51 @@
+import logging
+from dataclasses import dataclass
+
+
+from radon.raw import analyze
+from radon.metrics import h_visit
+
+
+@dataclass
+class CodeMetrics:
+    code_as_string: str
+    no_of_functions: int = None
+    loc: int = None
+    lloc: int = None
+    sloc: int = None
+    difficulty: float = None
+
+    def __repr__(self):
+        return f'CodeMetrics(difficulty: {self.difficulty},' \
+               f'no_of_functions: {self.no_of_functions}, ' \
+               f'loc: {self.loc}, ' \
+               f'lloc: {self.lloc}, ' \
+               f'lloc: {self.sloc})'
+
+    def calc_difficulty(self):
+        value = self.difficulty
+        if value == 0:
+            return "Easy"
+        elif 0 < value <= 1:
+            return "Medium"
+        elif 1 < value < 3:
+            return "Hard"
+        elif value >= 3:
+            return 'Very Hard'
+
+
+class CodeMetricsFactory:
+    @staticmethod
+    def build(code_as_string):
+        cm = CodeMetrics(code_as_string)
+        raw = analyze(cm.code_as_string)
+        visit_data = h_visit(cm.code_as_string)
+
+        cm.difficulty = round(visit_data.total.difficulty, 2)
+        cm.no_of_functions = len(visit_data.functions)
+        cm.loc = raw.loc
+        cm.lloc = raw.lloc
+        cm.sloc = raw.sloc
+        logging.debug(f'CodeMetricsFactory.build: {cm}')
+
+        return cm
---
+++ packages/examon-core/examon_core-1.0.10.tar.gz/examon_core-1.0.10/examon_core/todo.md
@@ -0,0 +1,4 @@
+* Improve test coverage
+* Add end to end tests
+* Use factories as much as possible
+* Finalise for version 1
