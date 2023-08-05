162eb8f618154ec3a70105ce1b0c0bf139721409
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:59 +0000

    Add package kellanz-unique-package-name/0.1/kellanz-unique-package-name-0.1.tar.gz

---
+++ packages/kellanz-unique-package-name/kellanz-unique-package-name-0.1.tar.gz/kellanz-unique-package-name-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kellanz-unique-package-name',  # replace with your unique package name
+    version='0.1',
+    packages=find_packages(),
+    # other metadata...
+)
+
