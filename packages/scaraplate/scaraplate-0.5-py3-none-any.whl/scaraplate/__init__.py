c8859e0b7a3f36010b39b6c895ff8b9b75f772d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:58 +0000

    Add package scaraplate/0.5/scaraplate-0.5-py3-none-any.whl

---
+++ packages/scaraplate/scaraplate-0.5-py3-none-any.whl/scaraplate/__init__.py
@@ -0,0 +1,20 @@
+from .automation import (
+    GitCloneProjectVCS,
+    GitCloneTemplateVCS,
+    ProjectVCS,
+    TemplateVCS,
+    automatic_rollup,
+)
+from .rollup import InvalidScaraplateTemplateError, rollup
+from .template import TemplateMeta
+
+__all__ = (
+    "GitCloneProjectVCS",
+    "GitCloneTemplateVCS",
+    "InvalidScaraplateTemplateError",
+    "ProjectVCS",
+    "TemplateMeta",
+    "TemplateVCS",
+    "automatic_rollup",
+    "rollup",
+)
