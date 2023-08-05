1ac4264cb56f17ee048c5cdef8f7a93be9942cb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:08 +0000

    Add package jupyterlite-xeus-python/0.9.2/jupyterlite-xeus-python-0.9.2.tar.gz

---
+++ packages/jupyterlite-xeus-python/jupyterlite-xeus-python-0.9.2.tar.gz/jupyterlite-xeus-python-0.9.2/tests/test_xeus_python_env.py
@@ -0,0 +1,63 @@
+"""Test creating Python envs for jupyterlite-xeus-python."""
+
+import os
+from tempfile import TemporaryDirectory
+from pathlib import Path
+
+from jupyterlite_core.app import LiteStatusApp
+
+from jupyterlite_xeus_python.env_build_addon import XeusPythonEnv
+
+
+def test_python_env():
+    app = LiteStatusApp(log_level="DEBUG")
+    app.initialize()
+    manager = app.lite_manager
+
+    addon = XeusPythonEnv(manager)
+    addon.packages = ["numpy", "ipyleaflet"]
+
+    for step in addon.post_build(manager):
+        pass
+
+    # Check env
+    assert os.path.isdir("/tmp/xeus-python-kernel/envs/xeus-python-kernel")
+
+    assert os.path.isfile(
+        "/tmp/xeus-python-kernel/envs/xeus-python-kernel/bin/xpython_wasm.js"
+    )
+    assert os.path.isfile(
+        "/tmp/xeus-python-kernel/envs/xeus-python-kernel/bin/xpython_wasm.wasm"
+    )
+
+    # Check empack output
+    assert os.path.isfile(Path(addon.cwd.name) / "empack_env_meta.json")
+
+    os.remove(Path(addon.cwd.name) / "empack_env_meta.json")
+
+
+def test_python_env_from_file_1():
+    app = LiteStatusApp(log_level="DEBUG")
+    app.initialize()
+    manager = app.lite_manager
+
+    addon = XeusPythonEnv(manager)
+    addon.environment_file = "environment-1.yml"
+
+    for step in addon.post_build(manager):
+        pass
+
+    # Check env
+    assert os.path.isdir("/tmp/xeus-python-kernel/envs/xeus-python-kernel-1")
+
+    assert os.path.isfile(
+        "/tmp/xeus-python-kernel/envs/xeus-python-kernel-1/bin/xpython_wasm.js"
+    )
+    assert os.path.isfile(
+        "/tmp/xeus-python-kernel/envs/xeus-python-kernel-1/bin/xpython_wasm.wasm"
+    )
+
+    # Check empack output
+    assert os.path.isfile(Path(addon.cwd.name) / "empack_env_meta.json")
+
+    os.remove(Path(addon.cwd.name) / "empack_env_meta.json")
