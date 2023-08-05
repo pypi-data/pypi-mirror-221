4891ff0b18ddbc2cad60912cb4608f81fbe55321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:25 +0000

    Add package mage-ai/0.9.7/mage-ai-0.9.7.tar.gz

---
+++ packages/mage-ai/mage-ai-0.9.7.tar.gz/mage-ai-0.9.7/mage_ai/api/resources/BlockResource.py
@@ -0,0 +1,208 @@
+import urllib.parse
+
+from mage_ai.api.errors import ApiError
+from mage_ai.api.resources.GenericResource import GenericResource
+from mage_ai.cache.block import BlockCache
+from mage_ai.data_preparation.models.block import Block
+from mage_ai.data_preparation.models.block.dbt import DBTBlock
+from mage_ai.data_preparation.models.block.utils import clean_name
+from mage_ai.data_preparation.models.constants import (
+    FILE_EXTENSION_TO_BLOCK_LANGUAGE,
+    BlockLanguage,
+    BlockType,
+)
+from mage_ai.data_preparation.models.custom_templates.custom_block_template import (
+    CustomBlockTemplate,
+)
+from mage_ai.data_preparation.utils.block.convert_content import convert_to_block
+from mage_ai.orchestration.db import safe_db_query
+from mage_ai.settings.repo import get_repo_path
+
+
+class BlockResource(GenericResource):
+    @classmethod
+    @safe_db_query
+    async def create(self, payload, user, **kwargs):
+        pipeline = kwargs.get('parent_model')
+
+        block_type = payload.get('type')
+        content = payload.get('content')
+        language = payload.get('language')
+        name = payload.get('name')
+
+        """
+        New DBT models include "content" in its block create payload,
+        whereas creating blocks from existing DBT model files do not.
+        """
+        if payload.get('type') == BlockType.DBT and content and language == BlockLanguage.SQL:
+            dbt_block = DBTBlock(
+                name,
+                clean_name(name),
+                BlockType.DBT,
+                configuration=payload.get('configuration'),
+                language=language,
+            )
+            if dbt_block.file_path and dbt_block.file.exists():
+                raise Exception('DBT model at that folder location already exists. \
+                    Please choose a different model name, or add a DBT model by \
+                    selecting single model from file.')
+
+        payload_config = payload.get('config')
+
+        block_name = name or payload.get('uuid')
+        block_attributes = dict(
+            color=payload.get('color'),
+            config=payload_config,
+            configuration=payload.get('configuration'),
+            extension_uuid=payload.get('extension_uuid'),
+            language=language,
+            pipeline=pipeline,
+            priority=payload.get('priority'),
+            upstream_block_uuids=payload.get('upstream_blocks', []),
+        )
+
+        replicated_block_uuid = payload.get('replicated_block')
+        if replicated_block_uuid:
+            replicated_block = pipeline.get_block(replicated_block_uuid)
+            if replicated_block:
+                block_type = replicated_block.type
+                block_attributes['language'] = replicated_block.language
+                block_attributes['replicated_block'] = replicated_block.uuid
+            else:
+                error = ApiError.RESOURCE_INVALID.copy()
+                error.update(
+                    message=f'Replicated block {replicated_block_uuid} ' +
+                    f'does not exist in pipeline {pipeline.uuid}.',
+                )
+                raise ApiError(error)
+
+        if payload_config and payload_config.get('custom_template_uuid'):
+            template_uuid = payload_config.get('custom_template_uuid')
+            custom_template = CustomBlockTemplate.load(template_uuid=template_uuid)
+            block = custom_template.create_block(
+                block_name,
+                pipeline,
+                extension_uuid=block_attributes.get('extension_uuid'),
+                priority=block_attributes.get('priority'),
+                upstream_block_uuids=block_attributes.get('upstream_block_uuids'),
+            )
+            content = custom_template.load_template_content()
+        else:
+            block = Block.create(
+                block_name,
+                block_type,
+                get_repo_path(),
+                **block_attributes,
+            )
+
+        if content:
+            if payload.get('converted_from'):
+                content = convert_to_block(block, content)
+
+            block.update_content(content)
+
+        cache = await BlockCache.initialize_cache()
+        cache.add_pipeline(block, pipeline)
+
+        return self(block, user, **kwargs)
+
+    @classmethod
+    @safe_db_query
+    def member(self, pk, user, **kwargs):
+        error = ApiError.RESOURCE_INVALID.copy()
+
+        query = kwargs.get('query', {})
+
+        extension_uuid = query.get('extension_uuid', [None])
+        if extension_uuid:
+            extension_uuid = extension_uuid[0]
+
+        block_type = query.get('block_type', [None])
+        if block_type:
+            block_type = block_type[0]
+
+        pipeline = kwargs.get('parent_model')
+        if pipeline:
+            block = pipeline.get_block(pk, block_type=block_type, extension_uuid=extension_uuid)
+            if block:
+                return self(block, user, **kwargs)
+            else:
+                if extension_uuid:
+                    message = f'Block {pk} does not exist in pipeline {pipeline.uuid} ' \
+                        f'for extension {extension_uuid}.'
+                else:
+                    message = f'Block {pk} does not exist in pipeline {pipeline.uuid}.'
+                error.update(message=message)
+                raise ApiError(error)
+
+        block_type_and_uuid = urllib.parse.unquote(pk)
+        parts = block_type_and_uuid.split('/')
+
+        if len(parts) < 2:
+            error.update(message='The url path should be in block_type/block_uuid format.')
+            raise ApiError(error)
+
+        block_type = parts[0]
+        block_uuid_with_extension = '/'.join(parts[1:])
+        parts2 = block_uuid_with_extension.split('.')
+
+        language = None
+        if len(parts2) >= 2:
+            block_uuid = '.'.join(parts2[:-1])
+            language = FILE_EXTENSION_TO_BLOCK_LANGUAGE[parts2[-1]]
+        else:
+            block_uuid = block_uuid_with_extension
+
+        block_language = query.get('block_language', [None])
+        if block_language:
+            block_language = block_language[0]
+        if block_language:
+            language = block_language
+
+        if BlockType.DBT == block_type:
+            block = DBTBlock(
+                block_uuid,
+                block_uuid,
+                block_type,
+                configuration=dict(file_path=block_uuid_with_extension),
+                language=language,
+            )
+        else:
+            block = Block.get_block(block_uuid, block_uuid, block_type, language=language)
+
+        if not block.exists():
+            error.update(ApiError.RESOURCE_NOT_FOUND)
+            raise ApiError(error)
+
+        return self(block, user, **kwargs)
+
+    @safe_db_query
+    async def delete(self, **kwargs):
+        query = kwargs.get('query', {})
+
+        force = query.get('force', [False])
+        if force:
+            force = force[0]
+
+        pipeline = kwargs.get('parent_model')
+        cache = await BlockCache.initialize_cache()
+        if pipeline:
+            cache.remove_pipeline(self.model, pipeline.uuid)
+
+        return self.model.delete(force=force)
+
+    @safe_db_query
+    def update(self, payload, **kwargs):
+        query = kwargs.get('query', {})
+        update_state = query.get('update_state', [False])
+        if update_state:
+            update_state = update_state[0]
+        self.model.update(
+            payload,
+            update_state=update_state,
+        )
+
+    async def get_pipelines_from_cache(self):
+        await BlockCache.initialize_cache()
+
+        return self.model.get_pipelines_from_cache()
---
+++ packages/mage-ai/mage-ai-0.9.7.tar.gz/mage-ai-0.9.7/mage_ai/orchestration/backfills/service.py
@@ -0,0 +1,139 @@
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from mage_ai.data_preparation.models.triggers import (
+    ScheduleInterval,
+    ScheduleStatus,
+)
+from mage_ai.orchestration.db.models.schedules import Backfill, PipelineRun, PipelineSchedule
+from mage_ai.shared.hash import merge_dict
+from typing import Dict, List
+import dateutil.parser
+
+
+def start_backfill(backfill: Backfill) -> List[PipelineRun]:
+    pipeline_runs = []
+    variables_list = __build_variables_list(backfill)
+
+    if len(variables_list) == 0:
+        return []
+
+    backfill_variables = backfill.variables
+
+    pipeline_schedule = backfill.pipeline_schedule
+    if not pipeline_schedule:
+        pipeline_schedule = PipelineSchedule.create(
+            name=f'Backfill {backfill.name}',
+            pipeline_uuid=backfill.pipeline_uuid,
+            schedule_interval=ScheduleInterval.ONCE,
+            start_time=datetime.utcnow(),
+            variables=backfill_variables,
+        )
+
+    for backfill_run_variables in variables_list:
+        execution_date = None
+        if 'execution_date' in backfill_run_variables:
+            execution_date = dateutil.parser.parse(backfill_run_variables['execution_date'])
+
+        pipeline_run = PipelineRun.create(
+            backfill_id=backfill.id,
+            execution_date=execution_date,
+            pipeline_schedule_id=pipeline_schedule.id,
+            pipeline_uuid=backfill.pipeline_uuid,
+            status=PipelineRun.PipelineRunStatus.RUNNING,
+            variables=merge_dict(backfill_variables or {}, backfill_run_variables),
+        )
+        pipeline_runs.append(pipeline_run)
+
+    backfill.update(pipeline_schedule_id=pipeline_schedule.id)
+    pipeline_schedule.update(status=ScheduleStatus.ACTIVE)
+
+    return pipeline_runs
+
+
+def preview_run_dates(backfill: Backfill) -> List[Dict]:
+    variables_list = __build_variables_list(backfill)
+
+    return variables_list
+
+
+def cancel_backfill(backfill: Backfill) -> None:
+    for pipeline_run in PipelineRun.query.filter(
+        PipelineRun.backfill_id == backfill.id,
+        PipelineRun.status.not_in([
+            PipelineRun.PipelineRunStatus.CANCELLED,
+            PipelineRun.PipelineRunStatus.COMPLETED,
+            PipelineRun.PipelineRunStatus.FAILED,
+        ]),
+    ):
+        pipeline_run.update(status=PipelineRun.PipelineRunStatus.CANCELLED)
+
+    backfill.update(status=Backfill.Status.CANCELLED)
+    backfill.pipeline_schedule.update(status=ScheduleStatus.INACTIVE)
+
+
+def __build_variables_list(backfill: Backfill) -> List[Dict]:
+    if backfill.block_uuid:
+        return []
+
+    dates = __build_dates(backfill)
+    number_of_dates = len(dates)
+
+    arr = []
+
+    for idx, execution_date in enumerate(dates):
+        interval_end_datetime = None
+        interval_seconds = None
+        interval_start_datetime = execution_date
+
+        if idx < number_of_dates - 1:
+            interval_end_datetime = dates[idx + 1]
+            interval_seconds = interval_end_datetime.timestamp() - execution_date.timestamp()
+
+        if interval_end_datetime:
+            interval_end_datetime = interval_end_datetime.isoformat()
+
+        if interval_start_datetime:
+            interval_start_datetime = interval_start_datetime.isoformat()
+
+        arr.append(dict(
+            ds=execution_date.strftime('%Y-%m-%d'),
+            execution_date=execution_date.isoformat(),
+            hr=execution_date.strftime('%H'),
+            interval_end_datetime=interval_end_datetime,
+            interval_seconds=interval_seconds,
+            interval_start_datetime=interval_start_datetime,
+        ))
+
+    return arr
+
+
+def __build_dates(backfill: Backfill) -> List[datetime]:
+    dates = []
+
+    current_datetime = backfill.start_datetime
+    while current_datetime <= backfill.end_datetime:
+        dates.append(current_datetime)
+
+        interval_type = backfill.interval_type
+        if interval_type not in [
+            Backfill.IntervalType.MONTH,
+            Backfill.IntervalType.YEAR,
+        ]:
+            unit = f'{interval_type.value}s'
+            if Backfill.IntervalType.CUSTOM == interval_type:
+                unit = 'seconds'
+            current_datetime += timedelta(**{unit: backfill.interval_units})
+        else:
+            key = None
+            if Backfill.IntervalType.MONTH == interval_type:
+                key = 'months'
+            elif Backfill.IntervalType.YEAR == interval_type:
+                key = 'years'
+
+            delta = {
+                key: backfill.interval_units,
+            }
+            current_datetime = current_datetime + relativedelta(**delta)
+            current_datetime = current_datetime.replace(tzinfo=current_datetime.tzinfo)
+
+    return dates
