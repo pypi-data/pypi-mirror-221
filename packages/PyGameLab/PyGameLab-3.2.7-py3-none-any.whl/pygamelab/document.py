a75a508468eb49968f593829ad7b0cfab28ccb4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:10 +0000

    Add package PyGameLab/3.2.7/PyGameLab-3.2.7-py3-none-any.whl

---
+++ packages/PyGameLab/PyGameLab-3.2.7-py3-none-any.whl/pygamelab/document.py
@@ -0,0 +1,43 @@
+import difflib
+from PyGameLab import data
+
+class Unicode:
+    @staticmethod
+    def char(char_name):
+        if data.initialized:
+            if char_name in Unicode.key:
+                return Unicode.key[char_name]
+            else:
+                matches = difflib.get_close_matches(
+                    char_name, Unicode.key.keys(), n=1)
+                if matches:
+                    return Unicode.key[matches[0]]
+                else:
+                    raise ValueError(
+                        "No se encontró el carácter Unicode correspondiente.")
+        else:
+            raise NameError("PyGameLab no se ha inicializado correctamente.")
+
+
+
+
+class Colors:
+    class text:
+        RESET = "\033[0m"
+        for i in range(256):
+            locals()[f"C{i}"] = f"\033[38;5;{i}m"
+
+    class background:
+        RESET = "\033[0m"
+        for i in range(256):
+            locals()[f"C{i}"] = f"\033[48;5;{i}m"
+
+    class styles:
+        RESET_ALL = "\033[0m"
+        BRIGHT = "\033[1m"
+        DIM = "\033[2m"
+        ITALIC = "\033[3m"
+        UNDERLINE = "\033[4m"
+        BLINK = "\033[5m"
+        REVERSE = "\033[7m"
+        HIDDEN = "\033[8m"
