370760d4ec45acace29e56b7175c5ba5d0d6efec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:42 +0000

    Add package odooless/0.1.11/odooless-0.1.11.tar.gz

---
+++ packages/odooless/odooless-0.1.11.tar.gz/odooless-0.1.11/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='odooless',
+    version='0.1.11',
+    description='A DynamoDB ORM inspired by Odoo',
+    long_description='A DynamoDB ORM inspired by Odoo',
+    author='Sam Hasan',
+    author_email='sam@barameg.co',
+    url='https://github.com/Barameg/odooless.git',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    keywords='odooless',
+    install_requires=[
+        'boto3'
+        # List any dependencies your package needs
+    ],
+    python_requires='>=3.6',
+)
+
