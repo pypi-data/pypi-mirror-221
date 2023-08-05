9a580c79ac5457887bca240c754bbca3638385c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:00:27 +0000

    Add package RelayMe/0.0.7/RelayMe-0.0.7.tar.gz

---
+++ packages/RelayMe/RelayMe-0.0.7.tar.gz/RelayMe-0.0.7/RelayMe/__init__.py
@@ -0,0 +1,76 @@
+import logging
+import smtplib
+from email.message import EmailMessage
+
+
+class SenderConfig:
+    def __init__(self, config, format=None):
+        self.config = {key.lower(): value for key, value in config.items()}
+        self.logger = logging.getLogger('Sending Email')
+        self.logger.setLevel(logging.INFO)
+        formatter = logging.Formatter(
+            '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+
+        console_handler.setFormatter(formatter)
+        self.logger.addHandler(console_handler)
+        self.format = format
+
+    def send_email(self, body, recipients, subject=None, header=None, footer=None, sender=None, format=None):
+        email = EmailMessage()
+        email['Subject'] = subject or self.config['subject']
+        email['From'] = sender or self.config['sender']
+        email['To'] = recipients
+        head = (header or self.config['header']) + "\n\n"
+        foot = "\n\n" + (footer or self.config['footer'])
+        if (str(self.format).lower() == "html"):
+            message = f'''
+                    <!DOCTYPE html>
+                    <html>
+                        <body>
+                            <div style="padding:20px 0px">
+                                <div style="height: 500px;width:400px">
+                                    <div style="text-align:left;">
+                                        <h3 style="font-family:Georgia, 'Times New Roman', Times, serif;color#454349;">{str(head)}</h3>
+                                        <br>
+                                        <p>{str(body)}</p>
+                                        <br>
+                                        <h4>{str(foot)}</h4>
+                                    </div>
+                                </div>
+                            </div>
+                        </body>
+                    </html>
+                    '''
+            email.set_content(message, subtype='html')
+        elif format == "custom":
+            message = body
+            email.set_content(message)
+        else:
+            message = str(head + body + foot)
+            email.set_content(message)
+
+        try:
+            with smtplib.SMTP(self.config['server'], self.config['port']) as smtp:
+                if self.config.get('username') and self.config.get('password'):
+                    smtp.login(self.config['username'],
+                               self.config['password'])
+                smtp.send_message(email)
+            self.logger.info("Email sent successfully!")
+        except smtplib.SMTPException as e:
+            self.logger.error(f"Error sending email: {str(e)}")
+
+
+config = {
+    'EmailConfig': {
+        "Server": "smtp-relay.brevo.com",
+        "Port": "587",
+        "Username": "datalakealert@gmail.com",
+        "Password": "qACM2wgT7QnmRcLU",
+        "sender": "sample@email.com",
+        'subject': "Hello from the SenderConfig module!",
+        'header': "<strong>Error Alert</strong> from the AgriSompo Data Lake Processing System:",
+        'footer': "This is the footer"
+    }
+}
---
+++ packages/RelayMe/RelayMe-0.0.7.tar.gz/RelayMe-0.0.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='RelayMe',
+    version='0.0.7',
+    description='Sends emails with chosen email relay service',
+    long_description=open('README.txt').read() + '\n\n' +
+    open('CHANGELOG.txt').read(),
+    url='',
+    author='Elijah Phifer',
+    author_email='elijahphifer9@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords=['mail', 'email', 'relay', 'relayservice'],
+    packages=find_packages(),
+    install_requires=['']
+)
