d537898b99ca136546d08efa299a207d0f690801
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:45 +0000

    Add package mwj-apitest/1.1.4/mwj-apitest-1.1.4.tar.gz

---
+++ packages/mwj-apitest/mwj-apitest-1.1.4.tar.gz/mwj-apitest-1.1.4/mwjApiTest/templates/request_file_demo/run.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+'''
+@Time : 2023/7/14 16:21
+@Email : Lvan826199@163.com
+@公众号 : 梦无矶的测试开发之路
+@File : run.py
+'''
+__author__ = "梦无矶小仔"
+
+import mwjApiTest
+
+mwjApiTest.run()
