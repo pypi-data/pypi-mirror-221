fecbf98c1abf7fd9d0693a69e9269dc80b33ffd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:07:14 +0000

    Add package supervisor-shell-ui/0.2.7/supervisor-shell-ui-0.2.7.tar.gz

---
+++ packages/supervisor-shell-ui/supervisor-shell-ui-0.2.7.tar.gz/supervisor-shell-ui-0.2.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='supervisor-shell-ui',
+    version='v0.2.7',
+    author='Ciprian Mandache',
+    author_email='psyb0t@51k.eu',
+    description='A CLI alternative to the built-in web interface of Supervisor, offering a more convenient way to manage processes directly from the terminal.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires='>=3.6',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'supervisor-shell-ui=supervisor_shell_ui.main:main',
+        ],
+    },
+    license='GPL-3.0',
+)
