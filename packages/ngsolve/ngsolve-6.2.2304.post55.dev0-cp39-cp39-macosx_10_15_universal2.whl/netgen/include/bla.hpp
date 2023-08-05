7f3ede538f59e57bca1114e36c9e3b81b15a6199
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:08 +0000

    Add package ngsolve/6.2.2304.post55.dev0/ngsolve-6.2.2304.post55.dev0-cp39-cp39-macosx_10_15_universal2.whl

---
+++ packages/ngsolve/ngsolve-6.2.2304.post55.dev0-cp39-cp39-macosx_10_15_universal2.whl/netgen/include/bla.hpp
@@ -0,0 +1,31 @@
+#ifndef FILE_NGBLA
+#define FILE_NGBLA
+
+
+#include <ngstd.hpp>
+
+
+#include "complex_wrapper.hpp"
+
+#include "expr.hpp"
+#include "vector.hpp"
+#include "matrix.hpp"
+#include "ngblas.hpp"
+#include "cholesky.hpp"
+#include "symmetricmatrix.hpp"
+#include "bandmatrix.hpp"
+#include "triangular.hpp"
+#include "householder.hpp"
+#include "tensor.hpp"
+
+// so far we can move stuff to ngcore
+
+#include "calcinverse.hpp"
+#include "eigensystem.hpp"
+#include "simd_complex.hpp"
+
+#include "ng_lapack.hpp"
+#include "avector.hpp"
+
+
+#endif
