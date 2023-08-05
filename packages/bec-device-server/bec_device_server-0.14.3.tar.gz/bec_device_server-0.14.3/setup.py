0df6c63c12175c9da642ab2417a1dc1f5ab2f69d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:44 +0000

    Add package bec-device-server/0.14.3/bec_device_server-0.14.3.tar.gz

---
+++ packages/bec-device-server/bec_device_server-0.14.3.tar.gz/bec_device_server-0.14.3/setup.py
@@ -0,0 +1,24 @@
+import os
+import pathlib
+import subprocess
+
+from setuptools import setup
+
+current_path = pathlib.Path(__file__).parent.resolve()
+
+utils = f"{current_path}/../bec_lib/"
+
+ophyd_devices = os.getenv("OPHYD_DEVICES_PATH", f"{current_path}/../../ophyd_devices/")
+
+__version__ = "0.14.3"
+
+if __name__ == "__main__":
+    setup(
+        install_requires=["numpy", "ophyd", "msgpack", "pyyaml"],
+        entry_points={"console_scripts": ["bec-device-server = device_server:main"]},
+        extras_require={"dev": ["pytest", "pytest-random-order", "coverage", "black", "pylint"]},
+        version=__version__,
+    )
+    local_deps = [utils, ophyd_devices]
+    for dep in local_deps:
+        subprocess.run(f"pip install -e {dep}", shell=True, check=True)
