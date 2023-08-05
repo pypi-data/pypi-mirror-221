7301c99c5b9c86ae18d90700d3865bde910b5ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:51 +0000

    Add package skyramp/0.4.30/skyramp-0.4.30.tar.gz

---
+++ packages/skyramp/skyramp-0.4.30.tar.gz/skyramp-0.4.30/src/skyramp/__init__.py
@@ -0,0 +1,7 @@
+"""
+Provides utility functions and classes for leveraging Skyramp functionality.
+"""
+
+from skyramp.client import _Client as Client
+from skyramp.endpoint import _RestEndpoint as RestEndpoint
+from skyramp.endpoint import _GrpcEndpoint as GrpcEndpoint
---
+++ packages/skyramp/skyramp-0.4.30.tar.gz/skyramp-0.4.30/src/skyramp/client.py
@@ -0,0 +1,183 @@
+"""
+Defines a Skyramp client, which can be used to interact with a cluster.
+"""
+
+import ctypes
+import yaml
+
+from skyramp.utils import _library, _call_function
+
+
+class _Client:
+    """
+    Skyramp client object which can be used to interact with a cluster.
+    """
+
+    def __init__(
+        self,
+        kubeconfig_path: str = "",
+        cluster_name: str = "",
+        context: str = "",
+    ) -> None:
+        """
+        Initializes a Skyramp Client.
+
+        kubeconfig_path: The filesystem path of a kubeconfig
+        cluster_name: The name of the cluster.
+        context: The Kubernetes context within a kubeconfig
+        """
+        self.kubeconfig_path = kubeconfig_path
+        self.cluster_name = cluster_name
+        self.context = context
+
+        self._namespace_set = set()
+
+    def apply_local(self) -> None:
+        """
+        Creates a local cluster.
+        """
+        apply_local_function = _library.applyLocalWrapper
+        argtypes = []
+        restype = ctypes.c_char_p
+
+        _call_function(apply_local_function, argtypes, restype, [])
+
+        self.kubeconfig_path = self._get_kubeconfig_path()
+        if not self.kubeconfig_path:
+            raise Exception("no kubeconfig found")
+
+    def remove_local(self) -> None:
+        """
+        Removes a local cluster.
+        """
+        func = _library.removeLocalWrapper
+        argtypes = []
+        restype = ctypes.c_char_p
+
+        _call_function(func, argtypes, restype, [])
+
+    def add_kubeconfig(
+        self,
+        context: str,
+        cluster_name: str,
+        kubeconfig_path: str,
+    ) -> None:
+        """
+        Adds a preexisting Kubeconfig file to Skyramp.
+
+        context: The kubeconfig context to use
+        cluster_name: Name of the cluster
+        kubeconfig_path: filepath of the kubeconfig
+        """
+        func = _library.addKubeconfigWrapper
+        argtypes = [ctypes.c_char_p, ctypes.c_bool, ctypes.c_char_p, ctypes.c_char_p]
+        restype = ctypes.c_char_p
+
+        _call_function(
+            func,
+            argtypes,
+            restype,
+            [
+                context.encode(),
+                cluster_name.encode(),
+                kubeconfig_path.encode(),
+            ],
+        )
+
+        self.kubeconfig_path = kubeconfig_path
+
+    def remove_cluster(self, cluster_name: str) -> None:
+        """
+        Removes a cluster, corresponding to the name, from Skyramp
+        """
+        func = _library.removeClusterFromConfigWrapper
+        argtypes = [ctypes.c_char_p]
+        restype = ctypes.c_char_p
+
+        _call_function(func, argtypes, restype, [cluster_name.encode()])
+
+    def deploy_skyramp_worker(
+        self, namespace: str, worker_image: str, local_image: bool
+    ) -> None:
+        """
+        Installs a Skyramp worker onto a cluster if one is registered with Skyramp
+        """
+        if not self.kubeconfig_path:
+            raise Exception("no cluster to deploy worker to")
+
+        func = _library.deploySkyrampWorkerWrapper
+        argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_bool]
+        restype = ctypes.c_char_p
+
+        _call_function(
+            func,
+            argtypes,
+            restype,
+            [namespace.encode(), worker_image.encode(), local_image],
+        )
+
+        self._namespace_set.add(namespace)
+
+    def delete_skyramp_worker(self, namespace: str) -> None:
+        """
+        Removes the Skyramp worker, if a Skyramp worker is installed on a registered Skyramp cluster
+        """
+        if not self.kubeconfig_path:
+            raise Exception("no cluster to delete worker from")
+
+        if namespace not in self._namespace_set:
+            raise Exception(f"no worker to delete from {namespace} namespace")
+
+        func = _library.deleteSkyrampWorkerWrapper
+        argtypes = [ctypes.c_char_p]
+        restype = ctypes.c_char_p
+
+        _call_function(func, argtypes, restype, [namespace.encode()])
+
+        self._namespace_set.remove(namespace)
+
+    def mocker_apply(self, namespace: str, endpoint: str) -> None:
+        """
+        Applies a configuration to mocker.
+        namespace: The namespace where mocker resides
+        endpoint: The Skyramp enpdoint object
+        """
+        yaml_string = yaml.dump(endpoint.mock_description)
+
+        func = _library.applyMockDescriptionWrapper
+        argtypes = [
+            ctypes.c_char_p,
+            ctypes.c_char_p,
+        ]
+        restype = ctypes.c_char_p  # pylint: disable=duplicate-code
+
+        _call_function(
+            func,
+            argtypes,
+            restype,
+            [
+                namespace.encode(),
+                yaml_string.encode(),
+            ],
+        )
+
+    # This will be implemented when scenarios are added.
+    # def tester_start(self, namespace: str, scenario: str) -> str:
+    #     """
+    #     Runs tester at the given namespace and test name
+    #     namespace: The namespace where mocker resides
+    #     """
+    #     func = _library.runTesterStartWrapper
+    #     argtypes = [ctypes.c_char_p, ctypes.c_char_p]
+    #     restype = ctypes.c_char_p
+
+    #     _call_function(
+    #         func, argtypes, restype, [namespace.encode(), test_description.encode()]
+    #     )
+
+    def _get_kubeconfig_path(self) -> str:
+        func = _library.getKubeConfigPath
+        argtypes = []
+        restype = ctypes.c_char_p
+
+        return _call_function(func, argtypes, restype, [], True)
