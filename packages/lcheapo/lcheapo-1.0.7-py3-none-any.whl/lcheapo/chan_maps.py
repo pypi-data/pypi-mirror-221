317797b89c508b9dc45ffb067bbdbdd032ff6b10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:28 +0000

    Add package lcheapo/1.0.7/lcheapo-1.0.7-py3-none-any.whl

---
+++ packages/lcheapo/lcheapo-1.0.7-py3-none-any.whl/lcheapo/chan_maps.py
@@ -0,0 +1,4 @@
+chan_maps = {'SPOBS1': ['SH3:00', 'BDH:00'],
+             'SPOBS2': ['BDH:00', 'SH2:00', 'SH1:00', 'SH3:00'],
+             'BBOBS1': ['BH2:00', 'BH1:00', 'BHZ:00', 'BDH:00'],
+             'HYDROCT': ['BDH:00', 'BDH:01', 'BDH:02', 'BDH:03']}
