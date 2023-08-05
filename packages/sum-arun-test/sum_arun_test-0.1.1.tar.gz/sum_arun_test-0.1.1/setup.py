e3c5c983bfc198379198bce6e186a041e92c5051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:25 +0000

    Add package sum-arun-test/0.1.1/sum_arun_test-0.1.1.tar.gz

---
+++ packages/sum-arun-test/sum_arun_test-0.1.1.tar.gz/sum_arun_test-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sum_arun_test',
+    version='0.1.1',
+    packages=find_packages(),
+    author='Arun Uday',
+    description='A Python SDK for Testing sdk',
+)
