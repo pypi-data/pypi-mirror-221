1c9e2fbf3851fca3c432465201b6ae6f718ebbed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:53 +0000

    Add package ciokatana/0.1.0rc1/ciokatana-0.1.0rc1-py2.py3-none-any.whl

---
+++ packages/ciokatana/ciokatana-0.1.0rc1-py2.py3-none-any.whl/ciokatana/v1/components/widgets.py
@@ -0,0 +1,22 @@
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QLabel
+
+from ciokatana.v1 import const as k
+
+TEXT_COLOR = "#757575"
+LABEL_STYLESHEET = "QLabel {{color: {}; }}".format(TEXT_COLOR)
+CHECKBOX_STYLESHEET = "QCheckBox {{color: {}; }}".format(TEXT_COLOR)
+
+
+class FormLabel(QLabel):
+    def __init__(self, label, tooltip=None):
+        super(FormLabel, self).__init__()
+
+        self.setText(label)
+        self.setFixedWidth(170)
+        self.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignBottom)
+        self.setIndent(5)
+        self.setStyleSheet(LABEL_STYLESHEET)
+
+        if tooltip:
+            self.setToolTip(tooltip)
