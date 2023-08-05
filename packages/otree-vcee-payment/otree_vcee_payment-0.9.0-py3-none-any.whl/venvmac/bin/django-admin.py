87e7dd0e1dab7a948ff3163255d1f20a28173005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:57 +0000

    Add package otree-vcee-payment/0.9.0/otree_vcee_payment-0.9.0-py3-none-any.whl

---
+++ packages/otree-vcee-payment/otree_vcee_payment-0.9.0-py3-none-any.whl/venvmac/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/geoffreycastillo/Dropbox/phd/vcee/payment-app/venvmac/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
