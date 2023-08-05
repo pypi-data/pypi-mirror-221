8140cf47db80e373ce6fb036cdaa896cd9c3672c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:02:00 +0000

    Add package copyparty/1.8.7/copyparty-1.8.7-py3-none-any.whl

---
+++ packages/copyparty/copyparty-1.8.7-py3-none-any.whl/copyparty/sutil.py
@@ -0,0 +1,40 @@
+# coding: utf-8
+from __future__ import print_function, unicode_literals
+
+import tempfile
+from datetime import datetime
+
+from .bos import bos
+
+class StreamArc(object):
+    def __init__(
+        self,
+        log ,
+        fgen    ,
+        **kwargs 
+    ):
+        self.log = log
+        self.fgen = fgen
+
+    def gen(self)    :
+        raise Exception("override me")
+
+
+def errdesc(errors  )    :
+    report = ["copyparty failed to add the following files to the archive:", ""]
+
+    for fn, err in errors:
+        report.extend([" file: {}".format(fn), "error: {}".format(err), ""])
+
+    with tempfile.NamedTemporaryFile(prefix="copyparty-", delete=False) as tf:
+        tf_path = tf.name
+        tf.write("\r\n".join(report).encode("utf-8", "replace"))
+
+    dt = datetime.utcnow().strftime("%Y-%m%d-%H%M%S")
+
+    bos.chmod(tf_path, 0o444)
+    return {
+        "vp": "archive-errors-{}.txt".format(dt),
+        "ap": tf_path,
+        "st": bos.stat(tf_path),
+    }, report
