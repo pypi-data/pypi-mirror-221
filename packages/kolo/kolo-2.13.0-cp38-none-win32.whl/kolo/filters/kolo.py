7bc9d24d7202a2c995e4b9263cd925ef449dbf62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:10 +0000

    Add package kolo/2.13.0/kolo-2.13.0-cp38-none-win32.whl

---
+++ packages/kolo/kolo-2.13.0-cp38-none-win32.whl/kolo/filters/kolo.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+import os
+import types
+
+
+def kolo_filter(frame: types.FrameType, event: str, arg: object) -> bool:
+    """Don't profile kolo code"""
+    filename = frame.f_code.co_filename
+    paths = ("/kolo/middleware", "/kolo/profiler", "/kolo/serialize")
+    return any(os.path.normpath(path) in filename for path in paths)
