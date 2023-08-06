932b448cb167fa277ef7e60cab5e2aa38a0fdf3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:56 +0000

    Add package nasa-wildfires/1.1.1/nasa_wildfires-1.1.1-py2.py3-none-any.whl

---
+++ packages/nasa-wildfires/nasa_wildfires-1.1.1-py2.py3-none-any.whl/nasa_wildfires/__init__.py
@@ -0,0 +1,68 @@
+import csv
+
+import geojson
+import requests
+
+from .options import REGION_DICT
+
+REGION_LIST = list(REGION_DICT.keys())
+
+
+def get_modis(
+    region: str = "global", time_range: str = "24h"
+) -> geojson.FeatureCollection:
+    """
+    Hotspots detected by the MODIS satellite. Defaults to the world in a recent 24-hour period.
+
+    Returns GeoJSON.
+    """
+    base_url = "https://firms.modaps.eosdis.nasa.gov/data/active_fire/modis-c6.1/csv/MODIS_C6_1_{}_{}.csv"
+    name = REGION_DICT[region]
+    url = base_url.format(name, time_range)
+    features = _get_features(url)
+    return geojson.FeatureCollection(features)
+
+
+def get_viirs_suomi(
+    region: str = "global", time_range: str = "24h"
+) -> geojson.FeatureCollection:
+    """
+    Hotspots detected by the VIIRS Suomi-NPP (S-NPP) satellite. Defaults to the world in a recent 24-hour period.
+
+    Returns GeoJSON.
+    """
+    base_url = "https://firms.modaps.eosdis.nasa.gov/data/active_fire/suomi-npp-viirs-c2/csv/SUOMI_VIIRS_C2_{}_{}.csv"
+    name = REGION_DICT[region]
+    url = base_url.format(name, time_range)
+    features = _get_features(url)
+    return geojson.FeatureCollection(features)
+
+
+def get_viirs_noaa(
+    region: str = "global", time_range: str = "24h"
+) -> geojson.FeatureCollection:
+    """
+    Hotspots detected by the VIIRS NOAA-20 satellite. Defaults to the world in a recent 24-hour period.
+
+    Returns GeoJSON.
+    """
+    base_url = "https://firms.modaps.eosdis.nasa.gov/data/active_fire/noaa-20-viirs-c2/csv/J1_VIIRS_C2_{}_{}.csv"
+    name = REGION_DICT[region]
+    url = base_url.format(name, time_range)
+    features = _get_features(url)
+    return geojson.FeatureCollection(features)
+
+
+def _get_features(url: str) -> list:
+    """
+    Generic function for downloading data from NASA and reformatting as GeoJSON.
+    """
+    download = requests.get(url)
+    decoded_content = download.content.decode("utf-8")
+    reader = csv.DictReader(decoded_content.splitlines(), delimiter=",")
+    features = []
+    for r in reader:
+        coords = map(float, [r["longitude"], r["latitude"]])
+        f = geojson.Feature(geometry=geojson.Point(coords), properties=r)
+        features.append(f)
+    return features
---
+++ packages/nasa-wildfires/nasa_wildfires-1.1.1-py2.py3-none-any.whl/nasa_wildfires/cli.py
@@ -0,0 +1,97 @@
+import click
+import geojson
+
+from nasa_wildfires import get_modis, get_viirs_noaa, get_viirs_suomi, options
+
+
+@click.group()
+def cmd():
+    """
+    A command-line interface for downloading wildfire data from NASA satellites.
+
+    Returns GeoJSON.
+    """
+    pass
+
+
+@cmd.command(help="Hotspots detected by the MODIS satellite in a recent 24-hour period")
+@click.option(
+    "-r",
+    "--region",
+    default="Global",
+    type=click.Choice(list(options.REGION_DICT.keys()), case_sensitive=False),
+    help="Hotspot region",
+)
+@click.option(
+    "-t",
+    "--time-range",
+    default="24h",
+    type=click.Choice(options.TIME_RANGE_LIST, case_sensitive=False),
+    help="Time range",
+)
+@click.option("--indent", default=0, help="Indentation of output")
+@click.option(
+    "--sort-keys/--no-sort-keys", default=True, help="Sort the properties keys"
+)
+def modis(region, time_range, indent, sort_keys):
+    data = get_modis(region, time_range)
+    output = geojson.dumps(data, indent=indent, sort_keys=sort_keys)
+    click.echo(output)
+
+
+@cmd.command(
+    help="Hotspots detected by the VIIRS satellite (S-NPP) in a recent 24-hour period"
+)
+@click.option(
+    "-r",
+    "--region",
+    default="Global",
+    type=click.Choice(list(options.REGION_DICT.keys()), case_sensitive=False),
+    help="Hotspot region",
+)
+@click.option(
+    "-t",
+    "--time-range",
+    default="24h",
+    type=click.Choice(options.TIME_RANGE_LIST, case_sensitive=False),
+    help="Time range",
+)
+@click.option("--indent", default=0, help="Indentation of output")
+@click.option(
+    "--sort-keys/--no-sort-keys", default=True, help="Sort the properties keys"
+)
+def viirs_suomi(region, time_range, indent, sort_keys):
+    data = get_viirs_suomi(region, time_range)
+    output = geojson.dumps(data, indent=indent, sort_keys=sort_keys)
+    click.echo(output)
+
+
+@cmd.command(
+    help="Hotspots detected by the VIIRS satellite (NOAA-20) in a recent 24-hour period"
+)
+@click.option(
+    "-r",
+    "--region",
+    default="Global",
+    type=click.Choice(list(options.REGION_DICT.keys()), case_sensitive=False),
+    help="Hotspot region",
+)
+@click.option(
+    "-t",
+    "--time-range",
+    default="24h",
+    type=click.Choice(options.TIME_RANGE_LIST, case_sensitive=False),
+    help="Time range",
+)
+@click.option("--indent", default=0, help="Indentation of output")
+@click.option(
+    "--sort-keys/--no-sort-keys", default=True, help="Sort the properties keys"
+)
+def viirs_noaa(region, time_range, indent, sort_keys):
+    data = get_viirs_noaa(region, time_range)
+    output = geojson.dumps(data, indent=indent, sort_keys=sort_keys)
+    click.echo(output)
+
+
+if __name__ == "__main__":
+    cmd()
