8bc41a892a032d45da7c9506b5285c7f867decbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:12 +0000

    Add package geomancy/0.8.1/geomancy-0.8.1.tar.gz

---
+++ packages/geomancy/geomancy-0.8.1.tar.gz/geomancy-0.8.1/docs/index.md
@@ -0,0 +1,33 @@
+
+# Geomancy
+
+```{include} ../README.md
+:start-after: <!-- start intro -->
+:end-before: <!-- end intro -->
+```
+
+```{include} ../README.md
+:start-after: <!-- start example -->
+:end-before: <!-- end example -->
+```
+
+## Features
+
+```{include} ../README.md
+:start-after: <!-- start features -->
+:end-before: <!-- end features -->
+```
+
+```{toctree}
+:hidden:
+
+quickstart
+usage/index
+```
+```{toctree}
+:hidden:
+:caption: About the Project
+:maxdepth: 1
+
+changelog
+```
---
+++ packages/geomancy/geomancy-0.8.1.tar.gz/geomancy-0.8.1/tests/checks/test_exec.py
@@ -0,0 +1,46 @@
+"""
+Test the CheckExec class
+"""
+from geomancy.checks.exec import CheckExec
+
+
+def test_check_exec_get_current_version_no_pip():
+    """Test the CheckExec get_current_version method"""
+    check = CheckExec(name="python", value="python")
+
+    version = check.get_current_version()
+    assert isinstance(version, tuple)
+    assert version[0] >= 2  # python 2 or later
+
+
+def test_check_exec_exists():
+    """Tests CheckExec checking for an existing and a missing command"""
+    # Should exist
+    check = CheckExec(name="Check Python", value="python3")
+    assert check.check().passed
+
+    # Should not exist
+    check = CheckExec(name="Check Python", value="_miss_ing_")
+    assert not check.check().passed
+
+
+def test_check_exec_version():
+    """Tests CheckExec checking with version number"""
+    # Should be greater than version 2.0
+    check = CheckExec(name="Check Python", value="python>=2.0")
+    assert check.check().passed
+
+    # Should be less than version 1000.
+    check = CheckExec(name="Check Python", value="python>=1000.0")
+    assert not check.check().passed
+
+
+def test_check_exec_missing_version():
+    """Tests CheckExec check with a command that doesn't give a version number"""
+    # Command without a version number
+    check = CheckExec(name="Check ls", value="ls>=2.0")
+    assert not check.check().passed
+
+    # Without a version number, the check passes
+    check = CheckExec(name="Check ls", value="ls")
+    assert check.check().passed
