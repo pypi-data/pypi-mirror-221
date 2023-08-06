7ee9db4d10ae0ea700c0c1d8ac0e792254bda09d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:15 +0000

    Add package chromahacker/0.3.0/chromahacker-0.3.0.tar.gz

---
+++ packages/chromahacker/chromahacker-0.3.0.tar.gz/chromahacker-0.3.0/chromahacker/palettize.py
@@ -0,0 +1,26 @@
+import os
+import io
+
+import cv2
+import numpy as np
+
+from chromahacker.spline import spline_from # hey this line is a palindrome
+from chromahacker.process_image import url_to_image
+
+def palettize(url, output, *args, accurate=False):
+    np_array = url_to_image(url)
+
+    img = cv2.cvtColor(np_array, cv2.COLOR_RGB2GRAY)
+
+    fn = spline_from(*args, accurate=accurate)
+
+    if accurate:
+        display = np.array([[fn(j) for j in i] for i in img])
+    else:
+        display = np.rint(fn(img)).astype(np.uint8)
+
+    img_encode = cv2.imencode('.' + output, display)[1]
+
+    data_encode = np.array(img_encode)
+    byte_encode = data_encode.tobytes()
+    return byte_encode
