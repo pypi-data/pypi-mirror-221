a9e7148beda0e5f7a8681e856a0fe4b2b764c848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:44 +0000

    Add package pkglib35/1.0.0/pkglib35-1.0.0.tar.gz

---
+++ packages/pkglib35/pkglib35-1.0.0.tar.gz/pkglib35-1.0.0/setup.py
@@ -0,0 +1,14 @@
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pkglib35',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[],  # List any dependencies here
+    author='Your Name',
+    author_email='your@email.com',
+    description='A description of your package',
+    url='https://example.com',
+)
