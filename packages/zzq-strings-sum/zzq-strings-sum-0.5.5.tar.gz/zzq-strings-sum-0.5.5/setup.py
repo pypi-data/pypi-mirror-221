c7ecf0edb2f985c6f93849639e5546e6d086e0b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:18 +0000

    Add package zzq-strings-sum/0.5.5/zzq-strings-sum-0.5.5.tar.gz

---
+++ packages/zzq-strings-sum/zzq-strings-sum-0.5.5.tar.gz/zzq-strings-sum-0.5.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='zzq-strings-sum',
+    version='0.5.5',
+    description='A client for Databus API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='zzq',
+    author_email='zhaozhiqiang@vikadata.com',
+    package_dir = {"": "src"},
+    packages = find_packages(where="src"),
+    install_requires=['requests'],
+)
---
+++ packages/zzq-strings-sum/zzq-strings-sum-0.5.5.tar.gz/zzq-strings-sum-0.5.5/src/api/z_test_module.py
@@ -0,0 +1,11 @@
+# mymodule.py
+
+def greeting(name):
+    return f"Hello, {name}!"
+
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
