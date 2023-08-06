339e3630df4f0e08d3cb63b374658c8ccbb9a0eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:04 +0000

    Add package cubicweb-skillmat/1.0.0/cubicweb_skillmat-1.0.0-py3-none-any.whl

---
+++ packages/cubicweb-skillmat/cubicweb_skillmat-1.0.0-py3-none-any.whl/cubicweb_skillmat/__pkginfo__.py
@@ -0,0 +1,27 @@
+# pylint: disable-msg=W0622
+"""cubicweb-skillmat application packaging information"""
+
+modname = 'skillmat'
+distname = 'cubicweb-skillmat'
+
+numversion = (1, 0, 0)
+version = '.'.join(str(num) for num in numversion)
+
+license = 'LGPL'
+description = 'skill matrix component for the CubicWeb framework'
+author = 'Logilab'
+author_email = 'contact@logilab.fr'
+web = 'http://www.cubicweb.org/project/%s' % distname
+
+__depends__ = {'cubicweb': ">=4.0.0,<5.0.0",
+               'cubicweb-web': ">=1.0.0,<2.0.0",
+               'cubicweb-folder': ">=3.0.0,<4.0.0",
+               'cubicweb-comment': ">=3.0.0,<4.0.0",
+               "pandas": ">=1.5.0,<1.6.0"}
+
+classifiers = [
+    'Environment :: Web Environment',
+    'Framework :: CubicWeb',
+    'Programming Language :: Python',
+    'Programming Language :: JavaScript',
+    ]
