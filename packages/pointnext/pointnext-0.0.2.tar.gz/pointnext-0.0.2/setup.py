887404dbc8d5f3ac671ff8029a5f4a91c322c63b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:55 +0000

    Add package pointnext/0.0.2/pointnext-0.0.2.tar.gz

---
+++ packages/pointnext/pointnext-0.0.2.tar.gz/pointnext-0.0.2/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+from torch.utils.cpp_extension import BuildExtension, CppExtension, CUDAExtension, CUDA_HOME
+
+ext_modules = [
+    CUDAExtension(
+        'pointnext._C',
+        [
+            'csrc/pointnet2_api.cpp',
+            'csrc/ball_query.cpp',
+            'csrc/ball_query_gpu.cu',
+            'csrc/group_points.cpp',
+            'csrc/group_points_gpu.cu',
+            'csrc/interpolate.cpp',
+            'csrc/interpolate_gpu.cu',
+            'csrc/sampling.cpp',
+            'csrc/sampling_gpu.cu',
+        ],
+        extra_compile_args={'nvcc': ['-O3']})
+]
+
+setup(
+    name='pointnext',
+    packages=find_packages(exclude=("csrc")),
+    version='0.0.2',
+    license='MIT',
+    description='PointNext - Pytorch',
+    author='Kaidi Shen',
+    url='https://github.com/kentechx/pointnext',
+    long_description_content_type='text/markdown',
+    keywords=[
+        '3D segmentation',
+        '3D classification',
+        'point cloud understanding',
+    ],
+    install_requires=[
+        'torch>=1.10',
+        'einops>=0.6.1',
+        'pykeops>=2.1.2',
+    ],
+    ext_modules=ext_modules,
+    cmdclass={"build_ext": BuildExtension},
+    classifiers=[
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+)
