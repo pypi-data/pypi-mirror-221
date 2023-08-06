7f27ee3c2fc75c06779e24889b2db36295e1dfa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:36 +0000

    Add package x3deep/0.0.1/x3deep-0.0.1.tar.gz

---
+++ packages/x3deep/x3deep-0.0.1.tar.gz/x3deep-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="x3deep",
+    version="0.0.1",
+    license="MIT Licence",
+    author="blink",
+    packages=find_packages(),
+    include_package_data=True,
+)
