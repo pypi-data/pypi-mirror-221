030a5877a4233f29ce6112a9083093ee9b756323
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:56 +0000

    Add package tablemap/0.3.6/tablemap-0.3.6.tar.gz

---
+++ packages/tablemap/tablemap-0.3.6.tar.gz/tablemap-0.3.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+# To upload in pypi, 
+# python -m twine upload dist/*
+
+setup(
+    name='tablemap',
+    version='0.3.6',
+    description='sql not knowing sql',
+    packages=find_packages(),
+    long_description='',
+    url='https://github.com/nalssee/tablemap.git',
+    author='nalssee',
+    author_email='jinisrolling@gmail.com',
+    license='MIT',
+
+)
