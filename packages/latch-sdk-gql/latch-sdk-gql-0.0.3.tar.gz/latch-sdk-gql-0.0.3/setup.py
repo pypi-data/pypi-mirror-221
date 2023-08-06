587d27401a7cf848aa500d20aa73047f87e06a01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:50 +0000

    Add package latch-sdk-gql/0.0.3/latch-sdk-gql-0.0.3.tar.gz

---
+++ packages/latch-sdk-gql/latch-sdk-gql-0.0.3.tar.gz/latch-sdk-gql-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="latch-sdk-gql",
+    version="v0.0.3",
+    author_email="ayush@latch.bio",
+    description="Internal Latch GQL package",
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.8,<3.12",
+    install_requires=[
+        "gql==3.4.0",
+        "graphql-core==3.2.3",
+        "requests-toolbelt==0.10.1",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+)
