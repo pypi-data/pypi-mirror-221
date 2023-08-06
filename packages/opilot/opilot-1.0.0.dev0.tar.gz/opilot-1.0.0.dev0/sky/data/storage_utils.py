cf16647bb5a82326e685068dd6c7af34699e9c5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:33 +0000

    Add package opilot/1.0.0.dev0/opilot-1.0.0.dev0.tar.gz

---
+++ packages/opilot/opilot-1.0.0.dev0.tar.gz/opilot-1.0.0.dev0/sky/data/storage_utils.py
@@ -0,0 +1,51 @@
+"""Utility functions for the storage module."""
+from typing import Any, Dict, List
+
+from sky import sky_logging
+from sky.utils import log_utils
+from sky.utils.cli_utils import status_utils
+
+logger = sky_logging.init_logger(__name__)
+
+
+def format_storage_table(storages: List[Dict[str, Any]],
+                         show_all: bool = False) -> str:
+    """Format the storage table for display.
+
+    Args:
+        storage_table (dict): The storage table.
+
+    Returns:
+        str: The formatted storage table.
+    """
+    storage_table = log_utils.create_table([
+        'NAME',
+        'UPDATED',
+        'STORE',
+        'COMMAND',
+        'STATUS',
+    ])
+
+    for row in storages:
+        launched_at = row['launched_at']
+        if show_all:
+            command = row['last_use']
+        else:
+            command = status_utils.truncate_long_string(
+                row['last_use'], status_utils.COMMAND_TRUNC_LENGTH)
+        storage_table.add_row([
+            # NAME
+            row['name'],
+            # LAUNCHED
+            log_utils.readable_time_duration(launched_at),
+            # CLOUDS
+            ', '.join([s.value for s in row['store']]),
+            # COMMAND,
+            command,
+            # STATUS
+            row['status'].value,
+        ])
+    if storages:
+        return str(storage_table)
+    else:
+        return 'No existing storage.'
