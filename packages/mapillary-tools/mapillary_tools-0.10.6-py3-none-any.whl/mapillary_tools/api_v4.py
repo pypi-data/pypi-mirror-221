ef11b54b33a9a23fc6e9d976543d191f1bdc5422
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:04 +0000

    Add package mapillary-tools/0.10.6/mapillary_tools-0.10.6-py3-none-any.whl

---
+++ packages/mapillary-tools/mapillary_tools-0.10.6-py3-none-any.whl/mapillary_tools/api_v4.py
@@ -0,0 +1,68 @@
+import os
+import sys
+import typing as T
+from typing import Union
+
+import requests
+
+if sys.version_info >= (3, 8):
+    from typing import Literal  # pylint: disable=no-name-in-module
+else:
+    from typing_extensions import Literal
+
+MAPILLARY_CLIENT_TOKEN = os.getenv(
+    "MAPILLARY_CLIENT_TOKEN", "MLY|5675152195860640|6b02c72e6e3c801e5603ab0495623282"
+)
+MAPILLARY_GRAPH_API_ENDPOINT = os.getenv(
+    "MAPILLARY_GRAPH_API_ENDPOINT", "https://graph.mapillary.com"
+)
+REQUESTS_TIMEOUT = 60  # 1 minutes
+
+
+def get_upload_token(email: str, password: str) -> requests.Response:
+    resp = requests.post(
+        f"{MAPILLARY_GRAPH_API_ENDPOINT}/login",
+        params={"access_token": MAPILLARY_CLIENT_TOKEN},
+        json={"email": email, "password": password, "locale": "en_US"},
+        timeout=REQUESTS_TIMEOUT,
+    )
+    resp.raise_for_status()
+    return resp
+
+
+def fetch_organization(
+    user_access_token: str, organization_id: Union[int, str]
+) -> requests.Response:
+    resp = requests.get(
+        f"{MAPILLARY_GRAPH_API_ENDPOINT}/{organization_id}",
+        params={
+            "fields": ",".join(["slug", "description", "name"]),
+        },
+        headers={
+            "Authorization": f"OAuth {user_access_token}",
+        },
+        timeout=REQUESTS_TIMEOUT,
+    )
+    resp.raise_for_status()
+    return resp
+
+
+ActionType = Literal[
+    "upload_started_upload", "upload_finished_upload", "upload_failed_upload"
+]
+
+
+def logging(action_type: ActionType, properties: T.Dict) -> requests.Response:
+    resp = requests.post(
+        f"{MAPILLARY_GRAPH_API_ENDPOINT}/logging",
+        json={
+            "action_type": action_type,
+            "properties": properties,
+        },
+        headers={
+            "Authorization": f"OAuth {MAPILLARY_CLIENT_TOKEN}",
+        },
+        timeout=REQUESTS_TIMEOUT,
+    )
+    resp.raise_for_status()
+    return resp
