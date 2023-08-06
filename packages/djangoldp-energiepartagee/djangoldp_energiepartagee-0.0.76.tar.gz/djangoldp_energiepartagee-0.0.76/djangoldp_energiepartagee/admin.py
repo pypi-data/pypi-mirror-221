76d700327b4174f0d91af61f0d0aa8838debdcfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:27 +0000

    Add package djangoldp-energiepartagee/0.0.76/djangoldp_energiepartagee-0.0.76.tar.gz

---
+++ packages/djangoldp-energiepartagee/djangoldp_energiepartagee-0.0.76.tar.gz/djangoldp_energiepartagee-0.0.76/djangoldp_energiepartagee/admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from djangoldp.admin import DjangoLDPAdmin
+from djangoldp.models import Model
+from djangoldp_energiepartagee.models import Actor, Contribution, Region, College, Regionalnetwork, Interventionzone, Legalstructure, Collegeepa, Paymentmethod, Profile, Integrationstep, Relatedactor
+
+class EPModelAdmin(DjangoLDPAdmin):
+    readonly_fields=('urlid',)
+
+
+class ActorAdmin(EPModelAdmin):
+    list_display = ('shortname', 'longname', 'updatedate', 'createdate')
+    search_fields = ['longname', 'shortname']
+
+
+class RelatedactorAdmin(EPModelAdmin):
+    search_fields = ['actor__longname', 'actor__shortname', 'user__first_name', 'user__last_name', 'user__email']
+
+
+class ContributionAdmin(EPModelAdmin):
+    list_display = ('actor', 'year', 'updatedate', 'createdate')
+    search_fields = ['actor__longname','actor__shortname']
+
+admin.site.register(Actor, ActorAdmin)
+admin.site.register(Contribution, ContributionAdmin)
+admin.site.register(Region, EPModelAdmin)
+admin.site.register(College, EPModelAdmin)
+admin.site.register(Regionalnetwork, EPModelAdmin)
+admin.site.register(Interventionzone, EPModelAdmin)
+admin.site.register(Legalstructure, EPModelAdmin)
+admin.site.register(Collegeepa, EPModelAdmin)
+admin.site.register(Paymentmethod, EPModelAdmin)
+admin.site.register(Profile, EPModelAdmin)
+admin.site.register(Integrationstep, EPModelAdmin)
+admin.site.register(Relatedactor, RelatedactorAdmin)
