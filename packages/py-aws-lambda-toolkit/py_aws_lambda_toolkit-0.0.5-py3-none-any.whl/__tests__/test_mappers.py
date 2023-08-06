47d294d7b461a362627d003d576b09598c1f29e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:06 +0000

    Add package py-aws-lambda-toolkit/0.0.5/py_aws_lambda_toolkit-0.0.5-py3-none-any.whl

---
+++ packages/py-aws-lambda-toolkit/py_aws_lambda_toolkit-0.0.5-py3-none-any.whl/__tests__/test_mappers.py
@@ -0,0 +1,51 @@
+import unittest
+from py_aws_lambda_toolkit.mappers import mapper
+
+
+class TestMappers(unittest.TestCase):
+
+    def test_mapper(self):
+        data = {
+            'name': 'John',
+            'age': 30,
+            'city': 'New York'
+        }
+        fields = ['age', 'city']
+        expected = {'name': 'John'}
+        result = mapper(data, fields)
+        self.assertEqual(expected, result)
+
+    def test_mapper_with_list(self):
+        data = [
+            {
+                'name': 'John',
+                'age': 30,
+                'city': 'New York'
+            },
+            {
+                'name': 'Jane',
+                'age': 25,
+                'city': 'Boston'
+            }
+        ]
+        fields = ['age', 'city']
+        expected = [
+            {
+                'name': 'John'
+            },
+            {
+                'name': 'Jane'
+            }
+        ]
+        result = mapper(data, fields)
+        self.assertEqual(expected, result)
+
+    def test_mapper_with_invalid_data(self):
+        data = 'John'
+        fields = ['age', 'city']
+        with self.assertRaises(TypeError):
+            mapper(data, fields)
+
+
+if __name__ == '__main__':
+    unittest.main()
