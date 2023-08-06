0ff6804b026e843ca472c417d158acd037379c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:09 +0000

    Add package humlab-penelope/0.7.36/humlab_penelope-0.7.36-py3-none-any.whl

---
+++ packages/humlab-penelope/humlab_penelope-0.7.36-py3-none-any.whl/penelope/notebook/token_counts/__init__.py
@@ -0,0 +1,4 @@
+# type: ignore
+
+from .pipeline_gui import TokenCountsGUI
+from .token_count_gui import ComputeOpts, PivotKeySpec, TokenCountGUI
---
+++ packages/humlab-penelope/humlab_penelope-0.7.36-py3-none-any.whl/penelope/topic_modelling/utility.py
@@ -0,0 +1,47 @@
+import glob
+from dataclasses import dataclass
+from os.path import join, split
+
+from penelope import utility as pu
+
+
+def _find_model_options_files(path: str) -> list[str]:
+    return glob.glob(join(path, "**", "model_options.json"), recursive=True)
+
+
+def _find_model_folders(path: str) -> list[str]:
+    return [split(x)[0] for x in _find_model_options_files(path)]
+
+
+@dataclass
+class ModelFolder:
+    folder: str
+    name: str
+    options: dict
+
+    def to_dict(self):
+        return {
+            'folder': self.folder,
+            'name': self.name,
+            'options': self.options,
+        }
+
+
+def find_models(path: str) -> list[ModelFolder]:
+    """Return subfolders to path that contain a computed topic model"""
+    models = [
+        ModelFolder(
+            folder=folder,
+            name=split(folder)[1],
+            options=pu.read_json(join(folder, "model_options.json")),
+        )
+        for folder in _find_model_folders(path)
+    ]
+    return models
+
+
+def find_inferred_topics_folders(folder: str) -> list[str]:
+    """Return inferred data in sub-folders to `folder`"""
+    filenames = glob.glob(join(folder, "**/*document_topic_weights.zip"), recursive=True)
+    folders = [split(filename)[0] for filename in filenames]
+    return folders
