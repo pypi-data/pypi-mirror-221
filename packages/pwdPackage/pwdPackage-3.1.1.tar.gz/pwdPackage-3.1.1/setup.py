58557e76b5a8953836b5591a4932ec44d0248e4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:04 +0000

    Add package pwdPackage/3.1.1/pwdPackage-3.1.1.tar.gz

---
+++ packages/pwdPackage/pwdPackage-3.1.1.tar.gz/pwdPackage-3.1.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+setuptools.setup(
+    name='pwdPackage',
+    version="3.1.1",
+    author="Anant Chaudhary",
+    zip_safe=False,
+    description="password generator package",
+    long_description="A package for generating passwords.",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    package_data={'pwdPackage': ['data/*.csv']},  # Correct the package_data key
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=['pwdPackage'],
+    package_dir={'':'.'},
+    install_requires=[
+        # Add any dependencies here
+    ]
+)
+
+
