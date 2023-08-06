c398e1d26445eb4f2380436291e4f4c60cab8426
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:34 +0000

    Add package shzLib-t2/0.0.2/shzLib_t2-0.0.2.tar.gz

---
+++ packages/shzLib-t2/shzLib_t2-0.0.2.tar.gz/shzLib_t2-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+project_name = 'shzLib_t2'
+
+setup(name=project_name,
+    version='0.0.2',
+    license='MIT License',
+    author='Eliseu Brito',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='eliseubrito776@gmail.com',
+    keywords='shz tools shzlib',
+    description=u'Personal support tools in the development of varied projects',
+    packages=['shzLib_t2'],
+    install_requires=['numpy==1.25.1', 'pandas==2.0.3'],)
