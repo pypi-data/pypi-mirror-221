c601bc624184072b8f3b7ddf55f69dec334e8ffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:16 +0000

    Add package onqlave-python-sdk-pilot/0.0.20/onqlave-python-sdk-pilot-0.0.20.tar.gz

---
+++ packages/onqlave-python-sdk-pilot/onqlave-python-sdk-pilot-0.0.20.tar.gz/onqlave-python-sdk-pilot-0.0.20/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='onqlave-python-sdk-pilot',
+    version='0.0.20',
+    author='Onqlave Pty',
+    author_email='contact@onqlave.com',
+    maintainer='DC',
+    maintainer_email='dc@onqlave.com',
+    description='This Python SDK is designed to help developers easily integrate Onqlave Encryption As A Service into their python backend.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/onqlavelabs/onqlave-python/tree/dev', 
+    # download_url='',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Security',
+    ],
+    project_urls = {
+        # "Home Page":"",
+        "Issue Tracker": "https://github.com/onqlavelabs/onqlave-python/issues",
+        "Source Code": "https://github.com/onqlavelabs/onqlave-python/tree/dev",
+        
+    },
+
+    keywords=['encryption','privacy','sdk'],
+    license='MIT',
+
+    install_requires=requirements
+)
