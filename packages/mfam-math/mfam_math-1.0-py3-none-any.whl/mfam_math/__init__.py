f0a269f882acb2d89af9ada3b5c64a0ead04411e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:55 +0000

    Add package mfam-math/1.0/mfam_math-1.0-py3-none-any.whl

---
+++ packages/mfam-math/mfam_math-1.0-py3-none-any.whl/mfam_math/__init__.py
@@ -0,0 +1,1 @@
+from .math_operat import somar, multiplicar, subtrair, dividir
---
+++ packages/mfam-math/mfam_math-1.0-py3-none-any.whl/mfam_math/math_operat.py
@@ -0,0 +1,15 @@
+def somar(a, b):
+    """ retorna a soma de dois valores"""
+    return a + b
+    
+def multiplicar(a, b):
+    """ retorna a multiplicação de dois valores"""
+    return a * b
+    
+def subtrair(a, b):
+    """retorna a subtração de valor a - b"""
+    return a - b
+ 
+def dividir(a, b):
+    """retorna a divisão de dois numeros"""
+    return a / b
