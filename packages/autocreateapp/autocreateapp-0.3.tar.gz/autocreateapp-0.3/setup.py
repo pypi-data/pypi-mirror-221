1e52a811c82560089d7eec6d360b0187283e80c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:57 +0000

    Add package autocreateapp/0.3/autocreateapp-0.3.tar.gz

---
+++ packages/autocreateapp/autocreateapp-0.3.tar.gz/autocreateapp-0.3/setup.py
@@ -0,0 +1,21 @@
+# setup.py
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name='autocreateapp',
+    version='0.3',  # Update the version number to 0.2 (or any desired version)
+    packages=find_packages(),
+    install_requires=[
+        'Django',
+    ],
+    entry_points={
+        'console_scripts': [
+            'autocreateapp=autocreateapp.create_project:create_django_project',
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Specify the content type as Markdown
+)
