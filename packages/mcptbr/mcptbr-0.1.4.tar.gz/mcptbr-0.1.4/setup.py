a4709e6bc2987de533a7df04ae27a95d47a3803d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:34 +0000

    Add package mcptbr/0.1.4/mcptbr-0.1.4.tar.gz

---
+++ packages/mcptbr/mcptbr-0.1.4.tar.gz/mcptbr-0.1.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mcptbr',
+    version='0.1.4',
+    packages=find_packages(),
+    install_requires=[
+        "mcpi", # Dependências necessárias para sua biblioteca (se houver).
+    ],
+    author='Lucas Pereira',
+    author_email='lucasthe2@gmail.com',
+    description='Biblioteca mc.pi e outras em português.',
+    #url='https://github.com/seu_usuario/minha_biblioteca',
+    license='MIT',  # Substitua pela licença adequada.
+)
