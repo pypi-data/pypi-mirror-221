4f4f1874da471aab880f5a3a777cfac86834b76e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:23 +0000

    Add package pipe-strip/0.1.0/pipe_strip-0.1.0-py3-none-any.whl

---
+++ packages/pipe-strip/pipe_strip-0.1.0-py3-none-any.whl/pipe_strip/__init__.py
@@ -0,0 +1,9 @@
+"""The Pipe Strip, terminal version."""
+
+__author__ = "Isaiah Odhner"
+__copyright__ = "Public Domain"
+__credits__ = ["Isaiah Odhner"]
+__maintainer__ = "Isaiah Odhner"
+__email__ = "isaiahodhner@gmail.com"
+__version__ = "0.1.0"
+__license__ = "CC0"
