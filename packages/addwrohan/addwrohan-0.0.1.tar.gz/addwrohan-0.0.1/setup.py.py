eb731ef34bfda4e33d24448f7cd1354993307527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:44 +0000

    Add package addwrohan/0.0.1/addwrohan-0.0.1.tar.gz

---
+++ packages/addwrohan/addwrohan-0.0.1.tar.gz/addwrohan-0.0.1/setup.py.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'adds'
+
+# Setting up
+setup(
+    name="addwrohan",
+    version=VERSION,
+    author="wrohan",
+    author_email="<mail@wrohan.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
