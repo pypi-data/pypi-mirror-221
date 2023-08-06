fb29878aa6a77de2456b6b56b1efcd26a36de897
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:44 +0000

    Add package webpty/3.8/webpty-3.8-py3-none-any.whl

---
+++ packages/webpty/webpty-3.8-py3-none-any.whl/webpty/__init__.py
@@ -0,0 +1,8 @@
+"""
+A web-based application to access shell & shell based applications via a browser.
+"""
+
+from importlib_metadata import version as im_version
+
+
+__version__ = im_version("webpty")
