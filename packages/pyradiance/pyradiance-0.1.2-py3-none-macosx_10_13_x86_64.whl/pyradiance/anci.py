e035516cbf0e4bce40722dc0da1973e0a175c6b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:22 +0000

    Add package pyradiance/0.1.2/pyradiance-0.1.2-py3-none-macosx_10_13_x86_64.whl

---
+++ packages/pyradiance/pyradiance-0.1.2-py3-none-macosx_10_13_x86_64.whl/pyradiance/anci.py
@@ -0,0 +1,29 @@
+"""
+Auxiliary functions.
+"""
+
+from pathlib import Path
+from functools import wraps
+from subprocess import CalledProcessError
+
+BINPATH = Path(__file__).parent / "bin"
+
+
+def handle_called_process_error(func):
+    """
+    Decorator to handle subprocess.CalledProcessError.
+    """
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            result = func(*args, **kwargs)
+        except CalledProcessError as e:
+            errmsg = e.stderr or b""
+            raise RuntimeError(
+                f"An error occurred with exit code {e.returncode}: {errmsg.decode()}"
+            )
+        else:
+            return result
+
+    return wrapper
