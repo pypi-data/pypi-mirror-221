34c1e412802c5b0d0c19e5a84764f01f54d2b170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:32 +0000

    Add package caikit/0.13.0/caikit-0.13.0-py3-none-any.whl

---
+++ packages/caikit/caikit-0.13.0-py3-none-any.whl/caikit/interfaces/runtime/data_model/training_management.py
@@ -0,0 +1,53 @@
+# Copyright The Caikit Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# First Party
+import alog
+
+# Local
+from caikit.core.data_model import DataObjectBase, TrainingStatus, dataobject
+from caikit.core.toolkit.wip_decorator import Action, WipCategory, work_in_progress
+
+log = alog.use_channel("MDLOPS")
+
+RUNTIME_PACKAGE = "caikit_data_model.runtime"
+
+
+@work_in_progress(action=Action.WARNING, category=WipCategory.BETA)
+@dataobject(RUNTIME_PACKAGE)
+class TrainingInfoRequest(DataObjectBase):
+    training_id: str
+
+
+@work_in_progress(action=Action.WARNING, category=WipCategory.BETA)
+@dataobject(RUNTIME_PACKAGE)
+class TrainingJob(DataObjectBase):
+    training_id: str
+    model_name: str
+
+
+@work_in_progress(action=Action.WARNING, category=WipCategory.BETA)
+@dataobject(RUNTIME_PACKAGE)
+class ModelPointer(DataObjectBase):
+    model_id: str
+
+
+@work_in_progress(action=Action.WARNING, category=WipCategory.BETA)
+@dataobject(RUNTIME_PACKAGE)
+class TrainingInfoResponse(DataObjectBase):
+    training_id: str
+    status: TrainingStatus
+    submission_timestamp: str
+    completion_timestamp: str
+    error_code: str
