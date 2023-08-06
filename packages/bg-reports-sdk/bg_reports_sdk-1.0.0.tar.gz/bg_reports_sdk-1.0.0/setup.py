5207091a553a41d4a4331188bbf76ceac0d60567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:48 +0000

    Add package bg-reports-sdk/1.0.0/bg_reports_sdk-1.0.0.tar.gz

---
+++ packages/bg-reports-sdk/bg_reports_sdk-1.0.0.tar.gz/bg_reports_sdk-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bg_reports_sdk",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=["requests", "python-dotenv", "pytz"],
+    author="Nikita Surov",
+    author_email="n.surov@focus.bi",
+    description="Software development kit for creating reports and their schedulers.",
+    # url="https://github.com/your_username/app",
+)
