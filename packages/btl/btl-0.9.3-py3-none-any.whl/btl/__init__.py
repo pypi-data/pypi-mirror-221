9607eef4c8b42579012167f3e19f6a4ceedfe07b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:49 +0000

    Add package btl/0.9.3/btl-0.9.3-py3-none-any.whl

---
+++ packages/btl/btl-0.9.3-py3-none-any.whl/btl/__init__.py
@@ -0,0 +1,4 @@
+from .db import ToolDB
+from .library import Library
+from .shape import Shape
+from .tool import Tool
---
+++ packages/btl/btl-0.9.3-py3-none-any.whl/btl/params.py
@@ -0,0 +1,43 @@
+import re
+
+class Param(object):
+    name = None
+    label = None
+    unit = ''
+    fmt = '{}'
+    type = str
+    choices = None
+
+    def __init__(self, name=None, unit=None):
+        label = re.sub(r'([A-Z])', r' \1', name or '').strip().capitalize()
+        self.name = self.name if name is None else name
+        self.label = self.label if name is None else label
+        self.unit = self.unit if unit is None else unit
+
+    def format(self, value):
+        unit = ' '+self.unit if self.unit else ''
+        return self.fmt.format(value)+unit
+
+    def validate(self, value):
+        if not isinstance(value, self.type):
+            return False
+        if self.choices is not None and value not in self.choices:
+            return False
+        return True
+
+class BoolParam(Param):
+    type = bool
+
+class IntParam(Param):
+    type = int
+
+class FloatParam(Param):
+    type = float
+
+class DistanceParam(FloatParam):
+    unit = 'mm'
+    fmt = '{}'
+
+class AngleParam(FloatParam):
+    unit = '°'
+    fmt = '{}'
