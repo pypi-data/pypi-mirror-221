8c03c391b334d7b883a9a50d7ddb61810cb769da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:59 +0000

    Add package django-feedback-govuk/0.1.4/django_feedback_govuk-0.1.4.tar.gz

---
+++ packages/django-feedback-govuk/django_feedback_govuk-0.1.4.tar.gz/django_feedback_govuk-0.1.4/django_feedback_govuk/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    path("", views.FeedbackView.as_view(), name="feedback-submit"),
+    path("confirm/", views.feedback_confirm, name="feedback-confirm"),
+    path("listing/", views.FeedbackListingView.as_view(), name="feedback-listing"),
+    # path("submit-ajax", views.AjaxFeedbackView.as_view(), name="feedback-submit-ajax"),
+    # path("confirm-ajax/", views.feedback_confirm_ajax, name="feedback-confirm-ajax"),
+]
