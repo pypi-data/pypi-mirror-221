663a05309359c44e3b81b74dd9a338419fd36daa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:17 +0000

    Add package encord/0.1.89/encord-0.1.89.tar.gz

---
+++ packages/encord/encord-0.1.89.tar.gz/encord-0.1.89/encord/objects/__init__.py
@@ -0,0 +1,13 @@
+from encord.objects.classification import Classification
+from encord.objects.classification_instance import ClassificationInstance
+from encord.objects.common import (
+    ChecklistAttribute,
+    FlatOption,
+    NestableOption,
+    RadioAttribute,
+    Shape,
+    TextAttribute,
+)
+from encord.objects.ontology_labels_impl import LabelRowV2, OntologyStructure
+from encord.objects.ontology_object import Object
+from encord.objects.ontology_object_instance import AnswerForFrames, ObjectInstance
