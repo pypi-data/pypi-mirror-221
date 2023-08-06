241aa357dc7572157a93257edea0e6a4fe7f1281
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:41 +0000

    Add package chiku/0.0.11/chiku-0.0.11-py3-none-any.whl

---
+++ packages/chiku/chiku-0.0.11-py3-none-any.whl/chiku/mpmath/mptaylor.py
@@ -0,0 +1,19 @@
+from mpmath import *
+
+
+class mptaylor:
+
+    def __init__(self, f=sin, p=0, d=5):
+        mp.dps = 15
+        mp.pretty = True
+
+        self.poly = taylor(f, p, d)
+        nprint(chop(self.poly))
+
+
+    def predict(self, x):
+        return polyval(self.poly[::-1], x - p)
+
+
+    def get_d(self, d):
+        return mpf(d)
