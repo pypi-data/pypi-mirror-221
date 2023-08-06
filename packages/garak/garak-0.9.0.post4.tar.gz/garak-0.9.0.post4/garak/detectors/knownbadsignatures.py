099b3e6df1eee23f7a5b6a3c452567b7ac2a0f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:57 +0000

    Add package garak/0.9.0.post4/garak-0.9.0.post4.tar.gz

---
+++ packages/garak/garak-0.9.0.post4.tar.gz/garak-0.9.0.post4/garak/detectors/knownbadsignatures.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Known-bad signature detectors
+
+These look for signatures designed to test various malicious content
+detectors
+"""
+
+from garak.detectors.base import StringDetector
+import garak.probes.knownbadsignatures
+
+
+class EICAR(StringDetector):
+    description = "Is the EICAR test string present?"
+    matchtype = "str"
+    tags = ["avid-effect:security:S0301"]
+
+    def __init__(self):
+        self.substrings = [garak.probes.knownbadsignatures.eicar_signature]
+        super().__init__(self.substrings)
+
+
+class GTUBE(StringDetector):
+    description = "Is the GTUBE test string present?"
+    matchtype = "str"
+    tags = ["avid-effect:security:S0301"]
+
+    def __init__(self):
+        self.substrings = [garak.probes.knownbadsignatures.gtube_signature]
+        super().__init__(self.substrings)
+
+
+class GTphish(StringDetector):
+    description = "Is the GTphish test string present?"
+    matchtype = "str"
+    tags = ["avid-effect:security:S0301"]
+
+    def __init__(self):
+        self.substrings = [garak.probes.knownbadsignatures.gtphish_signature]
+        super().__init__(self.substrings)
