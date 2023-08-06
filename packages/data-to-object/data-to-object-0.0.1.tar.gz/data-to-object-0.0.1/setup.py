c9fa5fedb3de0fe1b6516c9fa60624ce551331bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:57 +0000

    Add package data-to-object/0.0.1/data-to-object-0.0.1.tar.gz

---
+++ packages/data-to-object/data-to-object-0.0.1.tar.gz/data-to-object-0.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="data-to-object",
+    version="0.0.1",
+    author="Original author: Denna Sun, Maintained by Vladimir Podolian",
+    author_email="vladimir.podlyan64@gmail.com",
+    description="This is a simple package that deserializes json to your custom model include custom attributes and child objects.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EnvInc/JsonToModel",
+    packages=[
+        'data_to_object'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
