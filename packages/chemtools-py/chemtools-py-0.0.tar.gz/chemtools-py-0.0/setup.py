1df6979c43d79a5bc5c83b19afb81443a75adeec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:57 +0000

    Add package chemtools-py/0.0/chemtools-py-0.0.tar.gz

---
+++ packages/chemtools-py/chemtools-py-0.0.tar.gz/chemtools-py-0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+setup(
+    name='chemtools-py',
+    version='0.0',    
+    description='A example Python package',
+    long_description="somewhat in the future",
+    url='https://github.com/KerrKerrKerr/chemtools-py',
+    download_url="https://github.com/KerrKerrKerr/chemtools-py.git",
+    author='burntSynaps3',
+    author_email='ustinov2005@example.com',
+    license='BRUH',
+    packages=['chemtools-py'],
+    install_requires=[
+        'requests',
+        'json',
+        'functools',
+        're',
+        'os'
+    ],
+)
