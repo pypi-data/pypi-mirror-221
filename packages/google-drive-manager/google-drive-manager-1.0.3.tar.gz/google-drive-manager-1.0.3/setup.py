59b1069df603cca5a77e2b275fe8fa764b6e357a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:19 +0000

    Add package google-drive-manager/1.0.3/google-drive-manager-1.0.3.tar.gz

---
+++ packages/google-drive-manager/google-drive-manager-1.0.3.tar.gz/google-drive-manager-1.0.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='google-drive-manager',
+    version='1.0.3',
+    author="Guillaume LECOSSOIS",
+    author_email="<guillaume.lecossois@diabeloop.fr>",
+    description='Un gestionnaire de Google Drive',
+    long_description_content_type="text/markdown",
+    long_description="""google-drive-manager is a library designed for interact with Google Drive
+                       in Python. With its simple API, you can manage data from various files and folders in the Diabeloop Google Drive. The list of the methods : 
+                        - get_service
+                        - connect
+                        - getSpreadSheetInfoByID
+                        - listSheets
+                        - getSheetByName
+                        - getAllValuesFromSheet
+                        - getPosDataInCol
+                        - getRowDataFromSheet
+                        - getRangeDataFromSheet
+                        - getColDataFromSheet
+                        - getRangeValueFromSheet
+                        - setColData
+                        - setRangeData
+                        - getFileByID """,
+    packages=find_packages(),
+    install_requires=[
+        "google-api-python-client==2.64.0",
+        "google-auth-oauthlib==0.5.0"
+    ],
+    keywords=['python', 'google', 'drive'],
+    python_requires='>=3.8'
+)
