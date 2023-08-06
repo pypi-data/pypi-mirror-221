eb641867c14d2cbb7487c67e18a702aae40dc467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:34 +0000

    Add package curveengine/1.1.4/curveengine-1.1.4.tar.gz

---
+++ packages/curveengine/curveengine-1.1.4.tar.gz/curveengine-1.1.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='curveengine',
+    version='1.1.4',
+    description='A simple curve bootstraping tool based on ORE/QuantLib',
+    long_description=open('readmepipy.md').read(),
+    author='Jose Melo',
+    package_data={
+        "": ["*.py"]
+    },
+    author_email='jmelo@live.cl',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    python_requires='>=3.10',
+    install_requires=[
+        'open-source-risk-engine']
+)
