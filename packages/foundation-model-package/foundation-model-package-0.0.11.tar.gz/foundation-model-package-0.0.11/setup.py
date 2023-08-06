ac202205e330a4703fc31a856407ae88052aa4ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:51 +0000

    Add package foundation-model-package/0.0.11/foundation-model-package-0.0.11.tar.gz

---
+++ packages/foundation-model-package/foundation-model-package-0.0.11.tar.gz/foundation-model-package-0.0.11/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="foundation-model-package",
+    version="0.0.11",
+    author="Matthew DeGuzman",
+    author_email="t-madeguzman@microsoft.com",
+    description="Package to use Foundation Models in Prompt Flow",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
