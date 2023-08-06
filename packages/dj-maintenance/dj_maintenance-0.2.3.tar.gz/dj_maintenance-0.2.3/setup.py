f9a4bb0086d21506d6476a4305f65af50a90be0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:54 +0000

    Add package dj-maintenance/0.2.3/dj_maintenance-0.2.3.tar.gz

---
+++ packages/dj-maintenance/dj_maintenance-0.2.3.tar.gz/dj_maintenance-0.2.3/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+name = "dj_maintenance",
+version = "0.2.3",
+long_description="dj_maintenance is a Django app to conduct sheduled dj-dj_maintenance on your web application. Detailed documentation is in the 'docs' directory."
+)
