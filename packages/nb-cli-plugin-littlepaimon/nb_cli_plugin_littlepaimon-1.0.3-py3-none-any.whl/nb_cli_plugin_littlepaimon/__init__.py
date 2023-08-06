34daa42ad26da4a51589bc4a6c8518de4311324d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:06 +0000

    Add package nb-cli-plugin-littlepaimon/1.0.3/nb_cli_plugin_littlepaimon-1.0.3-py3-none-any.whl

---
+++ packages/nb-cli-plugin-littlepaimon/nb_cli_plugin_littlepaimon-1.0.3-py3-none-any.whl/nb_cli_plugin_littlepaimon/__init__.py
@@ -0,0 +1,10 @@
+import sys
+
+if sys.version_info < (3, 10):
+    from importlib_metadata import version
+else:
+    from importlib.metadata import version
+try:
+    __version__ = version("nb-cli-plugin-littlepaimon")
+except Exception:
+    __version__ = None
---
+++ packages/nb-cli-plugin-littlepaimon/nb_cli_plugin_littlepaimon-1.0.3-py3-none-any.whl/nb_cli_plugin_littlepaimon/handlers/__init__.py
@@ -0,0 +1,6 @@
+from .cmd import *
+from .download import (
+    download_json as download_json,
+    download_with_bar as download_with_bar,
+)
+from .gocq import download_gocq as download_gocq
