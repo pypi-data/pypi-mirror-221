cf22736c6b26126932b35cee89073faccb9a66bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:56 +0000

    Add package coindicator/2.2.1/coindicator-2.2.1.tar.gz

---
+++ packages/coindicator/coindicator-2.2.1.tar.gz/coindicator-2.2.1/install.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# Install system dependencies
+if [[ $# -eq 0 ]]
+    then
+        echo "Usage: ./install.sh install_dir"
+        echo "E.g. ./install.sh /opt/coindicator"
+        exit 1
+fi
+
+args=("$@")
+
+echo Installing to ${args[0]}
+
+sudo apt-get install python3-venv python3-setuptools-scm python3-wheel python3-gi gir1.2-gtk-3.0 gir1.2-appindicator3-0.1 python3-pip patchelf -y
+
+# some users report requiring libgirepository1.0-dev libdbus-1-dev, libcairo2-dev, build-essential
+# some report having to install a newer version of cmake
+
+# sudo apt purge --auto-remove cmake
+# wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
+# sudo apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main'
+# sudo apt update
+# sudo apt install cmake
+
+# Install python packages
+sudo mkdir ${args[0]} 2>/dev/null
+sudo python3 -m venv ${args[0]}/venv
+source ${args[0]}/venv/bin/activate
+sudo -H -E env PATH=$PATH pip3 install -U coindicator
+
+# Install shortcut
+cat > /tmp/coindicator.desktop << EOL
+
+[Desktop Entry]
+Name=Coindicator
+GenericName=Cryptocoin price ticker
+Comment=Keep track of the cryptocoin prices on various exchanges
+Terminal=false
+Type=Application
+Categories=Utility;
+Keywords=crypto;coin;ticker;price;exchange;
+StartupNotify=false
+Path=${args[0]}/venv/bin
+Exec=coindicator
+Icon=/tmp/logo_248px.png
+X-GNOME-UsesNotifications=true
+EOL
+
+cp ./src/coin/resources/logo_248px.png /tmp
+
+
+desktop-file-install --dir=$HOME/.local/share/applications /tmp/coindicator.desktop
---
+++ packages/coindicator/coindicator-2.2.1.tar.gz/coindicator-2.2.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup_requirements = ["setuptools_scm[toml]>=5", "setuptools>=46.1.0"]
+
+if __name__ == "__main__":
+    try:
+        setup(use_scm_version={"version_scheme": "no-guess-dev"})
+    except:  # noqa
+        print(
+            "\n\nAn error occurred while building the project, "
+            "please ensure you have the most updated version of setuptools, "
+            "setuptools_scm and wheel with:\n"
+            "   pip install -U setuptools setuptools_scm wheel\n\n"
+        )
+        raise
