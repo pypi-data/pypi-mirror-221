16e9959411ffb8214f20781b058660de29f5107e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:17 +0000

    Add package toff/0.1.0/toff-0.1.0-py3-none-any.whl

---
+++ packages/toff/toff-0.1.0-py3-none-any.whl/toff/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+For information of TOFF:
+    Docs: https://TOFF.readthedocs.io/en/latest/
+    Source Code: https://github.com/ale94mleon/TOFF
+"""
+from ._version import __version__, __version_tuple__
+from .utils import *
+__author__ = "Alejandro Martínez León"
+__email__ = "ale94mleon@gmail.com"
