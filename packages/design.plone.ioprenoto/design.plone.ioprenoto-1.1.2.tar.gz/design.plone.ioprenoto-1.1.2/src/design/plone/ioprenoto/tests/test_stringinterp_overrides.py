63036fb2d07618aeb29a821b6467052a9a32f182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:58 +0000

    Add package design.plone.ioprenoto/1.1.2/design.plone.ioprenoto-1.1.2.tar.gz

---
+++ packages/design.plone.ioprenoto/design.plone.ioprenoto-1.1.2.tar.gz/design.plone.ioprenoto-1.1.2/src/design/plone/ioprenoto/tests/test_stringinterp_overrides.py
@@ -0,0 +1,142 @@
+# -*- coding: utf-8 -*-
+from datetime import date
+from plone import api
+from plone.app.testing import (
+    TEST_USER_ID,
+    setRoles,
+)
+from design.plone.ioprenoto.testing import DESIGN_PLONE_IOPRENOTO_FUNCTIONAL_TESTING
+from datetime import datetime
+from plone.registry.interfaces import IRegistry
+from plone.stringinterp.interfaces import IStringSubstitution
+from plone.volto.interfaces import IVoltoSettings
+from redturtle.prenotazioni.adapters.booker import IBooker
+from zope.component import getAdapter
+from zope.component import getUtility
+
+import unittest
+import transaction
+
+
+class TestStringinterpOverrides(unittest.TestCase):
+    layer = DESIGN_PLONE_IOPRENOTO_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.app = self.layer["app"]
+        self.portal = self.layer["portal"]
+        self.portal_url = self.portal.absolute_url()
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+
+        self.folder_prenotazioni = api.content.create(
+            container=self.portal,
+            type="PrenotazioniFolder",
+            title="Prenota foo",
+            description="",
+            daData=date.today(),
+            week_table=[
+                {
+                    "day": "Lunedì",
+                    "morning_start": "0700",
+                    "morning_end": "1000",
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Martedì",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Mercoledì",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Giovedì",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Venerdì",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Sabato",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Domenica",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+            ],
+            booking_types=[
+                {"name": "Type A", "duration": "30"},
+            ],
+            gates=["Gate A"],
+        )
+
+        year = api.content.create(
+            container=self.folder_prenotazioni, type="PrenotazioniYear", title="Year"
+        )
+        week = api.content.create(container=year, type="PrenotazioniWeek", title="Week")
+        self.day_folder = api.content.create(
+            container=week, type="PrenotazioniDay", title="Day"
+        )
+
+        booker = IBooker(self.folder_prenotazioni)
+        self.prenotazione = booker.create(
+            {
+                "booking_date": datetime.now(),
+                "booking_type": "Type A",
+                "title": "foo",
+            }
+        )
+
+        transaction.commit()
+
+    def test_booking_print_url_override(
+        self,
+    ):
+        self.assertEqual(
+            getAdapter(self.prenotazione, IStringSubstitution, "booking_print_url")(),
+            f"{self.portal_url}/prenotazione-appuntamenti-uffici?booking_id={self.prenotazione.UID()}",
+        )
+
+    def test_booking_print_url_with_delete_token_override(
+        self,
+    ):
+        self.assertEqual(
+            getAdapter(
+                self.prenotazione,
+                IStringSubstitution,
+                "booking_print_url_with_delete_token",
+            )(),
+            f"{self.portal_url}/prenotazione-appuntamenti-uffici?booking_id={self.prenotazione.UID()}",
+        )
+
+    def test_booking_print_url_override_with_custom_frontend_domain(
+        self,
+    ):
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(IVoltoSettings, prefix="volto", check=False)
+        settings.frontend_domain = "http://foo.bar"
+
+        self.assertEqual(
+            getAdapter(self.prenotazione, IStringSubstitution, "booking_print_url")(),
+            f"http://foo.bar/prenotazione-appuntamenti-uffici?booking_id={self.prenotazione.UID()}",
+        )
