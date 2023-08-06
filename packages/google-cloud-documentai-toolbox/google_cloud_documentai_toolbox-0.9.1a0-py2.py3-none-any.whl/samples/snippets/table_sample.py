6c0509931348de95ffee7b296cffa5a3dc3dfef3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:04 +0000

    Add package google-cloud-documentai-toolbox/0.9.1a0/google_cloud_documentai_toolbox-0.9.1a0-py2.py3-none-any.whl

---
+++ packages/google-cloud-documentai-toolbox/google_cloud_documentai_toolbox-0.9.1a0-py2.py3-none-any.whl/samples/snippets/table_sample.py
@@ -0,0 +1,40 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+# [START documentai_toolbox_table]
+
+from google.cloud.documentai_toolbox import document
+
+# TODO(developer): Uncomment these variables before running the sample.
+# Given a local document.proto or sharded document.proto in path
+# document_path = "path/to/local/document.json"
+# output_file_prefix = "output/table"
+
+
+def table_sample(document_path: str, output_file_prefix: str) -> None:
+    wrapped_document = document.Document.from_document_path(document_path=document_path)
+
+    print("Tables in Document")
+    for page in wrapped_document.pages:
+        for table_index, table in enumerate(page.tables):
+            print(table.to_dataframe())
+            # Write table to CSV file
+            output_file = f"{output_file_prefix}-{page.page_number}-{table_index}.csv"
+            with open(output_file, "w", encoding="utf-8") as f:
+                f.write(table.to_csv())
+
+
+# [END documentai_toolbox_table]
