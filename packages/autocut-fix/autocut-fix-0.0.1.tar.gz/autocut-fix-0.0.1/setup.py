01972eb580e0b9bca32f0eed533c95b78ac6e98b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:15 +0000

    Add package autocut-fix/0.0.1/autocut-fix-0.0.1.tar.gz

---
+++ packages/autocut-fix/autocut-fix-0.0.1.tar.gz/autocut-fix-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+requirements = [
+    "srt",
+    "moviepy",
+    "opencc-python-reimplemented",
+    "torchaudio",
+    "parameterized",
+    "openai-whisper",
+    "tqdm",
+]
+
+
+setup(
+    name="autocut-fix",
+    long_description_content_type='text/markdown',
+    install_requires=requirements,
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "autocut-fix = autocut.main:main",
+        ]
+    },
+)
