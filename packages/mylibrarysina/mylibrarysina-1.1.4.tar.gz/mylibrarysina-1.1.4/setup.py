f7c37b8578bf857e196076575db26c82d56f70b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:54 +0000

    Add package mylibrarysina/1.1.4/mylibrarysina-1.1.4.tar.gz

---
+++ packages/mylibrarysina/mylibrarysina-1.1.4.tar.gz/mylibrarysina-1.1.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='mylibrarysina',
+    version='1.1.4',
+    description='test',
+    author='Sina',
+    author_email='sinasohrab84@gmail.com',
+    packages=[],
+    install_requires=[],
+    classifiers=[
+    ],
+    python_requires='>=3.6',
+    url='https://github.com/SinaSohrab',
+    
+)
