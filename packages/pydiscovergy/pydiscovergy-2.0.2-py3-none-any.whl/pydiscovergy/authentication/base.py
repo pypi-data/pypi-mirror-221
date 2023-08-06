bb9219da0d202f085cafcb25602106f461c61db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:02 +0000

    Add package pydiscovergy/2.0.2/pydiscovergy-2.0.2-py3-none-any.whl

---
+++ packages/pydiscovergy/pydiscovergy-2.0.2-py3-none-any.whl/pydiscovergy/authentication/base.py
@@ -0,0 +1,14 @@
+"""Base authentication module for Discovergy API."""
+from abc import ABC, abstractmethod
+
+from httpx import AsyncClient
+
+
+class BaseAuthentication(ABC):
+    """Interface class for authentication classes."""
+
+    @abstractmethod
+    async def get_client(
+        self, email: str, password: str, timeout: int, httpx_client: AsyncClient = None
+    ) -> AsyncClient:
+        """Function that needed to be implemented by authentication modules."""
