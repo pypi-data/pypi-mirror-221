3b27c9a81be8c88de48c5771740efeda80754346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:58 +0000

    Add package astroQTpy/0.1.2/astroQTpy-0.1.2-py3-none-any.whl

---
+++ packages/astroQTpy/astroQTpy-0.1.2-py3-none-any.whl/astroqtpy/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.1.2"
+
+## TO DO:
+##     - update readthedocs website
+
+##     - email Kristo
+##     - advanced tutorial on defining a custion class
+##     - injection/recovery class
+
+    
