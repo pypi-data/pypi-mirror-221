f7174a8ea37f3edf71a4def37a9b0251a0433e04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:21 +0000

    Add package milho-multi-sqlite3-manager/0.4.0/milho_multi_sqlite3_manager-0.4.0-py3-none-any.whl

---
+++ packages/milho-multi-sqlite3-manager/milho_multi_sqlite3_manager-0.4.0-py3-none-any.whl/multisqlite3manager/__init__.py
@@ -0,0 +1,2 @@
+import multisqlite3manager.sqlite3_manager as sqlite3_manager
+import multisqlite3manager.file_manager as file_manager
---
+++ packages/milho-multi-sqlite3-manager/milho_multi_sqlite3_manager-0.4.0-py3-none-any.whl/multisqlite3manager/sqlite3_manager/__init__.py
@@ -0,0 +1,11 @@
+from multisqlite3manager.sqlite3_manager.sqlite3_manager import (
+    create_connection,
+    print_databases,
+    print_tables,
+    create_connection_in_memory,
+    from_sql_to_dataframe,
+    load_dataframe_in_disk,
+    load_dataframe_in_memory,
+    MULTISQLITE3MANAGER_SQLITE_FOLDER_PATH_DEFAULT,
+    MULTISQLITE3MANAGER_SQLITE_FOLDER_PATH_KEY
+)
