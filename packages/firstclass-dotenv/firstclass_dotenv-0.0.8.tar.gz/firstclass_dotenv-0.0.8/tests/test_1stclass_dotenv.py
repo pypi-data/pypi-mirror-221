5d691e3bfdddfea0e9d2ecd6a081c9180c41ed6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:18 +0000

    Add package firstclass-dotenv/0.0.8/firstclass_dotenv-0.0.8.tar.gz

---
+++ packages/firstclass-dotenv/firstclass_dotenv-0.0.8.tar.gz/firstclass_dotenv-0.0.8/tests/test_1stclass_dotenv.py
@@ -0,0 +1,33 @@
+import unittest
+import os
+from firstclass_dotenv import Dotenv
+from pprint import pprint 
+
+class Test1stclassDotenv(unittest.TestCase):
+  def test_init(self):
+    print("test __init__")
+    dotenv = Dotenv()
+    dotenv.load()
+    assert os.environ["envfile"] == ".env", ".env file is missing"
+
+  def test_setDotenv(self):
+    print("test setDotenv()")
+    dotenv = Dotenv()
+    dotenv.load(os.path.dirname(os.path.abspath(__file__)) + "/.env.specified1")
+    assert os.environ["envfile"]==".env.specified1", ".env.specified1 env file is missing"
+
+  def test_setDotenv_by_contructor(self):
+    print("test setDotenv_by_contructor")
+    dotenv = Dotenv(os.path.dirname(os.path.abspath(__file__)) + "/.env.specified2")
+    dotenv.load()
+    assert os.environ["envfile"] == ".env.specified2", ".env.specified2 env file is missing"
+
+
+  def test_setDotenv_by_contructor2(self):
+    print("test setDotenv_by_contructor")
+    dotenv = Dotenv(os.path.dirname(os.path.abspath(__file__)) + "/.env.specified3")
+    dotenv.load()
+    assert os.environ["envfile"] == ".env.specified#3", ".env.specified3 env file is missing"
+
+if __name__ == "__main__":
+  unittest.main()
