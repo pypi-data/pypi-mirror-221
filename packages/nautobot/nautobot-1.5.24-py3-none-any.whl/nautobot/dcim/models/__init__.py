37ca0e1255bebc992e3d77e9f14d99f57b0201f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:05 +0000

    Add package nautobot/1.5.24/nautobot-1.5.24-py3-none-any.whl

---
+++ packages/nautobot/nautobot-1.5.24-py3-none-any.whl/nautobot/dcim/models/__init__.py
@@ -0,0 +1,72 @@
+from .cables import Cable, CablePath
+from .device_component_templates import (
+    ConsolePortTemplate,
+    ConsoleServerPortTemplate,
+    DeviceBayTemplate,
+    FrontPortTemplate,
+    InterfaceTemplate,
+    PowerOutletTemplate,
+    PowerPortTemplate,
+    RearPortTemplate,
+)
+from .device_components import (
+    BaseInterface,
+    CableTermination,
+    ConsolePort,
+    ConsoleServerPort,
+    DeviceBay,
+    FrontPort,
+    Interface,
+    InventoryItem,
+    PathEndpoint,
+    PowerOutlet,
+    PowerPort,
+    RearPort,
+)
+from .devices import Device, DeviceRedundancyGroup, DeviceRole, DeviceType, Manufacturer, Platform, VirtualChassis
+from .locations import Location, LocationType
+from .power import PowerFeed, PowerPanel
+from .racks import Rack, RackGroup, RackReservation, RackRole
+from .sites import Region, Site
+
+__all__ = (
+    "BaseInterface",
+    "Cable",
+    "CablePath",
+    "CableTermination",
+    "ConsolePort",
+    "ConsolePortTemplate",
+    "ConsoleServerPort",
+    "ConsoleServerPortTemplate",
+    "Device",
+    "DeviceBay",
+    "DeviceBayTemplate",
+    "DeviceRedundancyGroup",
+    "DeviceRole",
+    "DeviceType",
+    "FrontPort",
+    "FrontPortTemplate",
+    "Interface",
+    "InterfaceTemplate",
+    "InventoryItem",
+    "Location",
+    "LocationType",
+    "Manufacturer",
+    "PathEndpoint",
+    "Platform",
+    "PowerFeed",
+    "PowerOutlet",
+    "PowerOutletTemplate",
+    "PowerPanel",
+    "PowerPort",
+    "PowerPortTemplate",
+    "Rack",
+    "RackGroup",
+    "RackReservation",
+    "RackRole",
+    "RearPort",
+    "RearPortTemplate",
+    "Region",
+    "Site",
+    "VirtualChassis",
+)
