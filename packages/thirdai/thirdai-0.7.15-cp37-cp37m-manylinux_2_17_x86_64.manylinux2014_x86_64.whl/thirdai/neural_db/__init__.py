62acce9f1269b98f36dc62efb65ce89f58cedc4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:27 +0000

    Add package thirdai/0.7.15/thirdai-0.7.15-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/thirdai/thirdai-0.7.15-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/thirdai/neural_db/__init__.py
@@ -0,0 +1,17 @@
+try:
+    from . import parsing_utils
+    from .documents import (
+        CSV,
+        DOCX,
+        PDF,
+        URL,
+        Document,
+        Reference,
+        SentenceLevelDOCX,
+        SentenceLevelPDF,
+    )
+    from .neural_db import CancelState, NeuralDB, Strength, Sup
+except ImportError as error:
+    raise ImportError(
+        "To use thirdai.neural_db, please install the additional dependencies by running 'pip install thirdai[neural_db]'"
+    )
