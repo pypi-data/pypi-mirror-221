adbf26ec7c3b2174892a187c4025390df1cf8e89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:04 +0000

    Add package CaptchaCracker/0.0.7/CaptchaCracker-0.0.7.tar.gz

---
+++ packages/CaptchaCracker/CaptchaCracker-0.0.7.tar.gz/CaptchaCracker-0.0.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CaptchaCracker",
+    version="0.0.7",
+    license='MIT',
+    author="Wooil Jeong",
+    author_email="wooil@kakao.com",
+    description="CaptchaCracker is an open source Python library that provides functions to create and apply deep learning models for Captcha Image recognition.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/WooilJeong/CaptchaCracker",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
