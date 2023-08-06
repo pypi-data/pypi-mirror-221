0c54992943f95fa2eb295a30abdee85555567203
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:42 +0000

    Add package baby-code/0.0.4/baby-code-0.0.4.tar.gz

---
+++ packages/baby-code/baby-code-0.0.4.tar.gz/baby-code-0.0.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='baby-code',
+    version='0.0.4',
+    packages=find_packages(),
+    install_requires=[
+        'Flask',
+        'langchain',
+        'llama-cpp-python',
+        'flask_cors',
+    ],
+    entry_points={
+        'console_scripts': [
+            'baby-code = baby_code.engine:run_llm',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
