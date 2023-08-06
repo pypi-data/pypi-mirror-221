6d9b11ac764bcd178579ad4c0c02c7024925023b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:38 +0000

    Add package red_utils/0.1.6/red_utils-0.1.6.tar.gz

---
+++ packages/red_utils/red_utils-0.1.6.tar.gz/red_utils-0.1.6/red_utils/loguru_utils/__init__.py
@@ -0,0 +1,24 @@
+## Import default constants
+from __future__ import annotations
+
+from .src import (
+    LogLevel,
+    add_sink,
+    default_app_log_file_sink,
+    default_color_fmt,
+    default_error_log_file_sink,
+    default_fmt,
+    default_log_dir,
+    default_sinks,
+    default_stderr_color_sink,
+    default_stderr_sink,
+    default_stdout_color_sink,
+    default_stdout_sink,
+    default_trace_log_file_sink,
+    init_logger,
+    log_levels,
+    valid_compression_strs,
+    validate_compression_str,
+    validate_level,
+    validate_logger,
+)
---
+++ packages/red_utils/red_utils-0.1.6.tar.gz/red_utils-0.1.6/red_utils/time_utils/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from .src import (
+    datetime_as_dt,
+    datetime_as_str,
+    default_format,
+    get_ts,
+    twelve_hour_format,
+)
