d97ea7634bb1579897e1aa07ab6930f778fc096b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:37 +0000

    Add package echofish-aws-resample-and-write-to-zarr-store-lambda/1.0.0.dev20230727004630/echofish-aws-resample-and-write-to-zarr-store-lambda-1.0.0.dev20230727004630.tar.gz

---
+++ packages/echofish-aws-resample-and-write-to-zarr-store-lambda/echofish-aws-resample-and-write-to-zarr-store-lambda-1.0.0.dev20230727004630.tar.gz/echofish-aws-resample-and-write-to-zarr-store-lambda-1.0.0.dev20230727004630/src/echofish_aws_resample_and_write_to_zarr_store_lambda/s3fs_operations.py
@@ -0,0 +1,37 @@
+# s3fs_operations.py
+import os
+import s3fs
+
+
+class S3FSOperations:
+    #####################################################################
+    def __get_s3_fs(
+            self,
+            access_key_id=None,
+            secret_access_key=None
+    ):
+        if access_key_id:
+            s3_fs = s3fs.S3FileSystem(
+                key=access_key_id,
+                secret=secret_access_key
+            )
+            print('NODD Authenticated w passed credentials')
+        else:
+            s3_fs = s3fs.S3FileSystem()
+            print('NODD Authenticated wo passed credentials')
+        return s3_fs
+
+    #####################################################################
+    def s3_map(
+            self,
+            s3_zarr_store_path,
+            access_key_id=None,
+            secret_access_key=None,
+    ):
+        s3_fs = self.__get_s3_fs(
+            access_key_id=access_key_id,
+            secret_access_key=secret_access_key
+        )
+        return s3fs.S3Map(root=s3_zarr_store_path, s3=s3_fs, check=True)
+
+    #####################################################################
