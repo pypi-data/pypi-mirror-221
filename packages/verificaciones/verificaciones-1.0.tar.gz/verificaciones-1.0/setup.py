518e7791bcff48db87fc7c2e328d8f22d3d29b45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:29 +0000

    Add package verificaciones/1.0/verificaciones-1.0.tar.gz

---
+++ packages/verificaciones/verificaciones-1.0.tar.gz/verificaciones-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'verificaciones',
+  packages = find_packages(), 
+  version = '1.0',
+  description = 'Verificaciones al usuario',
+  
+)
