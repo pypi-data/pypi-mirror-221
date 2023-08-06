2d6a3502ee6fe9e5d16f2cb41b398cac4edcb896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:56 +0000

    Add package gs-sql/0.0/gs_sql-0.0.tar.gz

---
+++ packages/gs-sql/gs_sql-0.0.tar.gz/gs_sql-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gs_sql',
+      version='0.0',
+      description='Ability to use SQL sources when working with GoogleSheetAPI',
+      packages=['gs_sql'],
+      author_email='bondareff7@gmail.com',
+      zip_safe=False)
