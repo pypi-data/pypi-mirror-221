3dce8b98caee181add345f16da7652d57d1eb1ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:55 +0000

    Add package hlk-package/1.0.0/hlk_package-1.0.0.tar.gz

---
+++ packages/hlk-package/hlk_package-1.0.0.tar.gz/hlk_package-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hlk_package',
+    version='1.0.0',
+    description='何林凯的测试第三方库',
+    author='helenlinken',
+    author_email='1264510459@qq.com',
+    packages=find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
