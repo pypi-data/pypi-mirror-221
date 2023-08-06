b835e6c075b55e78fa7821c67c15cde7eb9df7d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:37 +0000

    Add package shz-geo/0.0.2/shz_geo-0.0.2.tar.gz

---
+++ packages/shz-geo/shz_geo-0.0.2.tar.gz/shz_geo-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+project_name = 'shz_geo'
+
+setup(name=project_name,
+    version='0.0.2',
+    license='MIT License',
+    author='Eliseu Brito',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='eliseubrito776@gmail.com',
+    keywords='shz geo shz_geo shzgeo',
+    description=u'Personal support tools in the development of geospatial projects',
+    packages=['shz_geo'],
+    install_requires=['modis_tools==1.1.1'],)
