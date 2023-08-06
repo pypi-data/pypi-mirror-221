af54a2037eab4aa0c5cb3324a46b7627df9d4c38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:33 +0000

    Add package deforum/0.1.3/deforum-0.1.3.tar.gz

---
+++ packages/deforum/deforum-0.1.3.tar.gz/deforum-0.1.3/tests/test.py
@@ -0,0 +1,11 @@
+from deforum import Deforum
+
+# Create an instance of the Deforum class
+dd = Deforum()
+
+# Use dd instance to call generate function with the values prompt, max_frames, and strength
+dd.generate(
+    prompt="Cat Sushi",
+    max_frames=80,
+    strength=0.5,
+)
