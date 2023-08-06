671e58c7c840343a36c1e91299acb7a83aedbe6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:26 +0000

    Add package odoo14-addon-ssi-m2o-configurator-mixin/14.0.2.0.0/odoo14_addon_ssi_m2o_configurator_mixin-14.0.2.0.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-m2o-configurator-mixin/odoo14_addon_ssi_m2o_configurator_mixin-14.0.2.0.0-py3-none-any.whl/odoo/addons/ssi_m2o_configurator_mixin/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "Many2one Configurator Mixin",
+    "version": "14.0.2.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "OpenSynergy Indonesia, PT. Simetri Sinergi Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        "base",
+    ],
+    "data": [],
+}
