d130505844c6631c5a227ba84d76bf51b0d70a7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:15 +0000

    Add package pe/0.5.0/pe-0.5.0.tar.gz

---
+++ packages/pe/pe-0.5.0.tar.gz/pe-0.5.0/test/test_regression.py
@@ -0,0 +1,29 @@
+
+import pytest
+
+import pe
+
+
+@pytest.mark.parametrize('parser', ['packrat', 'machine', 'machine-python'])
+def test_capture_choice(parser):
+    assert pe.match(r'~("a" / "b" / "c")', 'c', parser=parser) is not None
+    assert pe.match(r'~("a" / "b" / "c")', 'b', parser=parser) is not None
+    assert pe.match(r'~("a" / "b" / "c")', 'a', parser=parser) is not None
+
+
+@pytest.mark.parametrize('parser', ['packrat', 'machine', 'machine-python'])
+def test_multi_range_charclass(parser):
+    p = pe.compile(
+        r'''
+        Start    <- ["] CHAR* ["]
+        CHAR     <- [ !#-\[\]-\U0010ffff]
+        ''',
+        parser=parser,
+    )
+    assert p.match('""') is not None
+    assert p.match('"a"') is not None
+    assert p.match('"ab"') is not None
+    assert p.match('"1a"') is not None
+    assert p.match('"1"') is not None
+    assert p.match('"\U0010ffff"') is not None
+    assert p.match('"a1"') is not None
