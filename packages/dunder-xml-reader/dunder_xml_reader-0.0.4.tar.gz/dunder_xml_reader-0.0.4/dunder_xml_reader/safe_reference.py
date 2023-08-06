b0fbafd1d87408129fa302a196ab916f6de3d06b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:50 +0000

    Add package dunder-xml-reader/0.0.4/dunder_xml_reader-0.0.4.tar.gz

---
+++ packages/dunder-xml-reader/dunder_xml_reader-0.0.4.tar.gz/dunder_xml_reader-0.0.4/dunder_xml_reader/safe_reference.py
@@ -0,0 +1,56 @@
+"""A None-guarding safe referencer"""
+import sys
+
+
+class SafeReference:
+
+    def __init__(self, object, default):
+        self.object = object
+        self.default = default
+
+    def __getattr__(self, item):
+        try:
+            return SafeReference(getattr(self.object, item), self.default)
+        except:
+            if self.object is not self.default:
+                print(f"SafeReference: Dereference fault: {self.object.__class__.__name__}.{item}", file=sys.stderr)
+            return SafeReference(self.default, self.default)
+
+    class Iterator:
+        def __init__(self, obj):
+            self.obj = obj
+            self.max_index = len(obj)
+            self.current_index = 0
+
+        def __next__(self):
+            if self.current_index < self.max_index:
+                self.current_index += 1
+                return self.obj[self.current_index-1]
+            raise StopIteration
+
+    def __iter__(self):
+        return SafeReference.Iterator(self)
+
+    def __getitem__(self, item):
+        try:
+            return SafeReference(self.object.__getitem__(item), self.default)
+        except:
+            return SafeReference(self.default, self.default)
+
+    def __len__(self):
+        return len(self.object)
+
+    def __repr__(self):
+        return str(self.object)
+
+    def __bool__(self):
+        return bool(self.object)
+
+    def __eq__(self, other):
+        return other == self.object
+
+    def __call__(self, *args, **kwargs):
+        try:
+            return SafeReference(self.object(*args, *kwargs), self.default)
+        except TypeError:
+            return SafeReference(self.default, self.default)
