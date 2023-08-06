4f2f491d564cb4ba54725d6223a343e7e6e46b4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:48 +0000

    Add package CXRunner/1.0.6/CXRunner-1.0.6-py3-none-any.whl

---
+++ packages/CXRunner/CXRunner-1.0.6-py3-none-any.whl/CXRunner/__init__.py
@@ -0,0 +1,13 @@
+# URL: https://github.com/saasaa831/CXRunner
+
+from .xmlrunner.runner import XMLTestRunner
+from .htmlrunner.runner import HTMLTestRunner
+from ._email import SMTP
+from .config import label
+
+
+__author__ = "mastersaa"
+
+__version__ = "1.0.6"
+
+__description__ = "Unittest-based HTML test report."
