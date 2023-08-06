325193397808fe2be3216336b089248d80068141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:22 +0000

    Add package dominx/0.1.0/dominx-0.1.0.tar.gz

---
+++ packages/dominx/dominx-0.1.0.tar.gz/dominx-0.1.0/setup.py
@@ -0,0 +1,39 @@
+import glob
+import os
+import shutil
+
+from setuptools import Command, setup
+
+
+class clean(Command):
+    user_options = []
+    CLEAN_DIRS = [
+        "./build",
+        "./dist",
+        *[dir for dir in glob.glob("./**/*.egg-info", recursive=True)],
+        *[dir for dir in glob.glob("./**/__pycache__", recursive=True)],
+    ]
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        removed_dirs = 0
+        removed_files = 0
+        for dir in self.CLEAN_DIRS:
+            dir = os.path.relpath(dir)
+            for path in glob.glob(os.path.join(dir, "**/*"), recursive=True):
+                print("removing", path)
+                removed_files += 1
+            for path in glob.glob(dir):
+                print("removing", path)
+                removed_dirs += 1
+            if os.path.exists(dir):
+                shutil.rmtree(dir)
+        print(f"removed {removed_files} files and {removed_dirs} directories")
+
+
+setup(cmdclass={"clean": clean})
