52e01725aa0ce54d34403f5741acd3e7da22b141
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:57 +0000

    Add package pytools/2023.1.1/pytools-2023.1.1.tar.gz

---
+++ packages/pytools/pytools-2023.1.1.tar.gz/pytools-2023.1.1/test/test_persistent_dict.py
@@ -0,0 +1,387 @@
+import shutil
+import sys  # noqa
+import tempfile
+from dataclasses import dataclass
+from enum import Enum, IntEnum
+
+import pytest
+
+from pytools.persistent_dict import (
+    CollisionWarning, KeyBuilder, NoSuchEntryError, PersistentDict,
+    ReadOnlyEntryError, WriteOncePersistentDict)
+from pytools.tag import Tag, tag_dataclass
+
+
+# {{{ type for testing
+
+class PDictTestingKeyOrValue:
+
+    def __init__(self, val, hash_key=None):
+        self.val = val
+        if hash_key is None:
+            hash_key = val
+        self.hash_key = hash_key
+
+    def __getstate__(self):
+        return {"val": self.val, "hash_key": self.hash_key}
+
+    def __eq__(self, other):
+        return self.val == other.val
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+    def update_persistent_hash(self, key_hash, key_builder):
+        key_builder.rec(key_hash, self.hash_key)
+
+    def __repr__(self):
+        return "PDictTestingKeyOrValue(val={!r},hash_key={!r})".format(
+                self.val, self.hash_key)
+
+    __str__ = __repr__
+
+# }}}
+
+
+@tag_dataclass
+class SomeTag(Tag):
+    value: str
+
+
+class MyEnum(Enum):
+    YES = 1
+    NO = 2
+
+
+class MyIntEnum(IntEnum):
+    YES = 1
+    NO = 2
+
+
+@dataclass
+class MyStruct:
+    name: str
+    value: int
+
+
+def test_persistent_dict_storage_and_lookup():
+    try:
+        tmpdir = tempfile.mkdtemp()
+        pdict = PersistentDict("pytools-test", container_dir=tmpdir)
+
+        from random import randrange
+
+        def rand_str(n=20):
+            return "".join(
+                    chr(65+randrange(26))
+                    for i in range(n))
+
+        keys = [
+                (randrange(2000)-1000, rand_str(), None, SomeTag(rand_str()),
+                    frozenset({"abc", 123}))
+                for i in range(20)]
+        values = [randrange(2000) for i in range(20)]
+
+        d = dict(zip(keys, values))
+
+        # {{{ check lookup
+
+        for k, v in zip(keys, values):
+            pdict[k] = v
+
+        for k, v in d.items():
+            assert d[k] == pdict[k]
+            assert v == pdict[k]
+
+        # }}}
+
+        # {{{ check updating
+
+        for k, v in zip(keys, values):
+            pdict[k] = v + 1
+
+        for k, v in d.items():
+            assert d[k] + 1 == pdict[k]
+            assert v + 1 == pdict[k]
+
+        # }}}
+
+        # {{{ check store_if_not_present
+
+        for k, _ in zip(keys, values):
+            pdict.store_if_not_present(k, d[k] + 2)
+
+        for k, v in d.items():
+            assert d[k] + 1 == pdict[k]
+            assert v + 1 == pdict[k]
+
+        pdict.store_if_not_present(2001, 2001)
+        assert pdict[2001] == 2001
+
+        # }}}
+
+        # {{{ check dataclasses
+
+        for v in [17, 18]:
+            key = MyStruct("hi", v)
+            pdict[key] = v
+
+            # reuse same key, with stored hash
+            assert pdict[key] == v
+
+        with pytest.raises(NoSuchEntryError):
+            pdict[MyStruct("hi", 19)]
+
+        for v in [17, 18]:
+            # make new key instances
+            assert pdict[MyStruct("hi", v)] == v
+
+        # }}}
+
+        # {{{ check enums
+
+        pdict[MyEnum.YES] = 1
+        with pytest.raises(NoSuchEntryError):
+            pdict[MyEnum.NO]
+        assert pdict[MyEnum.YES] == 1
+
+        pdict[MyIntEnum.YES] = 12
+        with pytest.raises(NoSuchEntryError):
+            pdict[MyIntEnum.NO]
+        assert pdict[MyIntEnum.YES] == 12
+
+        # }}}
+
+        # check not found
+
+        with pytest.raises(NoSuchEntryError):
+            pdict.fetch(3000)
+
+    finally:
+        shutil.rmtree(tmpdir)
+
+
+def test_persistent_dict_deletion():
+    try:
+        tmpdir = tempfile.mkdtemp()
+        pdict = PersistentDict("pytools-test", container_dir=tmpdir)
+
+        pdict[0] = 0
+        del pdict[0]
+
+        with pytest.raises(NoSuchEntryError):
+            pdict.fetch(0)
+
+        with pytest.raises(NoSuchEntryError):
+            del pdict[1]
+
+    finally:
+        shutil.rmtree(tmpdir)
+
+
+def test_persistent_dict_synchronization():
+    try:
+        tmpdir = tempfile.mkdtemp()
+        pdict1 = PersistentDict("pytools-test", container_dir=tmpdir)
+        pdict2 = PersistentDict("pytools-test", container_dir=tmpdir)
+
+        # check lookup
+        pdict1[0] = 1
+        assert pdict2[0] == 1
+
+        # check updating
+        pdict1[0] = 2
+        assert pdict2[0] == 2
+
+        # check deletion
+        del pdict1[0]
+        with pytest.raises(NoSuchEntryError):
+            pdict2.fetch(0)
+
+    finally:
+        shutil.rmtree(tmpdir)
+
+
+def test_persistent_dict_cache_collisions():
+    try:
+        tmpdir = tempfile.mkdtemp()
+        pdict = PersistentDict("pytools-test", container_dir=tmpdir)
+
+        key1 = PDictTestingKeyOrValue(1, hash_key=0)
+        key2 = PDictTestingKeyOrValue(2, hash_key=0)
+
+        pdict[key1] = 1
+
+        # check lookup
+        with pytest.warns(CollisionWarning):
+            with pytest.raises(NoSuchEntryError):
+                pdict.fetch(key2)
+
+        # check deletion
+        with pytest.warns(CollisionWarning):
+            with pytest.raises(NoSuchEntryError):
+                del pdict[key2]
+
+        # check presence after deletion
+        assert pdict[key1] == 1
+
+        # check store_if_not_present
+        pdict.store_if_not_present(key2, 2)
+        assert pdict[key1] == 1
+
+    finally:
+        shutil.rmtree(tmpdir)
+
+
+def test_persistent_dict_clear():
+    try:
+        tmpdir = tempfile.mkdtemp()
+        pdict = PersistentDict("pytools-test", container_dir=tmpdir)
+
+        pdict[0] = 1
+        pdict.fetch(0)
+        pdict.clear()
+
+        with pytest.raises(NoSuchEntryError):
+            pdict.fetch(0)
+
+    finally:
+        shutil.rmtree(tmpdir)
+
+
+@pytest.mark.parametrize("in_mem_cache_size", (0, 256))
+def test_write_once_persistent_dict_storage_and_lookup(in_mem_cache_size):
+    try:
+        tmpdir = tempfile.mkdtemp()
+        pdict = WriteOncePersistentDict(
+                "pytools-test", container_dir=tmpdir,
+                in_mem_cache_size=in_mem_cache_size)
+
+        # check lookup
+        pdict[0] = 1
+        assert pdict[0] == 1
+        # do two lookups to test the cache
+        assert pdict[0] == 1
+
+        # check updating
+        with pytest.raises(ReadOnlyEntryError):
+            pdict[0] = 2
+
+        # check not found
+        with pytest.raises(NoSuchEntryError):
+            pdict.fetch(1)
+
+        # check store_if_not_present
+        pdict.store_if_not_present(0, 2)
+        assert pdict[0] == 1
+        pdict.store_if_not_present(1, 1)
+        assert pdict[1] == 1
+
+    finally:
+        shutil.rmtree(tmpdir)
+
+
+def test_write_once_persistent_dict_lru_policy():
+    try:
+        tmpdir = tempfile.mkdtemp()
+        pdict = WriteOncePersistentDict(
+                "pytools-test", container_dir=tmpdir, in_mem_cache_size=3)
+
+        pdict[1] = PDictTestingKeyOrValue(1)
+        pdict[2] = PDictTestingKeyOrValue(2)
+        pdict[3] = PDictTestingKeyOrValue(3)
+        pdict[4] = PDictTestingKeyOrValue(4)
+
+        val1 = pdict.fetch(1)
+
+        assert pdict.fetch(1) is val1
+        pdict.fetch(2)
+        assert pdict.fetch(1) is val1
+        pdict.fetch(2)
+        pdict.fetch(3)
+        assert pdict.fetch(1) is val1
+        pdict.fetch(2)
+        pdict.fetch(3)
+        pdict.fetch(2)
+        assert pdict.fetch(1) is val1
+        pdict.fetch(2)
+        pdict.fetch(3)
+        pdict.fetch(4)
+        assert pdict.fetch(1) is not val1
+
+    finally:
+        shutil.rmtree(tmpdir)
+
+
+def test_write_once_persistent_dict_synchronization():
+    try:
+        tmpdir = tempfile.mkdtemp()
+        pdict1 = WriteOncePersistentDict("pytools-test", container_dir=tmpdir)
+        pdict2 = WriteOncePersistentDict("pytools-test", container_dir=tmpdir)
+
+        # check lookup
+        pdict1[1] = 0
+        assert pdict2[1] == 0
+
+        # check updating
+        with pytest.raises(ReadOnlyEntryError):
+            pdict2[1] = 1
+
+    finally:
+        shutil.rmtree(tmpdir)
+
+
+def test_write_once_persistent_dict_cache_collisions():
+    try:
+        tmpdir = tempfile.mkdtemp()
+        pdict = WriteOncePersistentDict("pytools-test", container_dir=tmpdir)
+
+        key1 = PDictTestingKeyOrValue(1, hash_key=0)
+        key2 = PDictTestingKeyOrValue(2, hash_key=0)
+        pdict[key1] = 1
+
+        # check lookup
+        with pytest.warns(CollisionWarning):
+            with pytest.raises(NoSuchEntryError):
+                pdict.fetch(key2)
+
+        # check update
+        with pytest.raises(ReadOnlyEntryError):
+            pdict[key2] = 1
+
+        # check store_if_not_present
+        pdict.store_if_not_present(key2, 2)
+        assert pdict[key1] == 1
+
+    finally:
+        shutil.rmtree(tmpdir)
+
+
+def test_write_once_persistent_dict_clear():
+    try:
+        tmpdir = tempfile.mkdtemp()
+        pdict = WriteOncePersistentDict("pytools-test", container_dir=tmpdir)
+
+        pdict[0] = 1
+        pdict.fetch(0)
+        pdict.clear()
+
+        with pytest.raises(NoSuchEntryError):
+            pdict.fetch(0)
+    finally:
+        shutil.rmtree(tmpdir)
+
+
+def test_dtype_hashing():
+    np = pytest.importorskip("numpy")
+
+    keyb = KeyBuilder()
+    assert keyb(np.float32) == keyb(np.float32)
+    assert keyb(np.dtype(np.float32)) == keyb(np.dtype(np.float32))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        exec(sys.argv[1])
+    else:
+        pytest.main([__file__])
