59dcf91af7d92e96c90ca8cd3dbcaecb2436a79b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:07 +0000

    Add package flytekitplugins-kfpytorch/1.2.17/flytekitplugins-kfpytorch-1.2.17.tar.gz

---
+++ packages/flytekitplugins-kfpytorch/flytekitplugins-kfpytorch-1.2.17.tar.gz/flytekitplugins-kfpytorch-1.2.17/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+PLUGIN_NAME = "kfpytorch"
+
+microlib_name = f"flytekitplugins-{PLUGIN_NAME}"
+
+plugin_requires = ["cloudpickle", "flytekit>=1.1.0b0,<1.3.0,<2.0.0", "flyteidl>=1.2.11,<1.3.0"]
+
+__version__ = "1.2.17"
+
+setup(
+    name=microlib_name,
+    version=__version__,
+    author="flyteorg",
+    author_email="admin@flyte.org",
+    description="K8s based Pytorch plugin for Flytekit",
+    namespace_packages=["flytekitplugins"],
+    packages=[f"flytekitplugins.{PLUGIN_NAME}"],
+    install_requires=plugin_requires,
+    extras_require={
+        "elastic": ["torch>=1.9.0"],
+    },
+    license="apache2",
+    python_requires=">=3.7",
+    classifiers=[
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+)
