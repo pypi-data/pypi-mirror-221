338f54080b78247a835550e453b65863196436ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:48 +0000

    Add package Kerko/1.0.0/Kerko-1.0.0.tar.gz

---
+++ packages/Kerko/Kerko-1.0.0.tar.gz/Kerko-1.0.0/src/kerko/__init__.py
@@ -0,0 +1,38 @@
+"""
+Kerko: A Flask blueprint that provides faceted search for bibliographies based on Zotero.
+"""
+# pylint: disable=invalid-name
+
+import errno
+import pathlib
+import sys
+
+from flask import Blueprint
+
+from kerko.config_helpers import load_toml
+from kerko.jinja2 import register_filters
+
+# Kerko won't load translations on its own. To load them, an application may add
+# the following domain and translation directories to its Babel configuration.
+TRANSLATION_DOMAIN = 'kerko'
+TRANSLATION_DIRECTORIES = [str(pathlib.Path(__file__).parent / 'translations')]
+
+try:
+    DEFAULTS = load_toml(pathlib.Path(__file__).parent / 'default_config.toml')
+except RuntimeError as e:
+    print(e, file=sys.stderr)
+    sys.exit(errno.EINTR)  # This should make the WSGI server exit as well.
+
+
+def make_blueprint():
+    b = Blueprint(
+        'kerko', __name__, static_folder='static', template_folder='templates'
+    )
+    register_filters(b)
+    return b
+
+
+blueprint = make_blueprint()
+
+
+from kerko import views  # pylint: disable=wrong-import-position
