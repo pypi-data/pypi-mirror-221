33fd0ccf23b7532c2ce9746c7eb84ca7cd5e5a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:55 +0000

    Add package iqnus-boleto/1.0.9/iqnus-boleto-1.0.9.tar.gz

---
+++ packages/iqnus-boleto/iqnus-boleto-1.0.9.tar.gz/iqnus-boleto-1.0.9/setup.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+import os
+import pyboleto
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='iqnus-boleto',
+    version=pyboleto.__version__,
+    author='Trust-Code',
+    author_email='suporte@trustcode.com.br',
+    url='https://github.com/Trust-Code/python-boleto',
+    packages=find_packages(),
+    package_data={
+        '': ['LICENSE'],
+        'pyboleto': ['media/*.jpg', 'media/*.png', 'templates/*.html'],
+        'tests': ['xml/*.xml']
+    },
+    zip_safe=False,
+    provides=[
+        'pyboleto'
+    ],
+    license='BSD',
+    description='Python Library to create "boletos de cobrança bancária" for \
+    several Brazilian banks',
+    long_description=read('README.rst'),
+    long_description_content_type='text/x-rst',
+    download_url='https://github.com/luanpontes100/python-boleto',
+    scripts=[
+        'bin/html_pyboleto_sample.py',
+        'bin/pdf_pyboleto_sample.py'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: Portuguese (Brazilian)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Office/Business :: Financial',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Framework :: Django',
+    ],
+    platforms='any',
+    test_suite='tests.alltests.suite',
+    install_requires=[
+        'reportlab'
+    ],
+    tests_require=[
+        'pylint',
+        'tox',
+        'coverage',
+        'pep8',
+        'sphinx-pypi-upload',
+        'sphinx'
+    ]
+)
