5739a0cd2e1c1786543b4f2c418692a1075b6774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:02 +0000

    Add package cval-lib/0.0.2.21/cval-lib-0.0.2.21.tar.gz

---
+++ packages/cval-lib/cval-lib-0.0.2.21.tar.gz/cval-lib-0.0.2.21/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='cval-lib',
+    version='0.0.2.21',
+    description='python computer vision active learning library',
+    author='DGQ | Cyrill Belyakov',
+    author_email='',
+    url='https://cval.ai',
+    package_dir={
+        '': '.',
+    },
+    packages=find_packages(include=['cval_lib', 'cval_lib.*']),
+    install_requires=[
+        'pydantic',
+        'requests'
+    ]
+)
