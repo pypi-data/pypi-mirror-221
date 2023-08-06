55e4bb3c9c6bc2ae7d80e9a22574311f64d94c03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:10 +0000

    Add package my-math-library/0.1.0/my_math_library-0.1.0.tar.gz

---
+++ packages/my-math-library/my_math_library-0.1.0.tar.gz/my_math_library-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_math_library',
+    version='0.1.0',
+    description='A simple math library',
+    author='Your Name',
+    author_email='1264510459@qq.com',
+    packages=find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
