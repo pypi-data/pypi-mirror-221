8598a5a7e2ff0c188fe41ad0c994df84968f8cee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:57 +0000

    Add package gym-cas/0.0.10/gym_cas-0.0.10.tar.gz

---
+++ packages/gym-cas/gym_cas-0.0.10.tar.gz/gym_cas-0.0.10/src/gym_cas/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2023-present JACS <jacs@zbc.dk>
+#
+# SPDX-License-Identifier: MIT
+
+from sympy import *
+from sympy.abc import *
+from spb import *
+from .trigonometry import *
+from .vector import plot_vector, plot_vector_field
