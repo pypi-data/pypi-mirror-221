83f9080a67fd6eb29db8cee2a6fd55edd0c4710f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:58 +0000

    Add package saritasa-invocations/0.2.0/saritasa_invocations-0.2.0.tar.gz

---
+++ packages/saritasa-invocations/saritasa_invocations-0.2.0.tar.gz/saritasa_invocations-0.2.0/saritasa_invocations/__init__.py
@@ -0,0 +1,18 @@
+from saritasa_invocations import (
+    celery,
+    django,
+    docker,
+    fastapi,
+    git,
+    github_actions,
+    open_api,
+    pre_commit,
+    python,
+    system,
+)
+from saritasa_invocations.printing import (
+    print_error,
+    print_panel,
+    print_success,
+    print_warn,
+)
