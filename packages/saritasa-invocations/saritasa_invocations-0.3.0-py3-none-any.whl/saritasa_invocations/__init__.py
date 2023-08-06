9aed517b81b0f1c7f4bab9792c09fac6485a9af5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:58 +0000

    Add package saritasa-invocations/0.3.0/saritasa_invocations-0.3.0-py3-none-any.whl

---
+++ packages/saritasa-invocations/saritasa_invocations-0.3.0-py3-none-any.whl/saritasa_invocations/__init__.py
@@ -0,0 +1,19 @@
+from saritasa_invocations import (
+    alembic,
+    celery,
+    django,
+    docker,
+    fastapi,
+    git,
+    github_actions,
+    open_api,
+    pre_commit,
+    python,
+    system,
+)
+from saritasa_invocations.printing import (
+    print_error,
+    print_panel,
+    print_success,
+    print_warn,
+)
