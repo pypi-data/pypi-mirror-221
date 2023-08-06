1b15eab113e6428ac69b7697f89790d59acd9ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:08 +0000

    Add package spawn-lia/0.3.0/spawn-lia-0.3.0.tar.gz

---
+++ packages/spawn-lia/spawn-lia-0.3.0.tar.gz/spawn-lia-0.3.0/lia/simplify/verify_package.py
@@ -0,0 +1,9 @@
+"""The file is about providing functions to verify the package description in
+pyproject.toml or setup.py.
+
+:author: Julian M. Kleber
+"""
+
+
+def speak_veritas() -> None:
+    """With this function, lia asks you about the details of your package."""
