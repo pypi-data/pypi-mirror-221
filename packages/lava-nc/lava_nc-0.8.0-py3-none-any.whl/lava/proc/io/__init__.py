25c6aa719e293c6388032a64f7ab0f994accf545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:04 +0000

    Add package lava-nc/0.8.0/lava_nc-0.8.0-py3-none-any.whl

---
+++ packages/lava-nc/lava_nc-0.8.0-py3-none-any.whl/lava/proc/io/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (C) 2021-22 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+# See: https://spdx.org/licenses/
+
+from . import reset, source, sink, dataloader, encoder
+
+__all__ = ['reset', 'source', 'sink', 'dataloader', 'encoder']
