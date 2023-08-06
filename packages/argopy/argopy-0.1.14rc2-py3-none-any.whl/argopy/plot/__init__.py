612bf9310ef34114a2020e9cf2face9309a85d98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:53 +0000

    Add package argopy/0.1.14rc2/argopy-0.1.14rc2-py3-none-any.whl

---
+++ packages/argopy/argopy-0.1.14rc2-py3-none-any.whl/argopy/plot/__init__.py
@@ -0,0 +1,25 @@
+"""
+Plot Submodule docstring !
+"""
+from .plot import plot_trajectory, bar_plot, open_sat_altim_report, scatter_map, scatter_plot
+from .argo_colors import ArgoColors
+from .dashboards import open_dashboard as dashboard
+from .utils import discrete_coloring, latlongrid
+
+
+__all__ = (
+    # Also available on the argopy module level:
+    "dashboard",
+    "ArgoColors",
+
+    # Plot:
+    "plot_trajectory",
+    "bar_plot",
+    "scatter_map",
+    "scatter_plot",
+    "open_sat_altim_report",
+
+    # Utils:
+    "latlongrid",
+    "discrete_coloring",
+)
