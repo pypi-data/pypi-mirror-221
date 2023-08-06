7cf06a6bf35e1959650b0e6dc965bd1737cfe5fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:02 +0000

    Add package simple-pygame/0.0.7/simple_pygame-0.0.7-py3-none-any.whl

---
+++ packages/simple-pygame/simple_pygame-0.0.7-py3-none-any.whl/simple_pygame/constants.py
@@ -0,0 +1,16 @@
+"""
+A module that holds all the constants of Simple Pygame.
+"""
+AudioIsLoading = "AudioIsLoading"
+AudioEnded = "AudioEnded"
+
+SInt8 = "SInt8"
+SInt16 = "SInt16"
+SInt32 = "SInt32"
+UInt8 = "UInt8"
+
+MixerModule = "MixerModule"
+TransformModule = "TransformModule"
+AudioClass = "AudioClass"
+
+__version__ = "0.0.7"
