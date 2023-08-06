d6001eb0361aa20775065e8c4363717f38be64ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:27 +0000

    Add package log10-io/0.2.3/log10_io-0.2.3.tar.gz

---
+++ packages/log10-io/log10_io-0.2.3.tar.gz/log10_io-0.2.3/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name="Log10",
+    version="0.2.3",
+    description="Log10 LLM data management",
+    author="Log10 team",
+    author_email="team@log10.io",
+    url="",
+    packages=find_packages(),
+    install_requires=[
+        "openai",
+        "python-dotenv",
+    ],
+    extras_require={
+        "bigquery": ["google-cloud-bigquery"],
+    },
+)
