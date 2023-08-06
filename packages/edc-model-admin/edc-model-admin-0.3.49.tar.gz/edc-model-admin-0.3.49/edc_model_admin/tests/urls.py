aa1c1886a9c10a6ebfae08f80329a87b28333021
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:52 +0000

    Add package edc-model-admin/0.3.49/edc-model-admin-0.3.49.tar.gz

---
+++ packages/edc-model-admin/edc-model-admin-0.3.49.tar.gz/edc-model-admin-0.3.49/edc_model_admin/tests/urls.py
@@ -0,0 +1,35 @@
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic.base import RedirectView
+from edc_utils.paths_for_urlpatterns import paths_for_urlpatterns
+
+from .admin import *  # noqa
+from .views import CrfOneListView
+
+app_name = "edc_model_admin"
+
+urlpatterns = []
+
+for app_name in [
+    "edc_dashboard",
+    "edc_auth",
+    "edc_export",
+    "edc_consent",
+    "edc_device",
+    "edc_protocol",
+    "edc_reference",
+    "edc_visit_schedule",
+]:
+    for p in paths_for_urlpatterns(app_name):
+        urlpatterns.append(p)
+    # urlpatterns.append(path(f"{app_name}/", include(f"{app_name}.urls")))
+
+urlpatterns += [
+    path("admin/", admin.site.urls),
+    path("", CrfOneListView.as_view(), name="crfone-list"),
+    path("", include("edc_model_admin.tests.dashboard_app.urls")),
+    path("", include("edc_model_admin.tests.dashboard2_app.urls")),
+    path("", RedirectView.as_view(url="admin/"), name="administration_url"),
+    path("", RedirectView.as_view(url="admin/"), name="home_url"),
+    path("", RedirectView.as_view(url="admin/"), name="logout"),
+]
