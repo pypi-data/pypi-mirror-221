14a6e49ce019158384897bd109b31a50c3e36790
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:15 +0000

    Add package alsa-midi/1.0.2/alsa_midi-1.0.2-cp38-cp38-manylinux_2_17_x86_64.whl

---
+++ packages/alsa-midi/alsa_midi-1.0.2-cp38-cp38-manylinux_2_17_x86_64.whl/alsa_midi/util.py
@@ -0,0 +1,28 @@
+
+from ._ffi import alsa, ffi
+from .exceptions import ALSAError
+
+
+def _check_alsa_error(code):
+    if not isinstance(code, int):
+        raise TypeError("ALSA error code must be an int")
+    if code < 0:
+        message = ffi.string(alsa.snd_strerror(code))
+        raise ALSAError(message.decode(), code)
+
+
+def _ensure_4bit(value):
+    value = int(value)
+    if value < 0x00 or value > 0x0f:
+        raise ValueError("4-bit value expected")
+    return value
+
+
+def _ensure_7bit(value):
+    value = int(value)
+    if value < 0x00 or value > 0x7f:
+        raise ValueError("7-bit value expected")
+    return value
+
+
+__all__ = ["_check_alsa_error", "_ensure_4bit", "_ensure_7bit"]
