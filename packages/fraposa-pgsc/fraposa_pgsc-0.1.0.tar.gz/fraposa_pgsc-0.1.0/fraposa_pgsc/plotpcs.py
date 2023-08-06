9ae5f1115d308026859d7d168ddd5c024cdb0b95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:34 +0000

    Add package fraposa-pgsc/0.1.0/fraposa_pgsc-0.1.0.tar.gz

---
+++ packages/fraposa-pgsc/fraposa_pgsc-0.1.0.tar.gz/fraposa_pgsc-0.1.0/fraposa_pgsc/plotpcs.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+import fraposa as fp
+import argparse, sys
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('ref_filepref', help='Prefix of binary PLINK file for the reference data.')
+    parser.add_argument('stu_filepref', help='Prefix of binary PLINK file for the study data.')
+    args = parser.parse_args()
+
+    fp.plot_pcs(args.ref_filepref, args.stu_filepref)
