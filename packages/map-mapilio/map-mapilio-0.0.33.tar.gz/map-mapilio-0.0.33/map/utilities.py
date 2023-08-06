5fd7b79f2fa3299282698b101f990ae0f8119f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:10 +0000

    Add package map-mapilio/0.0.33/map-mapilio-0.0.33.tar.gz

---
+++ packages/map-mapilio/map-mapilio-0.0.33.tar.gz/map-mapilio-0.0.33/map/utilities.py
@@ -0,0 +1,13 @@
+from json import JSONEncoder
+import numpy as np
+import decimal
+
+
+class JSON_ENCODER_FIX(JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        if isinstance(obj, decimal.Decimal):
+            return float(obj)
+
+        return JSONEncoder.default(self, obj)
