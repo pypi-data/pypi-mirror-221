af8d25f32053bc1921d0ad4bfd70c83ace1476bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:56 +0000

    Add package azure-communication-jobrouter/1.0.0b1/azure-communication-jobrouter-1.0.0b1.zip

---
+++ packages/azure-communication-jobrouter/azure-communication-jobrouter-1.0.0b1.zip/azure-communication-jobrouter-1.0.0b1/azure/communication/jobrouter/aio/__init__.py
@@ -0,0 +1,12 @@
+# ------------------------------------
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+# ------------------------------------
+
+from ._router_client_async import JobRouterClient
+from ._router_administration_client_async import JobRouterAdministrationClient
+
+__all__ =[
+    'JobRouterClient',
+    'JobRouterAdministrationClient'
+]
