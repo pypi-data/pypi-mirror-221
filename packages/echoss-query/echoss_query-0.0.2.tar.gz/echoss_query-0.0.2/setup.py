69ebdeffb7857a65d81ec4fb890ebb956a7e6608
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:05 +0000

    Add package echoss-query/0.0.2/echoss_query-0.0.2.tar.gz

---
+++ packages/echoss-query/echoss_query-0.0.2.tar.gz/echoss_query-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, Extension
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+print(long_description)
+setup(
+    name='echoss_query',
+    version='0.0.2',
+    packages=['echoss_query'],
+    url='',
+    requires=['pandas','pymongo','PyMySQL','PyYAML','opensearch'],
+    license='',
+    author='incheolshin',
+    author_email='incheolshin@12cm.co.kr',
+    description='echoss AI Bigdata Solution - Query Package',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    package_data={},
+    python_requires= '>3.7',
+)
