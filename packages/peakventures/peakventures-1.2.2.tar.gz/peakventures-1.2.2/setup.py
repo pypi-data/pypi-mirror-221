eb79233b186592fc066ae7e7814339e68d3a61f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:34 +0000

    Add package peakventures/1.2.2/peakventures-1.2.2.tar.gz

---
+++ packages/peakventures/peakventures-1.2.2.tar.gz/peakventures-1.2.2/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['peakventures']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.28.11,<2.0.0',
+ 'tenacity>=8.2.2,<9.0.0',
+ 'websockets>=11.0.3,<12.0.0']
+
+setup_kwargs = {
+    'name': 'peakventures',
+    'version': '1.2.2',
+    'description': 'PeakVentures Python Utilities for DataBricks',
+    'long_description': 'None',
+    'author': 'Volodymyr Smirnov',
+    'author_email': 'volodymyr@peakventures.co',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
