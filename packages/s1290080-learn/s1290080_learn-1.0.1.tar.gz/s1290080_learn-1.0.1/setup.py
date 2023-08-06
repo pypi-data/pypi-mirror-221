eb302eaa7420400cf9917c57170fee70914bb671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:14 +0000

    Add package s1290080-learn/1.0.1/s1290080_learn-1.0.1.tar.gz

---
+++ packages/s1290080-learn/s1290080_learn-1.0.1.tar.gz/s1290080_learn-1.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="s1290080_learn",
+    version="1.0.1",
+    install_requires=['pandas', 'plotly', 'numpy', 'pami'],
+    python_requires = '>=3.5',
+)
