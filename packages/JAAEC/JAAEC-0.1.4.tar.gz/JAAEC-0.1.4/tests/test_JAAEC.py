ff7fb2cce7915e86e083cf1f76beff6803735dc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:13 +0000

    Add package JAAEC/0.1.4/JAAEC-0.1.4.tar.gz

---
+++ packages/JAAEC/JAAEC-0.1.4.tar.gz/JAAEC-0.1.4/tests/test_JAAEC.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+"""Tests for `JAAEC` package."""
+
+
+import unittest
+
+from JAAEC import JAAEC
+
+
+class TestJaaec(unittest.TestCase):
+    """Tests for `JAAEC` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
