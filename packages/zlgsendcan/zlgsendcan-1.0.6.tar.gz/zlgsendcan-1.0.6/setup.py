6b91808b3f34b2a7c413531ea89a63a134b65e47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:52:35 +0000

    Add package zlgsendcan/1.0.6/zlgsendcan-1.0.6.tar.gz

---
+++ packages/zlgsendcan/zlgsendcan-1.0.6.tar.gz/zlgsendcan-1.0.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+packages = ['zlgsendcan']# 唯一的包名，自己取名
+setup(
+    name='zlgsendcan',
+    version='1.0.6',
+    author='liuzhong',
+    author_email='liuzhong@autolink.com.cn',
+    description='zlg send canmsg',
+    packages=packages,
+    include_package_data=True,
+    package_data={'zlgsend': ['kerneldlls/*.dll','*.dll']},
+    install_requires=[
+        'PyQt5',
+        'cantools',
+        'PyYAML'
+    ],
+)
