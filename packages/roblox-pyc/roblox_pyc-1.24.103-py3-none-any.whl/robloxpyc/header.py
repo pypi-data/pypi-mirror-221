4fdb4de135b7624687f5d620f4c83ea191eb50f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:57 +0000

    Add package roblox-pyc/1.24.103/roblox_pyc-1.24.103-py3-none-any.whl

---
+++ packages/roblox-pyc/roblox_pyc-1.24.103-py3-none-any.whl/robloxpyc/header.py
@@ -0,0 +1,52 @@
+headertemplate = """--/ Compiled using roblox-pyc | Python compiler \--
+		
+		
+------------------------------------ BUILT IN -------------------------------
+repeat task.wait() until _G.pyc
+local py, import, builtin = _G.pyc(script).py
+
+{}
+-----------------------------------------------------------------------------
+"""
+lunarheadertemplate = """--/ Compiled using roblox-pyc | Lunar compiler \--
+		
+		
+------------------------------------ BUILT IN -------------------------------
+repeat task.wait() until _G.pyc
+local import, builtin = _G.pyc(script).lunar
+
+{}
+-----------------------------------------------------------------------------
+"""
+
+pyfooter = """
+
+------------------------------------ END ------------------------------------
+if script:IsA("ModuleScript") then 
+	return getfenv()
+else
+	repeat task.wait() until _G.pyc.libs
+	if not _G.pyc.libs then
+		_G.pyc.libs = {}
+	end
+	_G.pyc.libs[script] = getfenv()
+end
+------------------------------------ END ------------------------------------
+
+"""
+
+def header(functions):
+    code = ""
+    for i in functions:
+        code += "local "+i+" = builtin."+i+"\n"
+    
+    #print(code)
+    return headertemplate.format(code)
+
+def lunarheader(functions):
+    code = ""
+    for i in functions:
+        code += "local "+i+" = builtin."+i+"\n"
+    
+    #print(code)
+    return lunarheadertemplate.format(code)
