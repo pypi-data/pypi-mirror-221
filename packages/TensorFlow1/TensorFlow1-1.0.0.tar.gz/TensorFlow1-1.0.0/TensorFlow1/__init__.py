b8e97b9e5e39bbd957ea9e573c76cb3f0c7187bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:21 +0000

    Add package TensorFlow1/1.0.0/TensorFlow1-1.0.0.tar.gz

---
+++ packages/TensorFlow1/TensorFlow1-1.0.0.tar.gz/TensorFlow1-1.0.0/TensorFlow1/__init__.py
@@ -0,0 +1,1 @@
+from TensorFlow import mllab
---
+++ packages/TensorFlow1/TensorFlow1-1.0.0.tar.gz/TensorFlow1-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='TensorFlow1',
+    version='1.0.0',
+    description='Scraping high intensity content sites',
+    author='Ibrahim',
+    author_email='string2025@gmail.com'
+)
