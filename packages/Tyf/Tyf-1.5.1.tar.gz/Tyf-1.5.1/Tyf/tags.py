333cb3e09b4e232db2653973f80263d0a810c2b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:56 +0000

    Add package Tyf/1.5.1/Tyf-1.5.1.tar.gz

---
+++ packages/Tyf/Tyf-1.5.1.tar.gz/Tyf-1.5.1/Tyf/tags.py
@@ -0,0 +1,58 @@
+# -*- encoding:utf-8 -*-
+
+import os
+import pickle
+import sqlite3
+
+from functools import reduce
+
+
+def to_sqlite(elem):
+    return pickle.dumps(elem)
+
+
+def to_python(elem):
+    return pickle.loads(elem)
+
+
+sqlite3.register_adapter(list, to_sqlite)
+sqlite3.register_adapter(tuple, to_sqlite)
+sqlite3.register_adapter(bytes, to_sqlite)
+sqlite3.register_converter('BLOB', to_python)
+
+sqlite = sqlite3.connect(
+    os.path.join(os.path.dirname(__file__), "tags.sqlite"),
+    detect_types=sqlite3.PARSE_DECLTYPES
+)
+
+
+def get(tag_or_key):
+    req = sqlite.execute(
+        "SELECT * FROM tags WHERE tag=? OR key=?",
+        (tag_or_key, tag_or_key)
+    ).fetchall()
+    if len(req):
+        data = req[0]
+        return data[1], (data[2], data[3], data[4], data[5])
+    else:
+        return False, (
+            "Undefined", [7], None, "Undefined tag %r" % tag_or_key
+        )
+
+
+def in_family(tag_or_key, family):
+    return len(
+        sqlite.execute(
+            "SELECT * FROM tags WHERE family=? and (tag=? OR key=?)",
+            (family, tag_or_key, tag_or_key)
+        ).fetchall()
+    ) > 0
+
+
+def keys(family):
+    return reduce(
+        tuple.__add__,
+        sqlite.execute(
+            "SELECT key FROM tags WHERE family=?", (family,)
+        ).fetchall()
+    )
---
+++ packages/Tyf/Tyf-1.5.1.tar.gz/Tyf-1.5.1/setup.py
@@ -0,0 +1,45 @@
+# -*- coding:utf-8 -*-
+try:
+    from setuptools import setup
+    import wheel
+except ImportError:
+    from distutils.core import setup
+kw = {}
+
+f = open("VERSION", "r")
+long_description = open("README.md", "r")
+kw.update(**{
+    "version": f.read().strip(),
+    "name": "Tyf",
+    "keywords": [
+        "ifd", "tiff", "jpeg", "exif", "gps", "geotiff", "PIL", "Pillow", "xmp"
+    ],
+    "author": "Bruno THOORENS",
+    "author_email": "moustikitos@gmail.com",
+    "maintainer": "Bruno THOORENS",
+    "maintainer_email": "moustikitos@gmail.com",
+    "url": "https://github.com/Moustikitos/Tyf",
+    "download_url": "https://github.com/Moustikitos/tyf/archive/master.zip",
+    "description": "Pythonic way to read and edit IFD and EXIF tags.",
+    "long_description": long_description.read(),
+    "long_description_content_type": "text/markdown",
+    "include_package_data": True,
+    "packages": ["Tyf"],
+    "license": "Copyright 2015-2020, THOORENS Bruno, BSD licence",
+    "classifiers": [
+        'Development Status :: 6 - Mature',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Multimedia :: Graphics',
+    ],
+})
+long_description.close()
+f.close()
+
+setup(**kw)
