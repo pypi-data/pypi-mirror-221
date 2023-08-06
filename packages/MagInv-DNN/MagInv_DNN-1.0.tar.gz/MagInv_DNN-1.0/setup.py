24fe3ed6705c48e6e4f4a7e15da0fbffac9a1a9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:24 +0000

    Add package MagInv-DNN/1.0/MagInv_DNN-1.0.tar.gz

---
+++ packages/MagInv-DNN/MagInv_DNN-1.0.tar.gz/MagInv_DNN-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MagInv_DNN',
+    version='1.0',
+    author='Zahra Ashena',
+    description='Gravity and Magnetic Data DNN Inversion Toolkit',
+    packages=find_packages(),
+)
