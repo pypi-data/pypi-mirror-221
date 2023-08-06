c078fc7be1c249350caf6d70a41a6b14b74f14e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:12 +0000

    Add package pansurg/1.0/pansurg-1.0.tar.gz

---
+++ packages/pansurg/pansurg-1.0.tar.gz/pansurg-1.0/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup
+from setuptools_rust import Binding, RustExtension
+
+import os
+import subprocess
+
+
+setup(
+    name="pansurg",
+    version="1.0",
+    rust_extensions=[RustExtension("surrealdb.rust_surrealdb", binding=Binding.PyO3)],
+    packages=[
+        "surrealdb", 
+        "surrealdb.models",
+        "surrealdb.execution_mixins"
+    ],
+    package_data={
+        "surrealdb": ["binaries/*"],
+    },
+    # rust extensions are not zip safe, just like C-extensions.
+    zip_safe=False,
+    include_package_data=True
+)
+
