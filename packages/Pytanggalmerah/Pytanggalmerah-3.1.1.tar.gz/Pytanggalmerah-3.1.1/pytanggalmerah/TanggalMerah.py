c7c22c3fe5e8e7c169eb9416f5ad11418b471de1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:53 +0000

    Add package Pytanggalmerah/3.1.1/Pytanggalmerah-3.1.1.tar.gz

---
+++ packages/Pytanggalmerah/Pytanggalmerah-3.1.1.tar.gz/Pytanggalmerah-3.1.1/pytanggalmerah/TanggalMerah.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from datetime import datetime
+from pytz import timezone
+from .myrequests import MyRequests
+import json
+import os
+
+
+in_cache_path = os.path.abspath(os.path.dirname(__file__))
+
+
+class TanggalMerah:
+    def __init__(self, cache_path=None, cache_time=600):
+        self.event = set([])
+        self.date = datetime.now(timezone("Asia/Jakarta"))
+        req = MyRequests("https://raw.githubusercontent.com/guangrei/APIHariLibur_V2/main/holidays.json",
+                         cache_path=in_cache_path, cache_time=cache_time)
+        self.data = json.loads(req.response)
+
+    # end __init_()
+
+    def set_timezone(self, tz):
+        self.date = datetime.now(timezone(tz))
+        return self.date
+
+    # end set_timezone()
+
+    def check(self):
+        check = (self.is_sunday(), self.is_holiday())
+        return True in check
+    # end check()
+
+    def is_sunday(self):
+        now = self.date
+        day = now.strftime("%A")
+        if day == "Sunday":
+            self.event.add("sunday")
+            return True
+        else:
+            return False
+
+    # end is_sunday()
+
+    def is_holiday(self):
+        d = self.date.strftime("%Y-%m-%d")
+        if d in self.data:
+            self.event.add(self.data[d]['summary'])
+            return True
+        else:
+            return False
+
+    # end is_holiday()
+
+    def set_date(self, y, m, d):
+        self.date = datetime(int(y), int(m), int(d), 0, 0)
+        return self.date
+
+    # end set_date()
+
+    def get_event(self):
+        return list(self.event)
+
+    # end get_event()
+# end class TanggalMerah
+
+
+if __name__ == "__main__":
+    pass
---
+++ packages/Pytanggalmerah/Pytanggalmerah-3.1.1.tar.gz/Pytanggalmerah-3.1.1/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from os import path
+
+requirements = [
+    'requests',
+    'pytz',
+    'zcache'
+]
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md')) as f:
+    long_description = f.read()
+
+setup(
+    name='Pytanggalmerah',
+    version='3.1.1',
+    packages=['pytanggalmerah'],
+    scripts=["harilibur"],
+    license='MIT',
+    author="guangrei",
+    author_email="myawn@pm.me",
+    description="python module to check indonesia holiday calendar (include sunday)",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords="holiday indonesia calendar sunday",
+    url="https://github.com/guangrei/pytanggalmerah",
+    install_requires=requirements,
+)
