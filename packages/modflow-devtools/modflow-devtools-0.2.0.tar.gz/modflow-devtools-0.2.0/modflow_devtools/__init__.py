4a91bf5a643ae97b552774ced37084bd09537709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:29 +0000

    Add package modflow-devtools/0.2.0/modflow-devtools-0.2.0.tar.gz

---
+++ packages/modflow-devtools/modflow-devtools-0.2.0.tar.gz/modflow-devtools-0.2.0/modflow_devtools/__init__.py
@@ -0,0 +1,7 @@
+__author__ = "Joseph D. Hughes"
+__date__ = "Jul 26, 2023"
+__version__ = "0.2.0"
+__maintainer__ = "Joseph D. Hughes"
+__email__ = "jdhughes@usgs.gov"
+__status__ = "Production"
+__description__ = """Python tools for MODFLOW development and testing."""
