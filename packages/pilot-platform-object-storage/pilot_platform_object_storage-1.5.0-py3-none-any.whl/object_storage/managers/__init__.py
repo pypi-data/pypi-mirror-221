819fe60d98ea58f47d98b2537d467771421ce7ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:27 +0000

    Add package pilot-platform-object-storage/1.5.0/pilot_platform_object_storage-1.5.0-py3-none-any.whl

---
+++ packages/pilot-platform-object-storage/pilot_platform_object_storage-1.5.0-py3-none-any.whl/object_storage/managers/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (C) 2023 Indoc Research
+#
+# Contact Indoc Research for any questions regarding the use of this source code.
+
+from object_storage.managers.azure_blob_manager import AzureBlobStorageManager
+
+__all__ = ['AzureBlobStorageManager']
---
+++ packages/pilot-platform-object-storage/pilot_platform_object_storage-1.5.0-py3-none-any.whl/object_storage/providers/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (C) 2023 Indoc Research
+#
+# Contact Indoc Research for any questions regarding the use of this source code.
+
+from object_storage.providers.azure import AzureClient
+from object_storage.providers.enum import Provider
+
+__all__ = ['Provider', 'AzureClient']
