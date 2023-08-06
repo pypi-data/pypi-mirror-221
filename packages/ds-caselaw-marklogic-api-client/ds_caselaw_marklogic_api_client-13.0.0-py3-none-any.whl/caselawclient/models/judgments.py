8d7a7cd313e0b25496ba87727e917982b6829ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:42 +0000

    Add package ds-caselaw-marklogic-api-client/13.0.0/ds_caselaw_marklogic_api_client-13.0.0-py3-none-any.whl

---
+++ packages/ds-caselaw-marklogic-api-client/ds_caselaw_marklogic_api_client-13.0.0-py3-none-any.whl/caselawclient/models/judgments.py
@@ -0,0 +1,54 @@
+from functools import cached_property
+from typing import Any
+
+from ds_caselaw_utils import neutral_url
+
+from ..xml_helpers import get_xpath_match_string
+from .documents import Document
+
+
+class Judgment(Document):
+    document_noun = "judgment"
+    document_noun_plural = "judgments"
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        self.attributes_to_validate = self.attributes_to_validate + [
+            (
+                "has_ncn",
+                True,
+                "This {document_noun} has no neutral citation number",
+            ),
+            (
+                "has_valid_ncn",
+                True,
+                "The neutral citation number of this {document_noun} is not valid",
+            ),
+        ]
+
+        super(Judgment, self).__init__(*args, **kwargs)
+
+    @cached_property
+    def neutral_citation(self) -> str:
+        return get_xpath_match_string(
+            self.content_as_xml_tree,
+            "/akn:akomaNtoso/akn:*/akn:meta/akn:proprietary/uk:cite/text()",
+            {
+                "uk": "https://caselaw.nationalarchives.gov.uk/akn",
+                "akn": "http://docs.oasis-open.org/legaldocml/ns/akn/3.0",
+            },
+        )
+
+    @cached_property
+    def has_ncn(self) -> bool:
+        if not self.neutral_citation:
+            return False
+
+        return True
+
+    @cached_property
+    def has_valid_ncn(self) -> bool:
+        # The checks that we can convert an NCN to a URI using the function from utils
+        if not self.has_ncn or not neutral_url(self.neutral_citation):
+            return False
+
+        return True
