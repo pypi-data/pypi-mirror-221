97d2825f123714fffb836f11118b8a16a8e09d7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:02 +0000

    Add package minecraft-script/0.1.401/minecraft_script-0.1.401-py3-none-any.whl

---
+++ packages/minecraft-script/minecraft_script-0.1.401-py3-none-any.whl/minecraft_script/__init__.py
@@ -0,0 +1,54 @@
+from .lexer import Lexer
+from .parser import Parser
+from .interpreter import Interpreter, Context, SymbolTable
+
+version = "0.1.401"
+
+
+def run(text: str):
+    global_symbol_table = SymbolTable()
+
+    run_lexer = Lexer(text)
+    tokens = run_lexer.tokenize()
+
+    run_parser = Parser(tokens)
+    ast = run_parser.parse()
+
+    run_interpreter = Interpreter()
+    context = Context('main', global_symbol_table)
+    print(run_interpreter.visit(ast, context))
+
+
+def run_file(filepath: str):  # currently line-by-line
+    with open(filepath, 'rt') as file:
+        file_content = file.read()
+
+    global_symbol_table = SymbolTable()
+
+    for line in file_content.split('\n'):
+        run_lexer = Lexer(line)
+        tokens = run_lexer.tokenize()
+
+        run_parser = Parser(tokens)
+        ast = run_parser.parse()
+
+        run_interpreter = Interpreter()
+        context = Context('main', global_symbol_table)
+        print(run_interpreter.visit(ast, context))
+
+
+def run_shell():
+    global_symbol_table = SymbolTable()
+
+    while True:
+        text = input("> ")
+
+        run_lexer = Lexer(text)
+        tokens = run_lexer.tokenize()
+
+        run_parser = Parser(tokens)
+        ast = run_parser.parse()
+
+        run_interpreter = Interpreter()
+        context = Context('main', global_symbol_table)
+        print(run_interpreter.visit(ast, context))
