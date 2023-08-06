f9646ef86c844221a741a4e7f2a08bec7c4d7b0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:57 +0000

    Add package SuperPang/1.1.0/SuperPang-1.1.0.tar.gz

---
+++ packages/SuperPang/SuperPang-1.1.0.tar.gz/SuperPang-1.1.0/setup.py
@@ -0,0 +1,19 @@
+from Cython.Build import cythonize
+import setuptools
+
+extensions = [setuptools.Extension("superpang.lib.Compressor", sources=["src/superpang/lib/Compressor.pyx"]),
+              setuptools.Extension("superpang.lib.cutils", sources=["src/superpang/lib/cutils.pyx"]),
+              setuptools.Extension("superpang.lib.vtools", sources=["src/superpang/lib/vtools.pyx"])]
+
+setuptools.setup(
+    ext_modules = cythonize(extensions),
+    setup_requires = ['cython'],
+    version = open('src/superpang/VERSION').read().strip()
+    )
+
+
+# pushing stuff to pip :
+# rm -r build/* dist/* src/SuperPang.egg-info/ src/superpang/lib/superPang/lib/*c
+# python3 -m build
+# python3 -m twine upload --repository pypi dist/*tar.gz
+# rm -r build dist src/SuperPang.egg-info src/superPang/lib/*c
