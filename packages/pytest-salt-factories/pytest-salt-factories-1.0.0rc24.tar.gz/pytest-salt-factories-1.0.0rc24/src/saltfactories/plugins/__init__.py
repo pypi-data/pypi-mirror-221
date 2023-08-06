6deab3da6bbbe857c0200851704650746058750f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:14 +0000

    Add package pytest-salt-factories/1.0.0rc24/pytest-salt-factories-1.0.0rc24.tar.gz

---
+++ packages/pytest-salt-factories/pytest-salt-factories-1.0.0rc24.tar.gz/pytest-salt-factories-1.0.0rc24/src/saltfactories/plugins/__init__.py
@@ -0,0 +1,93 @@
+"""
+Salt Factories PyTest plugin interface.
+"""
+import logging
+import os
+import tempfile
+
+import pytest
+import pytestskipmarkers.utils.platform
+
+import saltfactories.utils.tempfiles
+
+log = logging.getLogger(__name__)
+
+
+def pytest_tempdir_temproot():
+    """
+    Define the temp directory to use as a base for the test run.
+    """
+    # Taken from https://github.com/saltstack/salt/blob/v2019.2.0/tests/support/paths.py
+    # Avoid ${TMPDIR} and gettempdir() on MacOS as they yield a base path too long
+    # for unix sockets: ``error: AF_UNIX path too long``
+    # Gentoo Portage prefers ebuild tests are rooted in ${TMPDIR}
+    if pytestskipmarkers.utils.platform.is_windows():
+        tempdir = "C:/Windows/Temp"
+    elif pytestskipmarkers.utils.platform.is_darwin():
+        tempdir = "/tmp"  # noqa: S108
+    else:
+        tempdir = os.environ.get("TMPDIR") or tempfile.gettempdir()
+    return os.path.abspath(os.path.realpath(tempdir))
+
+
+def pytest_tempdir_basename():
+    """
+    Return the temporary directory basename for the salt test suite.
+    """
+    return "saltfactories"
+
+
+def pytest_runtest_logstart(nodeid):
+    """
+    Signal the start of running a single test item.
+
+    This hook will be called **before** :func:`pytest_runtest_setup`, :func:`pytest_runtest_call` and
+    :func:`pytest_runtest_teardown` hooks.
+
+    :param str nodeid: full id of the item
+    :param location: a triple of ``(filename, linenum, testname)``
+    """
+    log.debug(">>>>>>> START %s >>>>>>>", nodeid)
+
+
+def pytest_runtest_logfinish(nodeid):
+    """
+    Signal the complete finish of running a single test item.
+
+    This hook will be called **after** :func:`pytest_runtest_setup`, :func:`pytest_runtest_call` and
+    :func:`pytest_runtest_teardown` hooks.
+
+    :param str nodeid: full id of the item
+    :param location: a triple of ``(filename, linenum, testname)``
+    """
+    log.debug("<<<<<<< END %s <<<<<<<", nodeid)
+
+
+def pytest_runtest_logreport(report):
+    """
+    Log the test running.
+
+    Process the :py:class:`_pytest.reports.TestReport` produced for each
+    of the setup, call and teardown runtest phases of an item.
+    See :func:`pytest_runtest_protocol` for a description of the runtest protocol.
+    """
+    if report.when == "call":
+        log.debug("======= %s %s ========", report.outcome.upper(), report.nodeid)
+
+
+@pytest.hookimpl(trylast=True)
+def pytest_load_initial_conftests(*_):
+    """
+    Register our pytest helpers.
+    """
+    # Do not move these deferred imports. It allows running against a Salt
+    # onedir build in salt's repo checkout.
+    import salt.version  # pylint: disable=import-outside-toplevel
+
+    if salt.version.__saltstack_version__ < "3005":
+        msg = f"Only salt>=3005 is supported(Installed version {salt.version.__saltstack_version__}"
+        raise pytest.UsageError(msg)
+    if "temp_directory" not in pytest.helpers:
+        pytest.helpers.register(saltfactories.utils.tempfiles.temp_directory, name="temp_directory")
+    if "temp_file" not in pytest.helpers:
+        pytest.helpers.register(saltfactories.utils.tempfiles.temp_file, name="temp_file")
---
+++ packages/pytest-salt-factories/pytest-salt-factories-1.0.0rc24.tar.gz/pytest-salt-factories-1.0.0rc24/tests/functional/loader/test_fixture_deps.py
@@ -0,0 +1,60 @@
+import logging
+from unittest.mock import patch
+
+import pytest
+
+import saltfactories
+
+try:
+    saltfactories.__salt__  # pylint: disable=pointless-statement
+    HAS_SALT_DUNDER = True
+except AttributeError:
+    HAS_SALT_DUNDER = False
+
+log = logging.getLogger(__name__)
+
+
+@pytest.fixture(autouse=True)
+def _confirm_saltfactories_does_not_have_salt_dunders():
+    assert (
+        HAS_SALT_DUNDER is False
+    ), "Weirdly, the saltfactories module has a __salt__ dunder defined. That's a bug!"
+
+
+def confirm_saltfactories_does_not_have_salt_dunders_after_setup_loader_mock_terminates(
+    setup_loader_mock,
+):
+    yield
+    with pytest.raises(AttributeError):
+        assert isinstance(saltfactories.__salt__, dict)  # pylint: disable=no-member
+
+
+@pytest.fixture
+def pre_loader_modules_patched_fixture():
+    with pytest.raises(AttributeError):
+        assert isinstance(saltfactories.__salt__, dict)  # pylint: disable=no-member
+    return False
+
+
+@pytest.fixture
+def configure_loader_modules(pre_loader_modules_patched_fixture):
+    return {
+        saltfactories: {
+            "__salt__": {"test.echo": lambda x: x, "foo": pre_loader_modules_patched_fixture}
+        }
+    }
+
+
+@pytest.fixture
+def _fixture_that_needs_loader_modules_patched():
+    assert saltfactories.__salt__["foo"] is False  # pylint: disable=no-member
+    with patch.dict(saltfactories.__salt__, {"foo": True}):  # pylint: disable=no-member
+        assert saltfactories.__salt__["foo"] is True  # pylint: disable=no-member
+        yield
+    assert saltfactories.__salt__["foo"] is False  # pylint: disable=no-member
+
+
+@pytest.mark.usefixtures("_fixture_that_needs_loader_modules_patched")
+def test_fixture_deps():
+    assert saltfactories.__salt__["foo"] is True  # pylint: disable=no-member
+    assert saltfactories.__salt__["test.echo"]("foo") == "foo"  # pylint: disable=no-member
