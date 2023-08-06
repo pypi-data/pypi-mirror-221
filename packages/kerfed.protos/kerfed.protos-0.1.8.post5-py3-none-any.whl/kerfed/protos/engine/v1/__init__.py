fc0c96149294b6e4a46dc83d6dc92ff1e2623235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:19 +0000

    Add package kerfed.protos/0.1.8.post5/kerfed.protos-0.1.8.post5-py3-none-any.whl

---
+++ packages/kerfed.protos/kerfed.protos-0.1.8.post5-py3-none-any.whl/kerfed/protos/engine/v1/__init__.py
@@ -0,0 +1,16 @@
+# kerfed.protos.engine.v1
+# ------------
+#
+# Helper to keep proto imports in one place.
+
+
+from .analysis_pb2 import AnalyzeRequest, AnalyzeResponse
+from .load_pb2 import LoadRequest, LoadResponse
+
+
+__all__ = [
+    AnalyzeRequest,
+    AnalyzeResponse,
+    LoadRequest,
+    LoadResponse,
+]
