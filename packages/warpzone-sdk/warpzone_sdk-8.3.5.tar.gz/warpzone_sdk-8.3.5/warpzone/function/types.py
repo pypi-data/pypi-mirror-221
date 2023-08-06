a621923421b4f556b87b601e3e940a143a53ecb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:59 +0000

    Add package warpzone-sdk/8.3.5/warpzone_sdk-8.3.5.tar.gz

---
+++ packages/warpzone-sdk/warpzone_sdk-8.3.5.tar.gz/warpzone_sdk-8.3.5/warpzone/function/types.py
@@ -0,0 +1,18 @@
+from typing import Any, Callable
+
+from warpzone.function.processors.dependencies import DependencyProcessor
+from warpzone.function.processors.outputs import OutputProcessor
+from warpzone.function.processors.triggers import TriggerProcessor
+
+SingleArgumentCallable = Callable[[Any], Any]
+
+
+def get_function_type(
+    trigger: TriggerProcessor,
+    output: OutputProcessor,
+    dependencies: list[DependencyProcessor],
+):
+    return Callable[
+        [trigger.arg_type] + [dep.return_type for dep in dependencies],
+        output.return_type,
+    ]
