3790dee66c80e19314a757ad4331c207e3242595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:42 +0000

    Add package pymeili/0.1.3/pymeili-0.1.3.tar.gz

---
+++ packages/pymeili/pymeili-0.1.3.tar.gz/pymeili-0.1.3/README.txt
@@ -0,0 +1,13 @@
+pymeili is a module to beautify your python plot with more simple way.
+
+【IMPORTANT】
+Before using this module, you need to install font-packages: https://dwl.freefontsfamily.com/download/futura/; moving the font file to installed module folder(e.g. C:\Users\Username\AppData\Local\Programs\Python\Python311\Lib\site-packages\pymeili).
+For more information and instruction, please go to https://github.com/VVVICTORZHOU/resources.git.
+
+
+Install guide: (run on your powershell or cmd)
+> pip install pymeili
+
+Update guide: (run on your powershell or cmd)
+> pip install --upgrade pymeili
+
---
+++ packages/pymeili/pymeili-0.1.3.tar.gz/pymeili-0.1.3/pymeili/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pymeili.beautifyplot import (
+    initplot,
+    colorlist,
+    cmaplist,
+    getBARHNO,
+    getBARNO,
+    getHISTNO,
+    getPLOTNO,
+    getSCATTERNO,
+    savefig,
+    title, lefttitle, righttitle,
+    scatter, plot, bar, hist, hist2d, pie, barh, table,
+    contour, contourf, colorbar, 
+    xlabel, ylabel, xticks, yticks, xyticks, ticksall,
+    xlim, ylim, xylim, xscale, yscale, xlog, ylog,
+    grid, spines, legend, hidespines, xaxisposition, yaxisposition,
+    axhline, axvline, text, labeltext, 
+    fill_between, fill_betweenx,
+    figsize, show, clf, close, figure,
+    imread, imshow, imsave,
+    
+)
+
+__version__: str = '0.1.3'
