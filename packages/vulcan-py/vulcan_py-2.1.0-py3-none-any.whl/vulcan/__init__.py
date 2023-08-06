8dabb25e767bf08f11164a4062d11f48d5143715
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:12 +0000

    Add package vulcan-py/2.1.0/vulcan_py-2.1.0-py3-none-any.whl

---
+++ packages/vulcan-py/vulcan_py-2.1.0-py3-none-any.whl/vulcan/__init__.py
@@ -0,0 +1,171 @@
+import distutils.core
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Any, Dict, List, Mapping, Optional, Tuple, Union, cast
+
+import sys
+import tomlkit
+import tomlkit.container
+import tomlkit.items
+from setuptools import setup
+
+if sys.version_info >= (3, 8):
+    from typing import TypedDict
+else:
+    from typing_extensions import TypedDict
+
+
+class VulcanConfigError(Exception):
+    pass
+
+
+class VersionDict(TypedDict, total=False):
+    version: str
+    extras: List[str]
+
+
+VersionSpecs = Mapping[str, Union[str, VersionDict]]
+
+
+def flatten_reqs(versions: VersionSpecs) -> List[str]:
+    return [to_pep508(lib, req) for lib, req in versions.items()]
+
+
+@dataclass
+class ShivOpts:
+    bin_name: str
+    console_script: Optional[str] = None
+    entry_point: Optional[str] = None
+    interpreter: Optional[str] = None
+    with_extras: Optional[List[str]] = None
+    extra_args: str = ''
+
+
+def list_or_none(val: Any) -> Optional[List[str]]:
+    return [str(v) for v in val] if val is not None else None
+
+
+def str_or_none(val: Any) -> Optional[str]:
+    return str(val) if val is not None else None
+
+
+def dict_or_none(val: Any) -> Optional[Dict[str, Any]]:
+    return {str(k): v for k, v in val.items()} if val is not None else None
+
+
+@dataclass
+class Vulcan:
+    source_path: Path
+    plugins: Optional[List[str]]
+    shiv_options: List[ShivOpts]
+    lockfile: Path
+    dependencies: Optional[List[str]]
+    configured_dependencies: VersionSpecs
+    extras: Optional[Dict[str, List[str]]]
+    configured_extras: Dict[str, List[str]]
+    dev_dependencies: Dict[str, VersionSpecs]
+    dynamic: Optional[List[str]]
+    no_lock: bool = False
+    python_lock_with: Optional[str] = None
+
+    @classmethod
+    def from_source(cls, source_path: Path, fail_on_missing_lock: bool = True) -> 'Vulcan':
+        with open(source_path / 'pyproject.toml') as f:
+            all_config = tomlkit.loads(f.read())
+            config = all_config['tool']['vulcan']  # type: ignore[index]
+            assert isinstance(config, dict)
+            dynamic = all_config['project'].get('dynamic', [])  # type: ignore[union-attr]
+        lockfile = source_path / config.get('lockfile', 'vulcan.lock')
+
+        no_lock = config.get('no-lock', False)
+        install_requires: Optional[List[str]] = []
+        extras_require: Optional[Dict[str, List[str]]] = {}
+        if not no_lock:
+            try:
+                install_requires, extras_require = get_requires(lockfile)
+            except FileNotFoundError:
+                if fail_on_missing_lock:
+                    raise
+                install_requires = None
+                extras_require = None
+
+        python_lock_with = config.get('python-lock-with')
+
+        shiv_ops = []
+        shiv_config = config.get('shiv', [])
+        for conf in shiv_config:
+            shiv_ops.append(
+                ShivOpts(
+                    bin_name=str(conf.get('bin_name')),
+                    console_script=str_or_none(conf.get('console_script')),
+                    entry_point=str_or_none(conf.get('entry_point')),
+                    interpreter=str_or_none(conf.get('interpreter')),
+                    with_extras=[str(e) for e in conf.get('with_extras', [])],
+                    extra_args=str(conf.get('extra_args', '')),
+                ))
+        # note that setuptools also checks this, and says that it _should_ consider this a warning, but will
+        # not for the intermediate period.
+        # We'll consider it an error until setuptools sees fit to do it for us, and then remove this check.
+        if 'dependencies' in config and 'dependencies' not in dynamic:
+            raise RuntimeError("tool.vulcan.dependencies configured but 'dependencies' not in dynamic,"
+                               " this is an error according to PEP-621. "
+                               "See https://peps.python.org/pep-0621/#dynamic for more information")
+        if 'extras' in config and 'optional-dependencies' not in dynamic:
+            raise RuntimeError("tool.vulcan.extras configured but 'optional-dependencies' not in dynamic,"
+                               " this is an error according to PEP-621. "
+                               "See https://peps.python.org/pep-0621/#dynamic for more information")
+
+        return cls(source_path=source_path,
+                   plugins=list_or_none(config.get('plugins')),
+                   lockfile=lockfile,
+                   shiv_options=shiv_ops,
+                   dependencies=install_requires,
+                   configured_dependencies=config.get('dependencies', {}),
+                   extras=extras_require,
+                   dev_dependencies=config.get('dev-dependencies', {}),
+                   configured_extras=config.get('extras', {}),
+                   no_lock=no_lock,
+                   python_lock_with=python_lock_with,
+                   dynamic=dynamic)
+
+    def setup(self, config_settings: Dict[str, str] = None) -> distutils.core.Distribution:
+        install_requires: Optional[List[str]]
+        extras_require: Optional[Dict[str, List[str]]]
+        if self.no_lock or (config_settings and config_settings.get('no-lock') == 'true'):
+            install_requires = flatten_reqs(self.configured_dependencies)
+            extras_require = self.configured_extras
+        else:
+            install_requires = self.dependencies
+            extras_require = self.extras
+        # setuptools apparently does not know what setuptools returns
+        # very reassuring
+        return setup(  # type: ignore[no-any-return,func-returns-value]
+            install_requires=install_requires,
+            extras_require=extras_require)
+
+
+def get_requires(lockfile: Path) -> Tuple[List[str], Dict[str, List[str]]]:
+    if not lockfile.exists():
+        raise FileNotFoundError(f"Expected lockfile {lockfile}, does not exist")
+    with lockfile.open() as f:
+        content = cast(tomlkit.container.Container, tomlkit.loads(f.read()))
+    return (
+        list(content['install_requires']),  # type: ignore
+        {k: list(v)
+         for k, v in content['extras_require'].items()})  # type: ignore
+
+
+def to_pep508(lib: str, req: Union[str, VersionDict]) -> str:
+    if not isinstance(req, (str, dict)):
+        raise VulcanConfigError(f"Invalid requirement {req} -- must be a dict or a string")
+    if isinstance(req, str):
+        # e.g. "example_lib", "~=1.2.3" -> "example_lib~=1.2.3"
+        return f'{lib}{req}'
+    try:
+        extras = f'[{",".join(req["extras"])}]' if 'extras' in req else ''
+        # "example_lib", {"version": "~=1.2.3", "extras"=["networkx","git"]}
+        #                           -> "example_lib[networkx,git]~=1.2.3"
+        # "example_lib", {"version": "~=1.2.3"} -> "example_lib~=1.2.3"
+        return f'{lib}{extras}{req["version"]}'
+    except KeyError as e:
+        raise VulcanConfigError(f'invalid requirement {lib} ({req}) -- {e}') from e
---
+++ packages/vulcan-py/vulcan_py-2.1.0-py3-none-any.whl/vulcan/cli.py
@@ -0,0 +1,247 @@
+import asyncio
+import asyncio.subprocess
+import os
+import shlex
+import subprocess
+import sys
+from pathlib import Path
+from typing import Callable, Dict, List, Optional, Tuple
+
+import build.env
+import click
+import packaging.version
+import tomlkit
+from pkg_resources import Requirement
+
+import build
+from vulcan import Vulcan, flatten_reqs
+from vulcan.build_backend import (get_pip_version, get_virtualenv_python,
+                                  install_develop)
+from vulcan.builder import resolve_deps
+
+version: Callable[[str], str]
+if sys.version_info >= (3, 8):
+    from importlib.metadata import PackageNotFoundError, version
+else:
+    from importlib_metadata import PackageNotFoundError, version
+
+pass_vulcan = click.make_pass_decorator(Vulcan)
+
+try:
+    vulcan_version = version('vulcan-py')
+except PackageNotFoundError:
+    vulcan_version = '0.0.0'
+
+
+@click.group()
+@click.version_option(vulcan_version)
+@click.pass_context
+def main(ctx: click.Context) -> None:
+    if sys.platform == 'win32':
+        # This is required for python <3.8, where this is not yet the default for windows.
+        # Can be removed when support for those are removed.
+        # https://docs.python.org/3.7/library/asyncio-eventloop.html#event-loop-implementations
+        #
+        # > By default asyncio is configured to use SelectorEventLoop on all platforms.
+        #
+        # And then https://docs.python.org/3.8/library/asyncio-eventloop.html#event-loop-implementations
+        #
+        # > By default asyncio is configured to use SelectorEventLoop on Unix and ProactorEventLoop on
+        #   Windows.
+        asyncio.set_event_loop(asyncio.ProactorEventLoop())
+    # don't fail on missing lock here, because this config object is not actually used for building only for
+    # cli values
+    ctx.obj = Vulcan.from_source(Path().absolute(), fail_on_missing_lock=False)
+
+
+async def build_shiv_apps(from_dist: str, vulcan: Vulcan, outdir: Path) -> List[Path]:
+    results = []
+    for app in vulcan.shiv_options:
+        try:
+            if app.with_extras:
+                dist = f'{from_dist}[{",".join(app.with_extras)}]'
+            else:
+                dist = from_dist
+            cmd = [sys.executable, '-m', 'shiv', dist, '-o', str(outdir / app.bin_name)]
+            if app.console_script:
+                cmd += ['-c', app.console_script]
+            if app.entry_point:
+                cmd += ['-e', app.entry_point]
+            if app.interpreter:
+                cmd += ['-p', app.interpreter]
+            if app.extra_args:
+                cmd += shlex.split(app.extra_args)
+            proc = await asyncio.subprocess.create_subprocess_exec(*cmd)
+            results.append((proc, app.bin_name))
+        except KeyError as e:
+            raise KeyError('missing config value in pyproject.toml: {e}') from e
+    await asyncio.gather(*(p.wait() for p, _ in results))
+    succeeded = []
+    failed = []
+    for proc, res in results:
+        if await proc.wait() == 0:
+            succeeded.append(outdir / res)
+        else:
+            failed.append(res)
+    for fail in failed:
+        print(f"failed to create executable {failed}", file=sys.stderr)
+    return succeeded
+
+
+@main.command(name='build')
+@click.option('--outdir', '-o', default='dist/', type=Path)
+@click.option('--lock/--no-lock', '_lock', default=True)
+@click.option('--wheel', is_flag=True, default=False)
+@click.option('--sdist', is_flag=True, default=False)
+@click.option('--shiv', is_flag=True, default=False)
+@pass_vulcan
+def build_out(config: Vulcan, outdir: Path, _lock: bool, wheel: bool, sdist: bool, shiv: bool) -> None:
+    "Create wheels, sdists, and shiv executables"
+    # for ease of use
+    if len([v for v in (shiv, wheel, sdist) if v]) != 1:
+        raise click.UsageError("Must specify exactly 1 of --shiv, --wheel, or --sdist")
+
+    should_lock = _lock and not config.no_lock
+
+    config_settings = {}
+    if not _lock:
+        config_settings['no-lock'] = 'true'
+    project = build.ProjectBuilder('.')
+    outdir.mkdir(exist_ok=True)
+    if sdist:
+        dist = project.build('sdist', str(outdir), config_settings=config_settings)
+    elif wheel or shiv:
+        if shiv and not should_lock:
+            raise click.UsageError("May not specify both --shiv and --no-lock; shiv builds must be locked")
+        dist = project.build('wheel', str(outdir), config_settings=config_settings)
+    else:
+        assert False, 'unreachable because dist_types is required'
+    if shiv:
+        try:
+            asyncio.get_event_loop().run_until_complete(build_shiv_apps(dist, config, outdir))
+        finally:
+            os.remove(dist)
+
+
+async def resolve_deps_or_report(config: Vulcan, python_version: str = None) -> Tuple[List[str], Dict[str, List[str]]]:
+    try:
+        return await resolve_deps(flatten_reqs(config.configured_dependencies),
+                                  config.configured_extras or {},
+                                  python_version)
+
+    except subprocess.CalledProcessError as e:
+        print(e.stderr.decode(), file=sys.stderr)
+        raise
+
+
+@main.command()
+@pass_vulcan
+def lock(config: Vulcan) -> None:
+    "Generate and update lockfile"
+
+    python_version = config.python_lock_with
+    # this check does not make sense on windows as far as I can tell,
+    # there is never a "python3.6" or "python2.7" binary just "python"
+    if python_version is None and sys.platform != 'win32':
+        try:
+            # default to configured lock value, then current venv value if it exists, fallback to vulcan's
+            # version
+            python = get_virtualenv_python()
+            python_version = subprocess.check_output(
+                [str(python), '-c', 'import sys; print(f"{sys.version_info.major}.{sys.version_info.minor}")'],
+                encoding='utf-8').strip()
+
+        except RuntimeError:
+            pass
+    install_requires, extras_require = asyncio.get_event_loop().run_until_complete(
+        resolve_deps_or_report(config, python_version))
+    doc = tomlkit.document()
+    doc['install_requires'] = tomlkit.array(install_requires).multiline(True)  # type: ignore
+    doc['extras_require'] = {
+        k: tomlkit.array(v).multiline(True)  # type: ignore
+        for k, v in extras_require.items()
+    }
+    with open(config.lockfile, 'w+') as f:
+        f.write(tomlkit.dumps(doc))
+
+
+@main.command()
+@click.argument('req', type=Requirement.parse)
+@click.option('--lock/--no-lock', '_lock', default=True)
+@pass_vulcan  # order matters, closest to the function definition comes first
+@click.pass_context
+def add(ctx: click.Context, config: Vulcan, req: Requirement, _lock: bool) -> None:
+    "Add new top-level dependency and regenerate lockfile"
+    name: str = req.name
+    if req.extras:
+        name = f'{name}[{",".join(req.extras)}]'
+    try:
+        venv_python = get_virtualenv_python()
+    except RuntimeError:
+        exit("Must be in a virtualenv to use `vulcan add`")
+    subprocess.check_call([str(venv_python), '-m', 'pip', 'install', str(req)])
+    if req.specifier:
+        # if the user gave a version spec, we blindly take that
+        version = str(req.specifier)
+    else:
+        # otherwise, we take a freeze to see what was actually installed
+        freeze = subprocess.check_output([str(venv_python), '-m', 'pip', 'freeze'], encoding='utf-8').strip()
+        try:
+            # try and find the thing we just added
+            line = next(ln for ln in freeze.split('\n') if ln.startswith(req.name))
+            # and parse it to a version
+            spec = packaging.version.parse(str(Requirement.parse(line.strip()).specifier
+                                               )[2:])  # remove the == at the start
+            version = f'~={spec.major}.{spec.minor}'
+        except StopIteration:
+            # failed to find the thing we just installed, give up.
+            version = ''
+    with open('pyproject.toml') as f:
+        parse = tomlkit.parse(f.read())
+    deps = parse['tool']['vulcan'].setdefault('dependencies', tomlkit.table())  # type: ignore
+    deps[name] = version
+    with open('pyproject.toml', 'w+') as f:
+        f.write(tomlkit.dumps(parse))
+    if not config.no_lock and _lock:
+        ctx.obj = Vulcan.from_source(Path().absolute(), fail_on_missing_lock=False)
+        ctx.invoke(lock)
+
+
+def install_dev_dependencies(target: str = None) -> None:
+    config = Vulcan.from_source(Path().absolute(), fail_on_missing_lock=False)
+
+    try:
+        virtual_env = get_virtualenv_python()
+    except RuntimeError:
+        exit('may not use vulcan develop outside of a virtualenv')
+    pip_version = get_pip_version(virtual_env)
+    if pip_version is None or pip_version < (21, 3):
+        print(f"pip version {pip_version} does not support editable installs for PEP517 projects,"
+              " Please upgrade your pip")
+
+    if target is not None and target not in config.dev_dependencies:
+        raise click.UsageError(f"No such dev dependency {target}")
+    if config.dev_dependencies:
+        for name, section in config.dev_dependencies.items():
+            if target is None or name == target:
+                print(f"Installing dev dependencies for {name}", flush=True)
+                # print(subprocess.check_output(...)) instead of just subprocess.check_call(...)
+                # purely because the CliRunner fixture isn't very good at actually capturing stdout, and it
+                # breaks pytest's capsys which _is_ usually good at that
+                # ah well
+                print(subprocess.check_output(
+                    [str(virtual_env), '-m', 'pip', 'install', *(flatten_reqs(section) or [])],
+                    encoding='utf-8'),
+                      flush=True)
+
+
+@main.command()
+@click.argument('dev_deps_target', required=False, type=str)
+@click.option('--build-isolation/--no-build-isolation', '_build_isolation', default=True)
+def develop(dev_deps_target: Optional[str], _build_isolation: bool) -> None:
+    install_develop(_build_isolation)
+    install_dev_dependencies(target=dev_deps_target)
+
+
+if __name__ == '__main__':
+    main()
