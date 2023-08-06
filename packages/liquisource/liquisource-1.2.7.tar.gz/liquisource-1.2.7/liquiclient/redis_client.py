8975edc53042beb2a06e83042ab02c6074eccbd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:52 +0000

    Add package liquisource/1.2.7/liquisource-1.2.7.tar.gz

---
+++ packages/liquisource/liquisource-1.2.7.tar.gz/liquisource-1.2.7/liquiclient/redis_client.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import redis
+from liquiclient.config import get_property
+
+
+# 获取mysql实例
+def get_redis_client():
+
+    host = get_property("redis.host")
+    port = get_property("redis.port")
+    username = get_property("redis.username")
+    password = get_property("redis.password")
+
+    client = redis.Redis(host=host, port=port, username=username, password=password, decode_responses=True)
+
+    return client
