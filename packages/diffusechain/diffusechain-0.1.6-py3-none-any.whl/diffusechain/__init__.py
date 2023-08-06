8846609796deed8fc3eeb2f00cd79458a090a21d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:22 +0000

    Add package diffusechain/0.1.6/diffusechain-0.1.6-py3-none-any.whl

---
+++ packages/diffusechain/diffusechain-0.1.6-py3-none-any.whl/diffusechain/__init__.py
@@ -0,0 +1,13 @@
+from .Automatic1111 import (
+    Automatic1111,
+    Automatic1111Result,
+    b64_img,
+)
+from .Diffusers import Diffusers
+
+__all__ = [
+    "Automatic1111",
+    "Automatic1111Result",
+    "b64_img",    
+    "Diffusers",
+]
