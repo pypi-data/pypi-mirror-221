25369dd4362c4d6214a7fedcc32a0345bde68bc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:52 +0000

    Add package tvolib/0.3.0/tvolib-0.3.0.tar.gz

---
+++ packages/tvolib/tvolib-0.3.0.tar.gz/tvolib-0.3.0/src/tvolib/mpl_utils/__init__.py
@@ -0,0 +1,9 @@
+import matplotlib as mpl
+from ._setup import setup
+from ._format import add_colorbar, format_datetime_axis
+from ._draw import draw_arrows, draw_earth, draw_multicolored_line
+
+
+mplc = mpl.colors
+plt = mpl.pyplot
+setup()
