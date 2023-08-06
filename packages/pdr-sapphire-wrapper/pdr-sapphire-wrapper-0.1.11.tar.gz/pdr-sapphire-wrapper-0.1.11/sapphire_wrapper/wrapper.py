303cc9d309e40c5343582f6ec991aa96d7a238f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:43 +0000

    Add package pdr-sapphire-wrapper/0.1.11/pdr-sapphire-wrapper-0.1.11.tar.gz

---
+++ packages/pdr-sapphire-wrapper/pdr-sapphire-wrapper-0.1.11.tar.gz/pdr-sapphire-wrapper-0.1.11/sapphire_wrapper/wrapper.py
@@ -0,0 +1,53 @@
+import os
+import ctypes
+import platform
+import sys
+
+lib = None
+bin_dir = os.path.join(sys.prefix, "sapphire_wrapper_bin")
+
+if platform.system() == "Darwin":
+    lib_path = os.path.join(bin_dir, "sapphirewrapper.dylib")
+    lib = ctypes.CDLL(lib_path)
+
+if platform.system() == "Windows":
+    raise Exception("Windows is not supported")
+
+if platform.system() == "Linux":
+    lib_path = os.path.join(bin_dir, "sapphirewrapper.so")
+    lib = ctypes.CDLL(lib_path)
+
+
+# Define argument types
+lib.SendETHTransaction.argtypes = [
+    ctypes.c_char_p,
+    ctypes.c_char_p,
+    ctypes.c_char_p,
+    ctypes.c_char_p,
+    ctypes.c_int,
+    ctypes.c_int,
+    ctypes.c_char_p,
+]
+
+# Define return type
+lib.SendETHTransaction.restype = ctypes.c_int
+
+
+def send_encrypted_sapphire_tx(
+    pk: str,
+    sender: str,
+    recipient: str,
+    rpc_url: str,
+    eth_amount: int,
+    gas_limit: int,
+    data: str,
+) -> int:
+    return lib.SendETHTransaction(
+        pk.encode("utf-8"),
+        sender.encode("utf-8"),
+        recipient.encode("utf-8"),
+        rpc_url.encode("utf-8"),
+        eth_amount,
+        gas_limit,
+        data.encode("utf-8"),
+    )
