9528911ec2445b67c6d04c9fcb54e7f3f9815f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:04 +0000

    Add package chatllm/2023.7.28.15.53.43/chatllm-2023.7.28.15.53.43.tar.gz

---
+++ packages/chatllm/chatllm-2023.7.28.15.53.43.tar.gz/chatllm-2023.7.28.15.53.43/chatllm/langchain/embeddings/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : __init__.py
+# @Time         : 2023/6/30 21:49
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  :
+
+from chatllm.langchain.embeddings.OpenAIEmbeddings import OpenAIEmbeddings
+from chatllm.langchain.embeddings.DashScopeEmbeddings import DashScopeEmbeddings
