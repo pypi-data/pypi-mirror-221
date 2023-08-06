669534da1fdd58604b4a459908996d2e08cad6c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:49 +0000

    Add package MapNester1/1.3.0/MapNester1-1.3.0.tar.gz

---
+++ packages/MapNester1/MapNester1-1.3.0.tar.gz/MapNester1-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name            = 'MapNester1',
+    version         = '1.3.0',
+    py_modules      = ['nester'],
+    author          = 'MapH4ck',
+    author_email    = 'test@test.com'  ,
+    url             = 'https://www.test.com',
+    description     = 'A simple printer of nested lists'
+)
