fc75bbc0b677d4fbcd97df396355b3f3d926e4c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:18 +0000

    Add package tdvisu/1.1.9/tdvisu-1.1.9.tar.gz

---
+++ packages/tdvisu/tdvisu-1.1.9.tar.gz/tdvisu-1.1.9/tdvisu/version.py
@@ -0,0 +1,10 @@
+"""
+Release versions have the form "M.N.P".
+Dev versions have the form "M.N.P-devN".
+"""
+
+# Base version.
+__version__ = '1.1.9'
+
+# Year-Month-Day of last version change
+__date__ = '2023-07-27'
