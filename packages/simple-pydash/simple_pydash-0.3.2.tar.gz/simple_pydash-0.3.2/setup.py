8b5fe77ba58fdbd309450a689801a6af0ad09654
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:01 +0000

    Add package simple-pydash/0.3.2/simple_pydash-0.3.2.tar.gz

---
+++ packages/simple-pydash/simple_pydash-0.3.2.tar.gz/simple_pydash-0.3.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="simple_pydash",
+    version="0.3.2",
+    author="Valerio Biscione",
+    author_email="valerio.biscione@gmail.com",
+    description="Lightweight, modular Python library for creating real-time interactive dashboards in the web browser.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ValerioB88/simple-pydash",
+    packages=["simple_pydash"],
+    include_package_data=True,
+    install_requires=[
+        "fastapi",
+        "uvicorn[standard]",
+        "numpy",
+        "Pillow",
+        "plotly",
+        "sty",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
