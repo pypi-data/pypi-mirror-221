7694e99e337272fc812c8fc3cb4ff40c77e7bc69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:59 +0000

    Add package prophecy-libs/1.5.10.dev0/prophecy-libs-1.5.10.dev0.tar.gz

---
+++ packages/prophecy-libs/prophecy-libs-1.5.10.dev0.tar.gz/prophecy-libs-1.5.10.dev0/prophecy/utils/transpiler/dml_schema.py
@@ -0,0 +1,9 @@
+# This contains wrappers over dml_schema implemented in scala
+
+from prophecy.utils.transpiler.abi_base import ScalaUtil
+
+def parse(schema):
+    spark = ScalaUtil.getAbiLib().spark
+    jvm = spark.sparkContext._jvm
+    return jvm.io.prophecy.abinitio.dml.DMLSchema.parsePy(schema)
+
