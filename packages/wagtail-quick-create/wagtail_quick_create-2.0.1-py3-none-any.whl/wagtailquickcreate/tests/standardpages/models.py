1274aca9bb6c6a46981ed1e737bdd1bb217180ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:12 +0000

    Add package wagtail-quick-create/2.0.1/wagtail_quick_create-2.0.1-py3-none-any.whl

---
+++ packages/wagtail-quick-create/wagtail_quick_create-2.0.1-py3-none-any.whl/wagtailquickcreate/tests/standardpages/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from wagtail.models import Page
+
+
+class InformationPage(Page):
+    parent_page_types = ['standardpages.IndexPage']
+
+    introduction = models.TextField(blank=True)
+
+
+class IndexPage(Page):
+
+    introduction = models.TextField(blank=True)
