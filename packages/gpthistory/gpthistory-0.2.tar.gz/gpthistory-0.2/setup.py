1557ec299e713b974c5434360d990892b60af26f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:08 +0000

    Add package gpthistory/0.2/gpthistory-0.2.tar.gz

---
+++ packages/gpthistory/gpthistory-0.2.tar.gz/gpthistory-0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gpthistory',
+    version='0.2',
+    description='A tool for searching through your chatgpt conversation history',
+    author='Shrikar Archak',
+    author_email='shrikar84@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Click',
+        'dotenv',
+        'openai',
+        'pandas',
+        'numpy'
+    ],
+    entry_points='''
+        [console_scripts]
+        gpthistory=gpthistory.gpthistory:main
+    ''',
+)
