2e2e455373e72a3d94f91df75ccaa2b03edec7d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:28 +0000

    Add package exy/0.1.0/exy-0.1.0.tar.gz

---
+++ packages/exy/exy-0.1.0.tar.gz/exy-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='exy',
+    version='0.1.0',
+    author='Yex',
+    description='A module testing',
+    packages=find_packages(),
+    install_requires=[],
+)
