6e63f305d33b2da846f2cdbb94ac06361852e058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:04 +0000

    Add package gik-distributions/0.1/gik-distributions-0.1.tar.gz

---
+++ packages/gik-distributions/gik-distributions-0.1.tar.gz/gik-distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='gik-distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['gik-distributions'],
+      zip_safe=False)
