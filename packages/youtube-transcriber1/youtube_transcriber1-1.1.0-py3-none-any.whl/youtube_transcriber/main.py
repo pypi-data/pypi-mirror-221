06a873ca297283c487dce4b13ddda4cf0afc4681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:34 +0000

    Add package youtube-transcriber1/1.1.0/youtube_transcriber1-1.1.0-py3-none-any.whl

---
+++ packages/youtube-transcriber1/youtube_transcriber1-1.1.0-py3-none-any.whl/youtube_transcriber/main.py
@@ -0,0 +1,12 @@
+import pytube as pt
+import whisper
+import platform
+import subprocess
+import tkinter as tk
+from tkinter import ttk
+
+
+from youtube_transcriber.gui.transcriber import root
+
+if __name__ == "__main__":
+    root.mainloop()
