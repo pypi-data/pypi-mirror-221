45ab02b6ea8fc37a0a3177ac2a8839234e402994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:59 +0000

    Add package hydromt-wflow/0.3.0/hydromt_wflow-0.3.0-py3-none-any.whl

---
+++ packages/hydromt-wflow/hydromt_wflow-0.3.0-py3-none-any.whl/hydromt_wflow/__init__.py
@@ -0,0 +1,18 @@
+"""hydroMT plugin for wflow models."""
+
+from os.path import join, dirname, abspath
+
+__version__ = "0.3.0"
+
+try:
+    import pcraster as pcr
+
+    HAS_PCRASTER = True
+except ImportError:
+    HAS_PCRASTER = False
+
+DATADIR = join(dirname(abspath(__file__)), "data")
+
+from .wflow import *
+from .wflow_sediment import *
+from .utils import *
