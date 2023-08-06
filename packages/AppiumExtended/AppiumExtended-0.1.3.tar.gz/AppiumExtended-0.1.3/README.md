f3eb42c1fafe61b75be87dba1e7b06180447492e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:33:50 +0000

    Add package AppiumExtended/0.1.3/AppiumExtended-0.1.3.tar.gz

---
+++ packages/AppiumExtended/AppiumExtended-0.1.3.tar.gz/AppiumExtended-0.1.3/README.md
@@ -0,0 +1,11 @@
+# appium_extended
+An extension library for adding ease of use Appium-Python-Client
+
+Required to be installed appium cli with plugins: appium-device-farm, appium-dashboard.
+
+`_npm i -g appium@next
+appium driver install uiautomator2
+appium plugin install --source=npm appium-device-farm
+appium plugin install --source=npm appium-dashboard_`
+
+
---
+++ packages/AppiumExtended/AppiumExtended-0.1.3.tar.gz/AppiumExtended-0.1.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='AppiumExtended',
+    version='0.1.3',
+    description='An extension library for adding ease of use Appium-Python-Client',
+    author='molokov-klim',
+    packages=find_packages(),
+    install_requires=[
+        'Appium-Python-Client==2.11.1',
+        'allure-pytest==2.13.2',
+        'zlib-compress==0.0.1',
+        'zlib-decompress==0.0.2',
+        'pylibjpeg==1.4.0',
+        'Pillow==9.5.0',
+        'requests==2.31.0',
+        'pyserial==3.5',
+        'opencv-python==4.8.0.74',
+        'pytesseract==0.3.10',
+        'numpy~=1.25.1',
+        'selenium~=4.10.0',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
+
