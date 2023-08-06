86e569eceaa07f2a307e5535c40c418e180dbc99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:21 +0000

    Add package waterline/0.2.0/waterline-0.2.0-py3-none-any.whl

---
+++ packages/waterline/waterline-0.2.0-py3-none-any.whl/waterline/suites/__init__.py
@@ -0,0 +1,11 @@
+# Core benchmarks
+from .polybench import PolyBench
+from .mibench import MiBench
+from .nas import NAS
+from .gap import GAP
+from .spec2017 import SPEC2017
+from .embench import Embench
+
+# Misc 'benchmarks'
+from .stockfish import Stockfish
+from .sqlite_tpch import SqliteTPCH
