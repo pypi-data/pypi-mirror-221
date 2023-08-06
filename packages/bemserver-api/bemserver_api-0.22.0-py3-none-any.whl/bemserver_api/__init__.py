5a31685c76f74ddbdfdef0c2f8949ab9c944de4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:09 +0000

    Add package bemserver-api/0.22.0/bemserver_api-0.22.0-py3-none-any.whl

---
+++ packages/bemserver-api/bemserver_api-0.22.0-py3-none-any.whl/bemserver_api/__init__.py
@@ -0,0 +1,47 @@
+"""BEMServer API"""
+import flask
+
+from bemserver_core import BEMServerCore
+
+from . import database
+from .extensions import (  # noqa
+    Api,
+    Blueprint,
+    Schema,
+    AutoSchema,
+    SQLCursorPage,
+    authentication,
+)
+from .resources import register_blueprints
+
+
+__version__ = "0.22.0"
+
+API_VERSION = __version__
+OPENAPI_VERSION = "3.1.0"
+
+
+def create_app(config_override=None):
+    """Create application
+
+    :param type config_override: Config class overriding default config.
+        Used for tests.
+    """
+    app = flask.Flask(__name__)
+    app.config.from_object("bemserver_api.settings.Config")
+    app.config.from_envvar("BEMSERVER_API_SETTINGS_FILE", silent=True)
+    app.config.from_object(config_override)
+
+    database.init_app(app)
+    api = Api(
+        spec_kwargs={
+            "version": API_VERSION,
+            "openapi_version": OPENAPI_VERSION,
+        }
+    )
+    api.init_app(app)
+    register_blueprints(api)
+
+    BEMServerCore()
+
+    return app
