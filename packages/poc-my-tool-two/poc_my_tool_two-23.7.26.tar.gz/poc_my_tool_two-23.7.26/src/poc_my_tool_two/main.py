10a3d60a8cdccadf923b5702995ad765b3e14c80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:48 +0000

    Add package poc-my-tool-two/23.7.26/poc_my_tool_two-23.7.26.tar.gz

---
+++ packages/poc-my-tool-two/poc_my_tool_two-23.7.26.tar.gz/poc_my_tool_two-23.7.26/src/poc_my_tool_two/main.py
@@ -0,0 +1,13 @@
+from poc_my_common_library_two.classes.process import Process
+
+
+def main():
+    print('main from poc-my-tool-two')
+
+    process = Process()
+
+    print(process.info())
+
+
+if __name__ == '__main__':
+    main()
