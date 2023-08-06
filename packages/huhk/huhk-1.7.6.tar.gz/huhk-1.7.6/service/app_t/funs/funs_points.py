6db531195a1c8edb855bcc0d79cda3b1e6085ee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:21 +0000

    Add package huhk/1.7.6/huhk-1.7.6.tar.gz

---
+++ packages/huhk/huhk-1.7.6.tar.gz/huhk-1.7.6/service/app_t/funs/funs_points.py
@@ -0,0 +1,7 @@
+from service.app_t.funs.points.funs_points_pointsApp import FunsPointsPointsapp
+from service.app_t.funs.points.funs_points_points import FunsPointsPoints
+
+
+class FunsPoints(FunsPointsPointsapp, FunsPointsPoints):
+    pass
+
