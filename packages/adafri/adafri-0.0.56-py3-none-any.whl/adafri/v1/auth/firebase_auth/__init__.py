0bb739f8c698cd10e390b49816bb1e0300129986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:56 +0000

    Add package adafri/0.0.56/adafri-0.0.56-py3-none-any.whl

---
+++ packages/adafri/adafri-0.0.56-py3-none-any.whl/adafri/v1/auth/firebase_auth/__init__.py
@@ -0,0 +1,2 @@
+from .firebase_auth import (FirestoreApp, create_firebase_user, create_firebase_custom_token, set_custom_claims,
+                            generate_email_verification_link, generate_password_reset_link, update_firebase_user)
