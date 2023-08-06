cf1e38146e9393e955fc2e353b4bc8dc691cc1b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:42 +0000

    Add package chiku/0.0.22/chiku-0.0.22.tar.gz

---
+++ packages/chiku/chiku-0.0.22.tar.gz/chiku-0.0.22/tests/test_remez.py
@@ -0,0 +1,19 @@
+import unittest
+from chiku import chebyshev, remez
+import numpy as np
+import random
+
+class Test_Remez(unittest.TestCase):
+	def test_remez(self):
+		def sigmoid(x):
+			return 1/(1+np.exp(-x))
+
+		self.cpoly = chebyshev.chebyshev(sigmoid, degree=6)
+		self.poly = remez.remez(sigmoid, self.cpoly, degree=4)
+
+		self.rnum = random.random()
+		self.assertAlmostEqual(self.poly.predict(self.rnum), sigmoid(self.rnum), 2)
+
+if __name__ == '__main__':
+	unittest.main()
+
