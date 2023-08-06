cf5cab222b5c800ab265bb030662a94c025349be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:35:37 +0000

    Add package emzed/3.0.0a13/emzed-3.0.0a13-cp311-cp311-win_amd64.whl

---
+++ packages/emzed/emzed-3.0.0a13-cp311-cp311-win_amd64.whl/emzed/pyopenms/__init__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+import numpy as np
+
+from emzed.remote_package import RemoteModule, setup_remote_venv
+
+if "pyopenms" not in sys.modules:
+    here = os.path.dirname(os.path.abspath(__file__))
+    env_path = os.path.join(here, "_pyopenms_venv")
+
+    PYOPENMS_VERSION = "3.0.0"
+
+    python_venv_exe = setup_remote_venv(
+        env_path,
+        [
+            ("pyopenms", PYOPENMS_VERSION),
+            ("numpy", str(np.__version__)),
+        ],
+    )
+
+    pyopenms = sys.modules["pyopenms"] = RemoteModule(python_venv_exe, "pyopenms")
+    pyopenms.load_optimizations(os.path.join(here, "optimizations.py"))
+
+
+def encode(s):
+    if isinstance(s, str):
+        return s.encode("utf-8")
+    return s
+
+
+def decode(s):
+    if isinstance(s, bytes):
+        return str(s, "utf-8")
+    return s
