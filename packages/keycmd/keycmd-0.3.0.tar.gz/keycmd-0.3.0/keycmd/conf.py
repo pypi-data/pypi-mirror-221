5f79af491aebca26bc8cf195ba51b3ac5c29fc82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:10 +0000

    Add package keycmd/0.3.0/keycmd-0.3.0.tar.gz

---
+++ packages/keycmd/keycmd-0.3.0.tar.gz/keycmd-0.3.0/keycmd/conf.py
@@ -0,0 +1,89 @@
+from pathlib import Path
+from pprint import pformat
+
+import tomli
+
+from .logs import vlog
+
+
+# exposed for testing
+USERPROFILE = "~"
+
+
+def load_toml(path):
+    """Load a toml file"""
+    with path.open("rb") as fh:
+        try:
+            return tomli.load(fh)
+        except tomli.TOMLDecodeError as err:
+            raise tomli.TOMLDecodeError(f"invalid TOML in {path}:\n{err}")
+
+
+def load_pyproj(path):
+    """Load [tool.keycmd] from a pyproject.toml file"""
+    data = load_toml(path)
+    return data.get("tool", {}).get("keycmd", {})
+
+
+def defaults():
+    """Generate the default config"""
+    return {"keys": {}}
+
+
+def merge_conf(a, b):
+    """
+    Merges two deep dictionary structures.
+    All other datatypes are simply overwritten
+    """
+    a = a.copy()
+    for key, value in b.items():
+        if isinstance(value, dict):
+            old_value = a.setdefault(key, {})
+            a[key] = merge_conf(old_value, value)
+        else:
+            a[key] = value
+    return a
+
+
+def load_conf():
+    """
+    Load merged configuration from the following files:
+    - defaults()
+    - ~/.keycmd
+    - first pyproject.toml found while walking file system up from .
+    - ./.keycmd
+    """
+    conf = defaults()
+    cwd = Path.cwd()
+
+    # ~/.keycmd
+    fpath = (Path(USERPROFILE).expanduser() / ".keycmd").resolve()
+    if fpath.is_file():
+        vlog(f"loading config file {fpath}")
+        conf = merge_conf(conf, load_toml(fpath))
+
+    # pyproject.toml
+    cur = cwd
+    while True:
+        pyproj = cur / "pyproject.toml"
+        if pyproj.is_file():
+            vlog(f"loading config file {pyproj}")
+            conf = merge_conf(conf, load_pyproj(pyproj))
+            break
+        # stop at the boundary of git repositories
+        if (cur / ".git").is_dir():
+            break
+        # stop if we can't go up anymore
+        if cur.parent == cur:
+            break
+        cur = cur.parent
+
+    # ./.keycmd
+    fpath = cwd / ".keycmd"
+    if fpath.is_file():
+        vlog(f"loading config file {fpath}")
+        conf = merge_conf(conf, load_toml(fpath))
+
+    vlog(f"merged config:\n{pformat(conf)}")
+
+    return conf
