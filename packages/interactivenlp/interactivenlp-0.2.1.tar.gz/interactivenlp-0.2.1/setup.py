92b6a80903c297ffd3b428cc36b3a55c56f6686a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:35 +0000

    Add package interactivenlp/0.2.1/interactivenlp-0.2.1.tar.gz

---
+++ packages/interactivenlp/interactivenlp-0.2.1.tar.gz/interactivenlp-0.2.1/setup.py
@@ -0,0 +1,46 @@
+from setuptools import find_packages
+from setuptools import setup
+from setuptools.command.install import install
+
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+
+    def run(self):
+        # Install nltk punkt
+        import nltk
+        nltk.download('punkt')
+        install.run(self)
+
+
+with open('README.md', "r") as f:
+    long_description = f.read()
+
+setup(
+    name='interactivenlp',
+    version='0.2.1',
+    description='Interactive NLP',
+    long_description=long_description,
+    author="Chunxu Yang",
+    author_email="chunxuyang@ucla.edu",
+    install_requires=[
+        'flask',
+        'flask-cors',
+        'newspaper3k',
+        'nltk',
+        'feedparser',
+        'pydantic',
+    ],
+    license='MIT',
+    packages=find_packages(
+        exclude=["examples", "tests"]
+    ),
+    platforms=["all"],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    cmdclass={
+        'install': PostInstallCommand,
+    },
+)
