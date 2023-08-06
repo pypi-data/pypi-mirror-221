c9b0f536129d0a24b53f45c3a6134d042eed8241
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:23 +0000

    Add package codecov-cli/0.1.17/codecov_cli-0.1.17-cp310-cp310-win_amd64.whl

---
+++ packages/codecov-cli/codecov_cli-0.1.17-cp310-cp310-win_amd64.whl/codecov_cli/commands/create_report_result.py
@@ -0,0 +1,69 @@
+import logging
+import uuid
+
+import click
+
+from codecov_cli.fallbacks import CodecovOption, FallbackFieldEnum
+from codecov_cli.helpers.git import GitService
+from codecov_cli.services.report import create_report_results_logic
+
+logger = logging.getLogger("codecovcli")
+
+
+@click.command()
+@click.option(
+    "--commit-sha",
+    help="Commit SHA (with 40 chars)",
+    cls=CodecovOption,
+    fallback_field=FallbackFieldEnum.commit_sha,
+    required=True,
+)
+@click.option(
+    "--code", help="The code of the report. If unsure, leave default", default="default"
+)
+@click.option(
+    "--slug",
+    cls=CodecovOption,
+    fallback_field=FallbackFieldEnum.slug,
+    help="owner/repo slug used instead of the private repo token in Self-hosted",
+    envvar="CODECOV_SLUG",
+    required=True,
+)
+@click.option(
+    "--git-service",
+    cls=CodecovOption,
+    fallback_field=FallbackFieldEnum.git_service,
+    type=click.Choice(service.value for service in GitService),
+)
+@click.option(
+    "-t",
+    "--token",
+    help="Codecov upload token",
+    type=click.UUID,
+    envvar="CODECOV_TOKEN",
+)
+@click.pass_context
+def create_report_results(
+    ctx,
+    commit_sha: str,
+    code: str,
+    slug: str,
+    git_service: str,
+    token: uuid.UUID,
+):
+    enterprise_url = ctx.obj.get("enterprise_url")
+    logger.debug(
+        "Creating report results",
+        extra=dict(
+            extra_log_attributes=dict(
+                commit_sha=commit_sha,
+                code=code,
+                slug=slug,
+                service=git_service,
+                enterprise_url=enterprise_url,
+            )
+        ),
+    )
+    create_report_results_logic(
+        commit_sha, code, slug, git_service, token, enterprise_url
+    )
