18b84c9a6127b244e49ea81f2219506d738bc2ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:30 +0000

    Add package vdk-jupyterlab-extension/0.1.946098977/vdk_jupyterlab_extension-0.1.946098977.tar.gz

---
+++ packages/vdk-jupyterlab-extension/vdk_jupyterlab_extension-0.1.946098977.tar.gz/vdk_jupyterlab_extension-0.1.946098977/src/vdkOptions/vdk_options.ts
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2023-2023 VMware, Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export enum VdkOption {
+  NAME = 'jobName',
+  TEAM = 'jobTeam',
+  PATH = 'jobPath',
+  CLOUD = 'cloud',
+  LOCAL = 'local',
+  ARGUMENTS = 'jobArguments',
+  DEPLOYMENT_REASON = 'deploymentReason',
+  DEPLOY_ENABLE = 'enableDeploy'
+}
