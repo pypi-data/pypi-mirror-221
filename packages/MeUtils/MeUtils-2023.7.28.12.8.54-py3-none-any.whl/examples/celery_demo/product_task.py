ec1e2d9eeea66ad998524be2060b99deb804c960
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:01 +0000

    Add package MeUtils/2023.7.28.12.8.54/MeUtils-2023.7.28.12.8.54-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2023.7.28.12.8.54-py3-none-any.whl/examples/celery_demo/product_task.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : product_task
+# @Time         : 2023/7/27 09:02
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 写成服务
+
+from meutils.pipe import *
+from celery_tasks.task01 import send_email
+from celery_tasks.task02 import send_msg
+
+
+# get/post: 100
+for i in range(10):
+    task = send_email.delay(i)
+    logger.info(f"Task: {task.id}")
+    task = send_msg.delay(i)
+    logger.info(f"Task: {task.id}")
