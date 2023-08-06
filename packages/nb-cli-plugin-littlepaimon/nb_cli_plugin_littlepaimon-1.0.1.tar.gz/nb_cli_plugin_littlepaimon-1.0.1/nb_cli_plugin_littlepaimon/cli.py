a39db601ccb0ca33605d0b136c936f4ca9b70084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:06 +0000

    Add package nb-cli-plugin-littlepaimon/1.0.1/nb_cli_plugin_littlepaimon-1.0.1.tar.gz

---
+++ packages/nb-cli-plugin-littlepaimon/nb_cli_plugin_littlepaimon-1.0.1.tar.gz/nb_cli_plugin_littlepaimon-1.0.1/nb_cli_plugin_littlepaimon/cli.py
@@ -0,0 +1,71 @@
+from typing import List, cast
+
+import click
+from nb_cli import _
+from nb_cli.cli import CLI_DEFAULT_STYLE, ClickAliasedGroup, run_sync, run_async
+from noneprompt import Choice, ListPrompt, CancelledError
+
+from . import __version__
+from .meta import LOGO
+
+
+@click.group(cls=ClickAliasedGroup,
+             invoke_without_command=True,
+             help='管理 LittlePaimon.')
+@click.version_option(
+    __version__,
+    "-V",
+    "--version",
+    prog_name="paimon",
+    message="%(prog)s: NB CLI plugin version %(version)s for LittlePaimon",
+)
+@click.pass_context
+@run_async
+async def paimon(ctx: click.Context):
+    """为 LittlePaimon 定制的Nonebot CLI 插件."""
+    if ctx.invoked_subcommand is not None:
+        return
+
+    command = cast(ClickAliasedGroup, ctx.command)
+
+    # auto discover sub commands and scripts
+    choices: List[Choice[click.Command]] = []
+    for sub_cmd_name in await run_sync(command.list_commands)(ctx):
+        if sub_cmd := await run_sync(command.get_command)(ctx, sub_cmd_name):
+            choices.append(
+                Choice(
+                    sub_cmd.help
+                    or _("Run subcommand {sub_cmd.name!r}").format(sub_cmd=sub_cmd),
+                    sub_cmd,
+                )
+            )
+    click.secho(LOGO, fg='green', bold=True)
+    click.secho('欢迎来到小派蒙的Nonebot CLI 插件!', fg='green', bold=True)
+
+    try:
+        result = await ListPrompt(
+            '你想要进行什么操作?', choices=choices
+        ).prompt_async(style=CLI_DEFAULT_STYLE)
+    except CancelledError:
+        ctx.exit()
+
+    sub_cmd = result.data
+    await run_sync(ctx.invoke)(sub_cmd)
+
+
+from .commands import create, install, resources, run, update
+
+paimon.add_command(create)
+paimon.add_command(run)
+paimon.add_command(install)
+paimon.add_command(update)
+paimon.add_command(resources)
+
+
+@paimon.command(
+    aliases=['show'],
+    help='展示小派蒙的LOGO.'
+)
+def logo():
+    click.secho(LOGO, fg='green', bold=True)
+    click.secho('欢迎来到小派蒙的Nonebot CLI插件!', fg='green', bold=True)
---
+++ packages/nb-cli-plugin-littlepaimon/nb_cli_plugin_littlepaimon-1.0.1.tar.gz/nb_cli_plugin_littlepaimon-1.0.1/nb_cli_plugin_littlepaimon/handlers/gocq.py
@@ -0,0 +1,58 @@
+import platform
+import shutil
+from pathlib import Path
+
+from cpuinfo import get_cpu_info
+from nb_cli.consts import WINDOWS
+
+from .download import download_with_bar
+
+_SYSTEM_MAP = {
+    "Windows": "windows",
+    "Linux":   "linux",
+    "Darwin":  "darwin",
+}
+
+_ARCHITECTURE_MAP = {
+    "X86_32": "386",
+    "X86_64": "amd64",
+    "ARM_7":  "armv7",
+    "ARM_8":  "arm64",
+}
+
+
+def _get_platform():
+    """
+    获取系统和架构
+
+    :return: 系统和架构的元组。
+    """
+    try:
+        system = _SYSTEM_MAP[platform.system()]
+        architecture = _ARCHITECTURE_MAP[get_cpu_info()["arch"]]
+    except KeyError:
+        raise RuntimeError(f"Unsupported platform: {platform.uname()!r}") from None
+    return system, architecture
+
+
+GOOS, GOARCH = _get_platform()
+
+ARCHIVE_EXT = ".zip" if WINDOWS else ".tar.gz"
+EXECUTABLE_EXT = ".exe" if WINDOWS else ""
+
+GOCQ_DIR = Path() / "go-cqhttp"
+
+
+def download_gocq(download_domain: str = 'github.com'):
+    """
+    下载gocq
+
+    :param download_domain: 下载源
+    """
+    GOCQ_DIR.mkdir(parents=True, exist_ok=True)
+    download_path = Path() / f'go-cqhttp_{GOOS}_{GOARCH}{ARCHIVE_EXT}'
+    url = f'https://{download_domain}/Mrs4s/go-cqhttp/releases/latest/download/go-cqhttp_{GOOS}_{GOARCH}{ARCHIVE_EXT}'
+    download_with_bar(url=url, save_path=download_path, show_name=f'go-cqhttp_{GOOS}_{GOARCH}{ARCHIVE_EXT}')
+    shutil.unpack_archive(download_path, extract_dir=GOCQ_DIR)
+    download_path.unlink()
+    return GOCQ_DIR / f'go-cqhttp{EXECUTABLE_EXT}'
