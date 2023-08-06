43bd83b8f00fff05ba578106cecc5d448d99bc2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:49 +0000

    Add package radstar/0.15.1/radstar-0.15.1-py3-none-any.whl

---
+++ packages/radstar/radstar-0.15.1-py3-none-any.whl/radstar/rs/cli/deps.py
@@ -0,0 +1,34 @@
+# -------------------------------------------------------------------------------------------------------------------- #
+
+# Copyright © 2021-2023 Peter Mathiasson
+# SPDX-License-Identifier: ISC
+
+# -------------------------------------------------------------------------------------------------------------------- #
+
+import subprocess
+import os
+
+from . import cli
+from .. import init_radstar, list_apps, shsplit
+
+# -------------------------------------------------------------------------------------------------------------------- #
+
+@cli.command()
+def update_deps():
+    ''' Update and install Python dependencies. '''
+
+    init_radstar(no_init=True)
+
+    in_files = []
+    for x in list_apps():
+        x = os.path.join(x.dir, 'requirements.in')
+        if os.path.exists(x):
+            in_files.append(x)
+
+    subprocess.check_call(
+        shsplit('pip-compile --resolver=backtracking --allow-unsafe --generate-hashes --upgrade -o requirements.txt'
+                ' --no-header') + in_files, cwd='/rs/project')
+
+    subprocess.check_call(shsplit('python -m pip install -r requirements.txt --require-virtualenv'), cwd='/rs/project')
+
+# -------------------------------------------------------------------------------------------------------------------- #
