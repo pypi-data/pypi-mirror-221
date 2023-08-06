eec6a6a66d150059fab27e544187c455e75e8aa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:50:13 +0000

    Add package gpcal/0.9.5.0/gpcal-0.9.5.0.tar.gz

---
+++ packages/gpcal/gpcal-0.9.5.0.tar.gz/gpcal-0.9.5.0/gpcal/__init__.py
@@ -0,0 +1,11 @@
+
+import gpcal.gpcal
+import gpcal.channelcal
+import gpcal.timecal
+import gpcal.obshelpers
+import gpcal.plothelpers
+import gpcal.synthetic
+import gpcal.aipsutil
+import gpcal.cleanhelpers
+import gpcal.polsolver
+
