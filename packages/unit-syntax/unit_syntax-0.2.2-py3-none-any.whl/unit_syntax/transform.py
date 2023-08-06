75853361c370c77ea22c642ab8f731f6f7e3efcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:55 +0000

    Add package unit-syntax/0.2.2/unit_syntax-0.2.2-py3-none-any.whl

---
+++ packages/unit-syntax/unit_syntax-0.2.2-py3-none-any.whl/unit_syntax/transform.py
@@ -0,0 +1,41 @@
+import ast
+from .parser import parse_string, UnitsExpr
+import pint
+
+
+class UnitExprTransformer(ast.NodeTransformer):
+    """
+    AST transformer to turn python-with-units into standard python
+    """
+
+    ureg: pint.UnitRegistry
+
+    def __init__(self, ureg: pint.UnitRegistry):
+        self.ureg = ureg
+
+    def generic_visit(self, node):
+        if isinstance(node, UnitsExpr):
+            # Check units are valid
+            try:
+                self.ureg.parse_units(node.units.value)
+            except pint.UndefinedUnitError as e:
+                raise SyntaxError(e)
+
+            value = self.generic_visit(node.value)
+            return ast.Call(
+                ast.Name(id="_unit_syntax_q", ctx=ast.Load()),
+                args=[value, node.units],
+                keywords=[],
+            )
+        else:
+            return super().generic_visit(node)
+
+
+def transform(code: str) -> str:
+    """Transform a string of python-with-units into a standard python string"""
+
+    tree = parse_string(code, mode="file")
+    ureg = pint._DEFAULT_REGISTRY
+    tree_std = UnitExprTransformer(ureg).visit(tree)
+    tree_std = ast.fix_missing_locations(tree_std)
+    return ast.unparse(tree_std)
