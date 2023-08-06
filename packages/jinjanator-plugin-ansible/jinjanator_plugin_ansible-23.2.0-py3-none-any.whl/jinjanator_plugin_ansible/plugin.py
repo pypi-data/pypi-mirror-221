c657999c8e29e32047b563c4b4ab070a5f4cd466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:17 +0000

    Add package jinjanator-plugin-ansible/23.2.0/jinjanator_plugin_ansible-23.2.0-py3-none-any.whl

---
+++ packages/jinjanator-plugin-ansible/jinjanator_plugin_ansible-23.2.0-py3-none-any.whl/jinjanator_plugin_ansible/plugin.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+
+from typing import cast
+
+import ansible.plugins.filter.core as filter_core  # type: ignore[import]
+import ansible.plugins.filter.mathstuff as filter_mathstuff  # type: ignore[import]
+import ansible.plugins.filter.urls as filter_urls  # type: ignore[import]
+import ansible.plugins.filter.urlsplit as filter_urlsplit  # type: ignore[import]
+import ansible.plugins.test.core as test_core  # type: ignore[import]
+import ansible.plugins.test.files as test_files  # type: ignore[import]
+import ansible.plugins.test.mathstuff as test_mathstuff  # type: ignore[import]
+import ansible.plugins.test.uri as test_uri  # type: ignore[import]
+
+from jinjanator_plugins import (
+    Filters,
+    Tests,
+    plugin_filters_hook,
+    plugin_tests_hook,
+)
+
+
+@plugin_filters_hook
+def plugin_filters() -> Filters:
+    return cast(
+        Filters,
+        {
+            **filter_core.FilterModule().filters(),
+            **filter_mathstuff.FilterModule().filters(),
+            **filter_urls.FilterModule().filters(),
+            **filter_urlsplit.FilterModule().filters(),
+        },
+    )
+
+
+@plugin_tests_hook
+def plugin_tests() -> Tests:
+    return cast(
+        Tests,
+        {
+            **test_core.TestModule().tests(),
+            **test_files.TestModule().tests(),
+            **test_mathstuff.TestModule().tests(),
+            **test_uri.TestModule().tests(),
+        },
+    )
