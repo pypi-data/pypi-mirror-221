673cb61d119ff4f7051e7d195a5516c9b6b12eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:53:05 +0000

    Add package ShanHeuniversity/0.1.1/ShanHeuniversity-0.1.1-py3-none-any.whl

---
+++ packages/ShanHeuniversity/ShanHeuniversity-0.1.1-py3-none-any.whl/ShanHeuniversity/MobAI/webapi/common.py
@@ -0,0 +1,18 @@
+from .ApiData import A
+
+__url__ = 'https://data.shuvtai.cn/MobAI/API/URL/data.txt'
+
+
+class URL(A):
+    def __init__(self, _url):
+        A.__init__(
+            self,
+            {
+                'chat': _url + '/chat',
+                'new': _url + '/new',
+                'apply': _url + '/apply',
+            }
+        )
+        self.chat = _url + '/chat'
+        self.new = _url + '/new'
+        self.apply = _url + '/apply'
