2c97664fccac5153cad0464aae02c4b05f7e8edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:31 +0000

    Add package nasa-wildfires/1.2.0/nasa-wildfires-1.2.0.tar.gz

---
+++ packages/nasa-wildfires/nasa-wildfires-1.2.0.tar.gz/nasa-wildfires-1.2.0/nasa_wildfires/options.py
@@ -0,0 +1,17 @@
+REGION_DICT = {
+    "global": "Global",
+    "canada": "Canada",
+    "alaska": "Alaska",
+    "usa-hawaii": "USA_contiguous_and_Hawaii",
+    "central-america": "Central_America",
+    "south-america": "South_America",
+    "europe": "Europe",
+    "north-central-africa": "Northern_and_Central_Africa",
+    "southern-africa": "Southern_Africa",
+    "russia-asia": "Russia_Asia",
+    "south-asia": "South_Asia",
+    "southeast-asia": "SouthEast_Asia",
+    "australia-newzealand": "Australia_NewZealand",
+}
+
+TIME_RANGE_LIST = ["24h", "48h", "7d"]
