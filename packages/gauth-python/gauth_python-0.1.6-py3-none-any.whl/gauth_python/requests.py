7f38b578d90f3eba54538d0c8aab10c4b0857265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:58 +0000

    Add package gauth-python/0.1.6/gauth_python-0.1.6-py3-none-any.whl

---
+++ packages/gauth-python/gauth_python-0.1.6-py3-none-any.whl/gauth_python/requests.py
@@ -0,0 +1,34 @@
+import requests
+
+global server_url
+global open_url
+
+server_url = "https://server.gauth.co.kr"
+open_url = "https://open.gauth.co.kr/user"
+    
+def code_issuance(email: str, password:str) -> str :
+    URL = server_url + "/oauth/code"
+    response = requests.post(URL, json={"email":email, "password" : password})
+    response_json = response.content.decode("utf-8")
+    return response_json
+
+def token_issuance(code : str, clientId: str, clientSecret: str, redirectUri: str) -> str :
+    URL = server_url + "/oauth/token"
+    response = requests.post(URL, json={"code" : code,
+                                        "clientId": clientId,
+                                        "clientSecret": clientSecret,
+                                        "redirectUri" : redirectUri})
+    response_json = response.content.decode("utf-8")
+    return response_json
+
+def token_reissuance(refreshToken : str) -> str:
+    URL = server_url + "/oauth/token"
+    response = requests.patch(URL, headers={"refreshToken" : "Bearer " + refreshToken})
+    response_json = response.content.decode("utf-8")
+    return response_json
+
+def user_info(accessToken : str) -> str:
+    URL = open_url + "/user"
+    response = requests.get(URL, headers={"Authorization": "Bearer " + accessToken})
+    response_json = response.content.decode("utf-8")
+    return response_json
