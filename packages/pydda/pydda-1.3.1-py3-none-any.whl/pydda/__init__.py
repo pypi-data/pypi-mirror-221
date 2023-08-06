627ccc4d0aeeeb8fbbdc030fc42a57e6147e7c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:00 +0000

    Add package pydda/1.3.1/pydda-1.3.1-py3-none-any.whl

---
+++ packages/pydda/pydda-1.3.1-py3-none-any.whl/pydda/__init__.py
@@ -0,0 +1,34 @@
+"""
+========================================
+PyDDA: Pythonic Direct Data Assimilation
+========================================
+"""
+
+from . import cost_functions
+from . import retrieval
+from . import vis
+from . import initialization
+from . import tests
+from . import constraints
+
+__version__ = '1.3.1'
+
+print("Welcome to PyDDA %s" % __version__)
+print("Detecting Jax...")
+try:
+    import jax
+    print("Jax engine enabled!")
+except ImportError:
+    print("Jax is not installed on your system, unable to use Jax engine.")
+
+print("Detecting TensorFlow...")
+try:
+    import tensorflow
+    print("TensorFlow detected. Checking for tensorflow-probability...")
+    import tensorflow_probability
+    print("TensorFlow-probability detected. TensorFlow engine enabled!")
+except (ImportError, AttributeError) as e:
+    print("Unable to load both TensorFlow and tensorflow-probability. " +
+            "TensorFlow engine disabled.")
+    print(e)
+
