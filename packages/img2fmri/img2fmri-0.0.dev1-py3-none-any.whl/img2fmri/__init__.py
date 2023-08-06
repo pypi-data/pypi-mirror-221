140895d00dcaef8636e004c655674b9be304389b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:39:30 +0000

    Add package img2fmri/0.0.dev1/img2fmri-0.0.dev1-py3-none-any.whl

---
+++ packages/img2fmri/img2fmri-0.0.dev1-py3-none-any.whl/img2fmri/__init__.py
@@ -0,0 +1,6 @@
+from imgtofmri.predict import (predict, 
+                              generate_activations, 
+                              generate_predictions,
+                              transform_to_MNI,
+                              smooth_brains,
+                              average_subjects)
