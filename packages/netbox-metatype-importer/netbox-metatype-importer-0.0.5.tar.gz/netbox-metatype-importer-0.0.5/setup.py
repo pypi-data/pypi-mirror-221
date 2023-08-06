c01b150ef2ff07d3fb80f77f7da508a063d5c9d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:43 +0000

    Add package netbox-metatype-importer/0.0.5/netbox-metatype-importer-0.0.5.tar.gz

---
+++ packages/netbox-metatype-importer/netbox-metatype-importer-0.0.5.tar.gz/netbox-metatype-importer-0.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='netbox-metatype-importer',
+    version='0.0.5',
+    description='Import Metatypes',
+    long_description='Import MetaTypes into NetBox',
+    long_description_content_type="text/markdown",
+    url='https://github.com/Onemind-Services-LLC/netbox-metatype-importer',
+    author='Abhimanyu Saharan',
+    author_email='asaharan@onemindservices.com',
+    maintainer='Prince Kumar',
+    maintainer_email='pkumar@onemindservices.com',
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+    ]
+)
