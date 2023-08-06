4f68c41fc59a2bad364a0bcc18b7b6c1cda043ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:38:25 +0000

    Add package pymeili/0.0.10/pymeili-0.0.10.tar.gz

---
+++ packages/pymeili/pymeili-0.0.10.tar.gz/pymeili-0.0.10/pymeili/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pymeili.beautifyplot import (
+    init, colorlist, cmaplist, getSCATTERNO, getPLOTNO, getBARNO, getBARHNO, getHISTNO,
+    savefig, title, lefttitle, righttitle, xlabel, ylabel, xticks, yticks, axvline, axhline,
+    spines, legend, grid, show, close, boxplot, plot, scatter, bar, barh, hist, pie,
+    xylim, xlim, ylim, xlog, ylog, xyticks, xscale, yscale, text, labeltext, clf, figsize,
+)
+
+__version__: str = '0.0.10'
