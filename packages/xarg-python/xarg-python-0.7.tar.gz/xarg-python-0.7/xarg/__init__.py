f39943a5d49cff99963069dbbe92d5e4030f7573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:20 +0000

    Add package xarg-python/0.7/xarg-python-0.7.tar.gz

---
+++ packages/xarg-python/xarg-python-0.7.tar.gz/xarg-python-0.7/xarg/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# coding:utf-8
+# Copyright (c) 2023 ZouMingzhe <zoumingzhe@qq.com>
+
+__version__ = "0.7"
+
+from .decorator import commands
+from .decorator import add_command
+from .decorator import run_command
+
+from .parser import argp
