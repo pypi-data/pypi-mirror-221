130d89d5159710a1b8a756f4880bc2cc6b4a8d0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:21 +0000

    Add package textual-paint/0.1.0/textual_paint-0.1.0.tar.gz

---
+++ packages/textual-paint/textual_paint-0.1.0.tar.gz/textual_paint-0.1.0/src/textual_paint/__init__.py
@@ -0,0 +1,9 @@
+"""MS Paint for the terminal, built with Textual."""
+
+__author__ = "Isaiah Odhner"
+__copyright__ = "Copyright Isaiah Odhner"
+__credits__ = ["Isaiah Odhner"]
+__maintainer__ = "Isaiah Odhner"
+__email__ = "isaiahodhner@gmail.com"
+__version__ = "0.1.0"
+__license__ = "MIT"
