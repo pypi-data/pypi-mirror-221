b5f3fef5f714854fd77175d2df9750e24d026597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:58 +0000

    Add package unitpy/0.0.9/unitpy-0.0.9-py3-none-any.whl

---
+++ packages/unitpy/unitpy-0.0.9-py3-none-any.whl/unitpy/config.py
@@ -0,0 +1,17 @@
+
+class ConfigUnit:
+    def __init__(self):
+        self.format_symbols = True
+        self.integer_format_numerator_parenthesis = False
+        self.integer_format_denominator_parenthesis = True
+        self.abbr = True  # false is full word
+        self.multiplication_seperator = ""  #  " " or " * "
+        self.division_seperator = "/"
+
+
+class Config:
+    def __init__(self):
+        self.units = ConfigUnit()
+
+
+config = Config()
