3079f0a5056b72f23256b61b2dfec3e5d6093d3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:11 +0000

    Add package great-expectations/0.17.7/great_expectations-0.17.7.tar.gz

---
+++ packages/great-expectations/great_expectations-0.17.7.tar.gz/great_expectations-0.17.7/great_expectations/agent/actions/__init__.py
@@ -0,0 +1,12 @@
+from great_expectations.agent.actions.agent_action import (
+    ActionResult,
+    AgentAction,
+    CreatedResource,
+)
+from great_expectations.agent.actions.data_assistants import (
+    RunMissingnessDataAssistantAction,
+    RunOnboardingDataAssistantAction,
+)
+from great_expectations.agent.actions.run_column_descriptive_metrics_action import (
+    ColumnDescriptiveMetricsAction,
+)
---
+++ packages/great-expectations/great_expectations-0.17.7.tar.gz/great_expectations-0.17.7/great_expectations/agent/models.py
@@ -0,0 +1,78 @@
+from typing import Literal, Sequence, Union
+
+from pydantic import BaseModel, Extra, Field
+from typing_extensions import Annotated
+
+
+class AgentBaseModel(BaseModel):
+    class Config:
+        extra: str = Extra.forbid
+
+
+class EventBase(AgentBaseModel):
+    type: str
+
+
+class RunDataAssistantEvent(EventBase):
+    type: str
+    datasource_name: str
+    data_asset_name: str
+
+
+class RunOnboardingDataAssistantEvent(RunDataAssistantEvent):
+    type: Literal[
+        "onboarding_data_assistant_request.received"
+    ] = "onboarding_data_assistant_request.received"
+
+
+class RunMissingnessDataAssistantEvent(RunDataAssistantEvent):
+    type: Literal[
+        "missingness_data_assistant_request.received"
+    ] = "missingness_data_assistant_request.received"
+
+
+class RunCheckpointEvent(EventBase):
+    type: Literal["run_checkpoint_request.received"] = "run_checkpoint_request.received"
+
+
+class RunColumnDescriptiveMetricsEvent(EventBase):
+    type: Literal[
+        "column_descriptive_metrics_request.received"
+    ] = "column_descriptive_metrics_request.received"
+    datasource_name: str
+    data_asset_name: str
+
+
+class UnknownEvent(EventBase):
+    type: Literal["unknown_event"] = "unknown_event"
+
+
+Event = Annotated[
+    Union[
+        RunOnboardingDataAssistantEvent,
+        RunMissingnessDataAssistantEvent,
+        RunCheckpointEvent,
+        RunColumnDescriptiveMetricsEvent,
+        UnknownEvent,
+    ],
+    Field(discriminator="type"),
+]
+
+
+class CreatedResource(AgentBaseModel):
+    resource_id: str
+    type: str
+
+
+class JobStarted(AgentBaseModel):
+    status: Literal["started"] = "started"
+
+
+class JobCompleted(AgentBaseModel):
+    status: Literal["completed"] = "completed"
+    success: bool
+    created_resources: Sequence[CreatedResource] = []
+    error_stack_trace: Union[str, None] = None
+
+
+JobStatus = Union[JobStarted, JobCompleted]
