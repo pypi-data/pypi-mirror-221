d90aa40ce49c90d136eddf1429dfa6a7890b02bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:22 +0000

    Add package djangoldp-i18n/3.0.0/djangoldp_i18n-3.0.0.tar.gz

---
+++ packages/djangoldp-i18n/djangoldp_i18n-3.0.0.tar.gz/djangoldp_i18n-3.0.0/djangoldp_i18n/tests/djangoldp_urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from djangoldp.permissions import LDPPermissions
+from djangoldp_i18n.tests.models import MultiLingualModel, MultiLingualChild
+from djangoldp_i18n.views import I18nLDPViewSet
+
+urlpatterns = [
+    path('multilingualmodel/', I18nLDPViewSet.urls(
+        model=MultiLingualModel, permission_classes=[LDPPermissions], nested_fields=['children'])),
+    path('multilingualchildren/', I18nLDPViewSet.urls(
+        model=MultiLingualChild, permission_classes=[LDPPermissions], nested_fields=[])),
+]
