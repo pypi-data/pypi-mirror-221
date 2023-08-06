c4511d8fcf616c2e9934ddac222c7775e8a1173d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:23:56 +0000

    Add package emmet-api/0.63.1/emmet-api-0.63.1.tar.gz

---
+++ packages/emmet-api/emmet-api-0.63.1.tar.gz/emmet-api-0.63.1/app.py
@@ -0,0 +1,19 @@
+from material_resources import resources as materials_resources
+from molecule_resources import resources as molecule_resources
+from legacy_resources import resources as legacy_resources
+
+from emmet.api.core.api import MAPI
+from emmet.api.core.documentation import description, tags_meta
+from emmet.api.core.settings import MAPISettings
+
+default_settings = MAPISettings()
+
+resources = {**materials_resources, **molecule_resources, **legacy_resources}
+
+api = MAPI(
+    resources=resources,
+    debug=default_settings.DEBUG,
+    description=description,
+    tags_meta=tags_meta,
+)
+app = api.app
