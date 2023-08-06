114328f8e50c65a1bd334b78aab545c5f1387ac7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:11 +0000

    Add package cron-lite/1.1/cron-lite-1.1.tar.gz

---
+++ packages/cron-lite/cron-lite-1.1.tar.gz/cron-lite-1.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+from distutils.core import setup
+from os import path, system
+from sys import argv
+
+if "upload" in argv:
+    print("running test")
+    assert system("python3.6 test_cron_lite.py") == 0
+
+this_directory = path.abspath(path.dirname(__file__))
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except:
+    long_description = ""
+
+setup(
+    name='cron-lite',
+    version='1.1',
+    description='A very light library to run python functions like cron jobs do.',
+    author='Rainy Chan',
+    author_email='rainydew@qq.com',
+    url='https://github.com/rainydew/cron_lite',
+    py_modules=["cron_lite"],
+    install_requires=['croniter>=1.3.4', 'pytz>=2022.7'],
+    keywords='cron task decorator schedule',
+    long_description=long_description,
+    python_requires=">=3.6"
+)
