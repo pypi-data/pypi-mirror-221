227e3fe2d2fef062e43636b94296a010cea928df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:14 +0000

    Add package alsa-midi/1.0.2/alsa_midi-1.0.2-cp310-cp310-manylinux_2_17_x86_64.whl

---
+++ packages/alsa-midi/alsa_midi-1.0.2-cp310-cp310-manylinux_2_17_x86_64.whl/alsa_midi/__init__.py
@@ -0,0 +1,49 @@
+"""Python interface to ALSA MIDI Sequencer."""
+
+from ._ffi import alsa, ffi
+from .address import ALL_SUBSCRIBERS, SYSTEM_ANNOUNCE, SYSTEM_TIMER, Address
+from .client import (AsyncSequencerClient, ClientInfo, ClientPool, ClientType, RemoveCondition,
+                     RemoveEvents, SequencerClient, SequencerType, SubscriptionQuery,
+                     SubscriptionQueryType, SystemInfo)
+from .event import (ActiveSensingEvent, BounceEvent, ChannelPressureEvent, ClientChangeEvent,
+                    ClientExitEvent, ClientStartEvent, ClockEvent, ContinueEvent,
+                    Control14BitChangeEvent, ControlChangeEvent, EchoEvent, Event, EventFlags,
+                    EventType, KeyPressureEvent, KeySignatureEvent, MidiBytesEvent,
+                    NonRegisteredParameterChangeEvent, NoteEvent, NoteOffEvent, NoteOnEvent,
+                    OSSEvent, PitchBendEvent, PortChangeEvent, PortExitEvent, PortStartEvent,
+                    PortSubscribedEvent, PortUnsubscribedEvent, ProgramChangeEvent, QueueSkewEvent,
+                    RealTime, RegisteredParameterChangeEvent, ResetEvent, ResultEvent,
+                    SetQueuePositionTickEvent, SetQueuePositionTimeEvent, SetQueueTempoEvent,
+                    SongPositionPointerEvent, SongSelectEvent, StartEvent, StopEvent,
+                    SyncPositionChangedEvent, SysExEvent, SystemEvent, TickEvent,
+                    TimeSignatureEvent, TuneRequestEvent, UserVar0Event, UserVar1Event,
+                    UserVar2Event, UserVar3Event, UserVar4Event)
+from .exceptions import ALSAError, Error, StateError
+from .port import READ_PORT, RW_PORT, WRITE_PORT, Port, PortCaps, PortInfo, PortType
+from .queue import Queue, QueueInfo, QueueStatus, QueueTempo, QueueTimer, QueueTimerType
+
+__all__ = [
+        "Address", "ALL_SUBSCRIBERS", "SYSTEM_TIMER", "SYSTEM_ANNOUNCE",
+        "SequencerClient", "AsyncSequencerClient", "ClientInfo", "ClientType", "SequencerType",
+        "SystemInfo", "SubscriptionQueryType", "SubscriptionQuery", "ClientPool", "RemoveEvents",
+        "RemoveCondition",
+        "RealTime", "EventType", "EventFlags", "Event", "MidiBytesEvent",
+        "Error", "StateError", "ALSAError",
+        "Port", "PortCaps", "PortType", "PortInfo",
+        "READ_PORT", "WRITE_PORT", "RW_PORT",
+        "Queue", "QueueInfo", "QueueStatus", "QueueTempo", "QueueTimer", "QueueTimerType",
+        "alsa", "ffi",
+
+        "SystemEvent", "ResultEvent", "NoteEvent", "NoteOnEvent", "NoteOffEvent",
+        "KeyPressureEvent", "ControlChangeEvent", "ProgramChangeEvent", "ChannelPressureEvent",
+        "PitchBendEvent", "Control14BitChangeEvent", "NonRegisteredParameterChangeEvent",
+        "RegisteredParameterChangeEvent", "SongPositionPointerEvent", "SongSelectEvent",
+        "TimeSignatureEvent", "KeySignatureEvent", "StartEvent", "ContinueEvent", "StopEvent",
+        "SetQueuePositionTickEvent", "SetQueuePositionTimeEvent", "SetQueueTempoEvent",
+        "ClockEvent", "TickEvent", "QueueSkewEvent", "SyncPositionChangedEvent",
+        "TuneRequestEvent", "ResetEvent", "ActiveSensingEvent", "EchoEvent", "OSSEvent",
+        "ClientStartEvent", "ClientExitEvent", "ClientChangeEvent", "PortStartEvent",
+        "PortExitEvent", "PortChangeEvent", "PortSubscribedEvent", "PortUnsubscribedEvent",
+        "SysExEvent", "BounceEvent", "UserVar0Event", "UserVar1Event", "UserVar2Event",
+        "UserVar3Event", "UserVar4Event"
+        ]
