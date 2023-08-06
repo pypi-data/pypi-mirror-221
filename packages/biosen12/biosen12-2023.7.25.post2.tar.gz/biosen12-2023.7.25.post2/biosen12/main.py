384e95899aa15702522058d6c79832bafc1f936f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:58 +0000

    Add package biosen12/2023.7.25.post2/biosen12-2023.7.25.post2.tar.gz

---
+++ packages/biosen12/biosen12-2023.7.25.post2.tar.gz/biosen12-2023.7.25.post2/biosen12/main.py
@@ -0,0 +1,6 @@
+__version__ = "2023.07.25-2"
+
+
+def hello():
+    print("hello")
+    return "hello"
---
+++ packages/biosen12/biosen12-2023.7.25.post2.tar.gz/biosen12-2023.7.25.post2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['biosen12']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'biosen12',
+    'version': '2023.7.25.post2',
+    'description': '',
+    'long_description': '# BioSEN12',
+    'author': 'Juan Sensio',
+    'author_email': 'it@earthpulse.es',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
