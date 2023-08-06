aa48783dcccf31e9ec17886a053e37dc49aa07ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:55 +0000

    Add package halmos/0.1.1/halmos-0.1.1.tar.gz

---
+++ packages/halmos/halmos-0.1.1.tar.gz/halmos-0.1.1/tests/test/Struct.t.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity >=0.8.0 <0.9.0;
+
+contract StructTest {
+    struct Point {
+        uint x;
+        uint y;
+    }
+
+    // TODO: support struct parameter
+    function check_Struct(Point memory) public pure {
+        assert(true);
+    }
+}
