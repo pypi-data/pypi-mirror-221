cf0b2ee8961ff4459785f58d3bf660731e3a90f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:03 +0000

    Add package strongmind-platform-sdk/3.0.1/strongmind-platform-sdk-3.0.1.tar.gz

---
+++ packages/strongmind-platform-sdk/strongmind-platform-sdk-3.0.1.tar.gz/strongmind-platform-sdk-3.0.1/platform_sdk/models/domain.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+@dataclass
+class Domain:
+    id: int
+    name: str
+    key: str
+    secret: str
+    token: str
+    partner: str
+    is_disabled: bool
---
+++ packages/strongmind-platform-sdk/strongmind-platform-sdk-3.0.1.tar.gz/strongmind-platform-sdk-3.0.1/test/factories/domain_factory.py
@@ -0,0 +1,16 @@
+from factory import Factory, Faker
+
+from platform_sdk.models.domain import Domain
+
+
+class DomainFactory(Factory):
+    class Meta:
+        model = Domain
+
+    id = Faker('random_int')
+    name = Faker('domain_name')
+    key = Faker('word')
+    secret = Faker('word')
+    token = Faker('word')
+    partner = Faker('word')
+    is_disabled = False
