6050e57f324af44e1e4b6fde9bb72674081b2d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:39 +0000

    Add package interactivenlp/0.2.5/interactivenlp-0.2.5.tar.gz

---
+++ packages/interactivenlp/interactivenlp-0.2.5.tar.gz/interactivenlp-0.2.5/interactivenlp/__init__.py
@@ -0,0 +1,10 @@
+__title__ = "packages"
+__author__ = "Chunxu Yang"
+__license__ = "MIT"
+
+from interactivenlp.models.article import *
+from interactivenlp.models.block import *
+from interactivenlp.server import *
+from interactivenlp.types.model import *
+from interactivenlp.utils.extraction import *
+from interactivenlp.models.task import *
