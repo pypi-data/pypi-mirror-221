6e5a2de29bea2f8a6d8e07dce5df233068c009f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:04 +0000

    Add package our1314-myutils/0.1.0/our1314-myutils-0.1.0.tar.gz

---
+++ packages/our1314-myutils/our1314-myutils-0.1.0.tar.gz/our1314-myutils-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='our1314-myutils',
+    version='0.1.0',
+    keywords='our1314',
+    description='a library for work',
+    license='MIT License',
+    url='https://github.com/our1314/our1314-python',
+    author='pp',
+    author_email='our1314@foxmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=[],
+)
