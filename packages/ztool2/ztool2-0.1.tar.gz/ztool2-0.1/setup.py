46c5f4c6062b12e7cbbde738ee9ac08a2d19529a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:24 +0000

    Add package ztool2/0.1/ztool2-0.1.tar.gz

---
+++ packages/ztool2/ztool2-0.1.tar.gz/ztool2-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ztool2',
+    version='0.1',
+    description='zhaotl tool',
+    author='zhaotl',
+    author_email='zhaotl@tinglong.net.cn',
+    url='',
+    packages=find_packages(),
+)
