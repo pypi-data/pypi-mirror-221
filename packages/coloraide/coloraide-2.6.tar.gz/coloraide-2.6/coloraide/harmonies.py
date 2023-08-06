3b87b7cfa41a52a5f239bca607419131f7beb04d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:37 +0000

    Add package coloraide/2.6/coloraide-2.6.tar.gz

---
+++ packages/coloraide/coloraide-2.6.tar.gz/coloraide-2.6/coloraide/harmonies.py
@@ -0,0 +1,243 @@
+"""Color harmonies."""
+from __future__ import annotations
+from abc import ABCMeta, abstractmethod
+from . import algebra as alg
+from .spaces import Cylindrical
+from .cat import WHITES
+from . import util
+from typing import TYPE_CHECKING, Any  # noqa: F401
+
+if TYPE_CHECKING:  # pragma: no cover
+    from .color import Color
+
+WHITE = util.xy_to_xyz(WHITES['2deg']['D65'])
+BLACK = [0, 0, 0]
+
+
+def adjust_hue(hue: float, deg: float) -> float:
+    """Adjust hue by the given degree."""
+
+    return hue + deg
+
+
+class Harmony(metaclass=ABCMeta):
+    """Color harmony."""
+
+    @abstractmethod
+    def harmonize(self, color: Color, space: str) -> list[Color]:
+        """Get color harmonies."""
+
+
+class Monochromatic(Harmony):
+    """
+    Monochromatic harmony.
+
+    Take a given color and create both tints and shades such that we have `RANGE` total steps ranging
+    from black -> color -> white. Normally, we will throw away pure black, pure white, and the duplicate
+    target color (as we interpolate with it on both sides) leaving us with RANGE - 3 colors to extract
+    the target `STEPS` from. The one exception is when targeting an achromatic color, and in that case,
+    we only throw away the duplicate color (though if a color is close enough to white or black, white
+    or black may not be included simply because we cannot get a reasonable step that includes it).
+
+    Once we have our `RANGE`, we can extract a total of `STEPS` colors with the target color at the center
+    (when possible). If the target color is too close to the either the minimum or maximum color step,
+    there may not be enough tints or shades on one side, so the result may have to draw heavier on the
+    side that has more plentiful tints or shades which would cause the target color to shift from the center.
+
+    The current `RANGE` was chosen as 12 as it seems to to provide OK contrast in most cases for the monochromatic
+    colors. The one exception is with a target color of black or very near black which may return at least one color
+    with very low contrast to black. Generally, extremely dark colors do not make a good target for color harmonies,
+    but it should be noted that OkLCh's lightness tends to the more darker side. The poor contrast may be
+    less with other color spaces.
+    """
+
+    DELTA_E = '2000'
+    RANGE = 12
+    STEPS = 5
+
+    def harmonize(self, color: Color, space: str) -> list[Color]:
+        """Get color harmonies."""
+
+        color1 = color.convert(space, norm=False).normalize()
+
+        if not isinstance(color1._space, Cylindrical):
+            raise ValueError('Color space must be cylindrical')
+
+        # Trim off black and white unless the color is achromatic,
+        # But always trim duplicate target color from left side.
+        if not color1.is_nan('hue'):
+            ltrim, rtrim = slice(1, -1, None), slice(None, -1, None)
+        else:
+            ltrim, rtrim = slice(None, -1, None), slice(None, None, None)
+
+        # Create black and white so we can generate tints and shades
+        # Ensure hue and alpha is masked so we don't interpolate them.
+        w = color.new('xyz-d65', WHITE, alg.NaN)
+        w.convert(space, in_place=True, norm=False).mask(['hue', 'alpha'], in_place=True)
+        b = color.new('xyz-d65', BLACK, alg.NaN)
+        b.convert(space, in_place=True, norm=False).mask(['hue', 'alpha'], in_place=True)
+
+        # Calculate how many tints and shades we need to generate
+        db = b.delta_e(color1, method=self.DELTA_E)
+        dw = w.delta_e(color1, method=self.DELTA_E)
+        steps_w = int(alg.round_half_up((dw / (db + dw)) * self.RANGE))
+        steps_b = self.RANGE - steps_w
+
+        kwargs = {
+            'space': space,
+            'method': 'linear',
+            'out_space': space
+        }  # type: dict[str, Any]
+
+        # Very close to black or is black, no need to interpolate from black to current color
+        if steps_b <= 1:
+            left = []
+            if steps_b == 1:
+                left.extend(color1.steps([b, color1], steps=steps_b, **kwargs))
+            steps = min(self.RANGE - (1 + steps_b), steps_w)
+            right = color1.steps([color1, w], steps=steps, **kwargs)[rtrim]
+
+        # Very close to white or is white, no need to interpolate from current color to white
+        elif steps_w <= 1:
+            right = []
+            if steps_w == 1:
+                right.extend(color1.steps([color1, w], steps=steps_w, **kwargs))
+            steps = min(self.RANGE - (1 + steps_w), steps_b)
+            right.insert(0, color1.clone())
+            left = color1.steps([b, color1], steps=steps, **kwargs)[ltrim]
+
+        # Anything else in between
+        else:
+            left = color1.steps([b, color1], steps=steps_b, **kwargs)[ltrim]
+            right = color1.steps([color1, w], steps=steps_w, **kwargs)[rtrim]
+
+        # Extract a subset of the results
+        len_l = len(left)
+        len_r = len(right)
+        l = int(self.STEPS // 2)
+        r = l + (1 if self.STEPS % 2 else 0)
+        if len_r < r:
+            return left[-self.STEPS + len_r:] + right
+        elif len_l < l:
+            return left + right[:self.STEPS - len_l]
+        return left[-l:] + right[:r]
+
+
+class Geometric(Harmony):
+    """Geometrically space the colors."""
+
+    COUNT = 0
+
+    def harmonize(self, color: Color, space: str) -> list[Color]:
+        """Get color harmonies."""
+
+        color1 = color.convert(space, norm=False).normalize()
+
+        if not isinstance(color1._space, Cylindrical):
+            raise ValueError('Color space must be cylindrical')
+
+        name = color1._space.hue_name()
+
+        degree = current = 360.0 / self.COUNT
+        colors = []
+        for _ in range(self.COUNT - 1):
+            colors.append(
+                color1.clone().set(name, lambda x, value=current: adjust_hue(x, value))
+            )
+            current += degree
+        colors.insert(0, color1)
+        return colors
+
+
+class Complementary(Geometric):
+    """Complementary colors."""
+
+    COUNT = 2
+
+
+class Triadic(Geometric):
+    """Triadic colors."""
+
+    COUNT = 3
+
+
+class TetradicSquare(Geometric):
+    """Tetradic (square)."""
+
+    COUNT = 4
+
+
+class SplitComplementary(Harmony):
+    """Split Complementary colors."""
+
+    def harmonize(self, color: Color, space: str) -> list[Color]:
+        """Get color harmonies."""
+
+        color1 = color.convert(space, norm=False).normalize()
+
+        if not isinstance(color1._space, Cylindrical):
+            raise ValueError('Color space must be cylindrical')
+
+        name = color1._space.hue_name()
+
+        color2 = color1.clone().set(name, lambda x: adjust_hue(x, 210))
+        color3 = color1.clone().set(name, lambda x: adjust_hue(x, -210))
+        return [color1, color2, color3]
+
+
+class Analogous(Harmony):
+    """Analogous colors."""
+
+    def harmonize(self, color: Color, space: str) -> list[Color]:
+        """Get color harmonies."""
+
+        color1 = color.convert(space, norm=False).normalize()
+
+        if not isinstance(color1._space, Cylindrical):
+            raise ValueError('Color space must be cylindrical')
+
+        name = color1._space.hue_name()
+
+        color2 = color1.clone().set(name, lambda x: adjust_hue(x, 30))
+        color3 = color1.clone().set(name, lambda x: adjust_hue(x, -30))
+        return [color1, color2, color3]
+
+
+class TetradicRect(Harmony):
+    """Tetradic (rectangular) colors."""
+
+    def harmonize(self, color: Color, space: str) -> list[Color]:
+        """Get color harmonies."""
+
+        color1 = color.convert(space, norm=False).normalize()
+
+        if not isinstance(color1._space, Cylindrical):
+            raise ValueError('Color space must be cylindrical')
+
+        name = color1._space.hue_name()
+
+        color2 = color1.clone().set(name, lambda x: adjust_hue(x, 30))
+        color3 = color1.clone().set(name, lambda x: adjust_hue(x, 180))
+        color4 = color1.clone().set(name, lambda x: adjust_hue(x, 210))
+        return [color1, color2, color3, color4]
+
+
+SUPPORTED = {
+    'complement': Complementary(),
+    'split': SplitComplementary(),
+    'triad': Triadic(),
+    'square': TetradicSquare(),
+    'rectangle': TetradicRect(),
+    'analogous': Analogous(),
+    'mono': Monochromatic()
+}  # type: dict[str, Harmony]
+
+
+def harmonize(color: Color, name: str, space: str) -> list[Color]:
+    """Get specified color harmonies."""
+
+    h = SUPPORTED.get(name)
+    if not h:
+        raise ValueError("The color harmony '{}' cannot be found".format(name))
+
+    return h.harmonize(color, space)
---
+++ packages/coloraide/coloraide-2.6.tar.gz/coloraide-2.6/docs/src/markdown/harmonies.md
@@ -0,0 +1,202 @@
+# Color Harmonies
+
+In color theory, color harmony refers to the property that certain aesthetically pleasing color combinations have.
+Modern day color theory probably starts with the first color wheel created by Isaac Newton. Based on his observations of
+light with prisms, he formed probably the first color wheel. From there, many others built upon this work, sometimes
+with opposing ideas.
+
+The original color wheel, while inspired by what was observed by light, was created based on experiments with pigments
+as well. As most know, in paint, red, yellow, and blue are considered primary colors. Newton thought this translated to
+light as well and stated they were also the primary colors of light. While this isn't actually true, his work was very
+important in reshaping how people viewed color.
+
+Over time, the color wheel was refined. The traditional model, which we will call an RYB color model, defined 12 colors
+that made up the wheel: the primary colors, the secondary colors, and the tertiary colors. The secondary colors are
+created by evenly mixing the primary colors, and the tertiary colors are created by evenly mixing those primary colors
+with the secondary colors.
+
+![RYB Color Wheel](images/color-wheel.png)
+
+The idea of color harmonies originates from the idea that colors, based on their relative position on the wheel, can
+form more pleasing color combinations.
+
+## Which Color Space is Best for Color Harmonies?
+
+As we know, these days, there are many color spaces out there: subtractive models, additive models, perceptually
+uniform models, high dynamic range models, etc. Many color spaces trying to solve specific issues based on the knowledge
+at the time.
+
+The early work that created the first color wheel was based on an RYB color model. In modern TVs and monitors, the RYB
+color model is not used. Electronic screens create all their colors with light based methods that mix red, green, and
+blue lights. In addition, the human eye perceives colors using red, green, and blue as well.
+
+In reality, we could create a color wheel from any of the various color spaces out there and end up with slightly
+different results. If we were to compose a color wheel based on the common sRGB color space, we could base it off the 3
+primary colors of light. Starting with red (0˚), we could extract the colors at evenly spaced degrees, 30˚ to be exact.
+This would give us our 12 colors for the sRGB color space.
+
+```py play
+Steps([Color('hsl', [x, 1, 0.5]) for x in range(0, 360, 30)])
+```
+
+From this we can construct an sRGB color wheel.
+
+![RGB Color Wheel](images/rgb-color-wheel.png)
+
+This is different from the RYB color wheel, and more accurate in relation to how light works, but does it yield better
+harmonies for colors?
+
+If we were to select the perceptually uniform OkLCh color space, and seed it with red's lightness and chroma, we'd get:
+
+```py play
+c = Color('red').convert('oklch', in_place=True)
+Steps([Color('oklch', [*c[0:2], x]) for x in range(0, 360, 30)])
+```
+
+This produces colors with visually more uniform lightness, does that mean these are better?
+
+The truth is, what is better or even harmonious can be largely subjective, and everyone has reasons for selecting
+certain color spaces for a specific task.
+
+Many artists swear by the classical color wheel, others are fine with using the sRGB color wheel as it is easy to work
+with in CSS via the HSL color space, and there are still others that are more interested in perceptually uniform color
+spaces that aim for more consistent hues and predictable lightness.
+
+As far as ColorAide is concerned, we've chosen to use OkLCh as the color space in which we work in. This is based
+mainly on the fact it keeps hue more consistent than some other options, and it allows us to support a wider gamut than
+options like HSL.
+
+```py play
+Steps(Color.steps(['black', 'blue', 'white'], steps=11, space='oklch'))
+Steps(Color.steps(['black', 'blue', 'white'], steps=11, space='hsl'))
+Steps(Color.steps(['black', 'blue', 'white'], steps=11, space='lch'))
+```
+
+While OkLCh is the default, we understand that there are many reasons to use other spaces, so use what you like, we
+won't judge :smile:.
+
+```py play
+Steps(Color('red').harmony('complement'))
+Steps(Color('red').harmony('complement', space='hsl'))
+```
+
+/// tip
+`harmony()` can output the results in any color space you need by setting `out_space`.
+
+```py play
+Steps(Color('red').harmony('complement', out_space='srgb'))
+```
+///
+
+
+## Supported Harmonies
+
+ColorAide currently supports 7 theorized color harmonies: [monochromatic](#monochromatic),
+[complementary](#complementary), [split complementary](#split-complementary), [analogous](#analogous),
+[triadic](#triadic), [square](#tetradic-square), and [rectangular](#tetradic-rectangular). By default, all color
+harmonies are calculated with the perceptually uniform OkLCh color space, but other color spaces can be used if desired.
+
+While we use OkLCh, we will actually visualize the examples in Okhsl. Okhsl is a derivative of OkLCh with the lightness
+adjusted to match CIELCh and reshaped in a cylindrical form. It is limited to the sRGB gamut only, but it can help
+visualize better what is happening in a familiar color wheel format. You can see the difference below:
+
+/// tab | OkLCh Color Slice
+![OkLCh Color Wheel](images/oklch-color-wheel.png)
+///
+
+/// tab | Okhsl Color Slice
+![Okhsl Color Wheel](images/okhsl-color-wheel.png)
+///
+
+### Monochromatic
+
+The monochromatic harmony pairs various tints and shades of a color together to create pleasing color schemes.
+
+![Harmony Monochromatic](images/harmony-mono.png)
+
+```py play
+Steps(Color('red').harmony('mono'))
+```
+
+/// note | Achromatic Colors
+Pure `#!color white` and `#!color black` will not be included in a monochromatic color harmony unless the color is
+achromatic.
+///
+
+### Complementary
+
+Complementary harmonies use a dyad of colors at opposite ends of the color wheel.
+
+![Harmony Complementary](images/harmony-complement.png)
+
+```py play
+Steps(Color('red').harmony('complement'))
+```
+
+### Split Complementary
+
+Split Complementary is similar to complementary, but actually uses a triad of colors. Instead of just choosing one
+complement, it splits and chooses two colors on the opposite side that are close, but not adjacent.
+
+![Harmony Split Complementary](images/harmony-split-complement.png)
+
+```py play
+Steps(Color('red').harmony('split'))
+```
+
+### Analogous
+
+Analogous harmonies consists of 3 adjacent colors.
+
+![Harmony Analogous](images/harmony-analogous.png)
+
+```py play
+Steps(Color('red').harmony('analogous'))
+```
+
+### Triadic
+
+Triadic draws an equilateral triangle between 3 colors on the color wheel. For instance, the primary colors have triadic
+harmony.
+
+![Harmony Triadic](images/harmony-triadic.png)
+
+```py play
+Steps(Color('red').harmony('triad'))
+```
+
+### Tetradic Square
+
+Tetradic color harmonies refer to a group of four colors. One tetradic color harmony can be found by drawing a square
+between four colors on the color wheel.
+
+![Harmony Tetradic](images/harmony-tetradic.png)
+
+```py play
+Steps(Color('red').harmony('square'))
+```
+
+### Tetradic Rectangular
+
+The rectangular tetradic harmony is very similar to the square tetradic harmony except that it draws a rectangle between
+four colors instead of a square.
+
+![Harmony Tetradic Rectangular](images/harmony-tetradic-rect.png)
+
+```py play
+Steps(Color('red').harmony('rectangle'))
+```
+
+## Changing the Default Harmony Color Space
+
+If you'd like to change the `#!py3 Color()` class's default harmony color space, it can be done with
+[class override](./color.md#override-default-settings). Simply derive a new `#!py3 Color()` class from the original and
+override the `HARMONY` property with the name of a suitable cylindrical color space. Afterwards, all color color
+harmony calculations will use the specified color space unless overridden via the method's `space` parameter.
+
+```py play
+class Custom(Color):
+    HARMONY = 'hsl'
+
+Steps(Custom('red').harmony('split'))
+```
