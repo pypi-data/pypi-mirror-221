8c9dabcdc248c11fa3317559fda5d8e21458a82a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:32:25 +0000

    Add package mysqlx-generator/1.6.6/mysqlx-generator-1.6.6.tar.gz

---
+++ packages/mysqlx-generator/mysqlx-generator-1.6.6.tar.gz/mysqlx-generator-1.6.6/mysqlx/__init__.py
@@ -0,0 +1,20 @@
+from sqlbatis import (
+    connection,
+    transaction,
+    with_connection,
+    with_transaction,
+    get_connection,
+    sql,
+    mapper,
+    init_snowflake,
+    get_snowflake_id
+)
+from .engin import MySqlEngin
+
+def init_db(host='127.0.0.1', port=3306, database='test', user='root', password='', mapper_path='mapper', pool_size=0, show_sql=False, **kwargs):
+    from sqlbatis import init_db as supper_init_db
+
+    MySqlEngin.init()
+    supper_init_db(host=host, port=port, user=user, database=database, password=password, mapper_path=mapper_path, pool_size=pool_size, \
+                   show_sql=show_sql, **kwargs)
+
