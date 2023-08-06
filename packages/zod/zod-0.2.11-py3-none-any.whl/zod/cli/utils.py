32b2e5e9c3f945f7dc67ef35b98819f3a4eb56d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:17 +0000

    Add package zod/0.2.11/zod-0.2.11-py3-none-any.whl

---
+++ packages/zod/zod-0.2.11-py3-none-any.whl/zod/cli/utils.py
@@ -0,0 +1,28 @@
+import enum
+from typing import Type
+
+from zod import ZodDataset, ZodDrives, ZodFrames, ZodSequences
+
+
+class SubDataset(enum.Enum):
+    FRAMES = "frames"
+    SEQUENCES = "sequences"
+    DRIVES = "drives"
+
+    @property
+    def folder(self) -> str:
+        if self == SubDataset.FRAMES:
+            return "single_frames"
+        else:
+            return self.value
+
+    @property
+    def dataset_cls(self) -> Type[ZodDataset]:
+        return _cls_map[self]
+
+
+_cls_map = {
+    SubDataset.FRAMES: ZodFrames,
+    SubDataset.SEQUENCES: ZodSequences,
+    SubDataset.DRIVES: ZodDrives,
+}
