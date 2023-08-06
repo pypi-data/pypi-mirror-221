6ab5299a352295d5018e229b03425485be52c7c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:03 +0000

    Add package jinjanator/23.4.0/jinjanator-23.4.0.tar.gz

---
+++ packages/jinjanator/jinjanator-23.4.0.tar.gz/jinjanator-23.4.0/src/jinjanator/context.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any, Mapping, TextIO
+
+
+if TYPE_CHECKING:  # pragma: no cover
+    from jinjanator_plugins import Format
+
+
+def read_context_data(
+    fmt: Format,
+    f: TextIO | None,
+    environ: Mapping[str, str],
+    import_env: str | None = None,
+    format_options: list[str] | None = None,
+) -> Mapping[str, Any]:
+    if not f:
+        msg = "no input supplied"
+        raise ValueError(msg)
+
+    context: dict[str, Any] = {}
+
+    context.update(fmt.parser(f.read(), format_options))
+
+    if import_env is not None:
+        if import_env == "":
+            context.update(environ)
+        else:
+            context[import_env] = environ
+
+    # Done
+    return context
---
+++ packages/jinjanator/jinjanator-23.4.0.tar.gz/jinjanator-23.4.0/tests/test_argparse.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+from typing import Any, Mapping
+
+import pytest
+
+from jinjanator_plugins import Format
+
+from jinjanator.cli import parse_args
+
+
+def fake_env_parser(
+    data: str, options: list[str] | None = None  # noqa: ARG001
+) -> Mapping[str, Any]:
+    return {"foo": "bar"}
+
+
+fake_env_format = Format(parser=fake_env_parser, suffixes=["env"])
+
+
+def test_invalid_arg() -> None:
+    """Ensure that an invalid argument is not accepted."""
+    with pytest.raises(SystemExit):
+        parse_args({}, ["--test-invalid-arg"])
+
+
+@pytest.mark.parametrize(
+    "args",
+    [
+        ["--format", "env"],
+        ["--format-option", "opt"],
+        ["--import-env", "env"],
+        ["--output-file", "output"],
+        ["--quiet"],
+        ["--undefined"],
+        ["-e", "env"],
+        ["-f", "env"],
+        ["-o", "output"],
+    ],
+)
+def test_args(args: list[str]) -> None:
+    """Ensure that known arguments are accepted."""
+    parse_args({"env": fake_env_format}, [*args, "template"])
+
+
+def test_version() -> None:
+    """Ensure that '--version' argument is accepted and program exits without an error."""
+    with pytest.raises(SystemExit) as excinfo:
+        parse_args({}, ["--version"])
+    assert 0 == excinfo.value.code
+
+
+@pytest.mark.xfail()
+@pytest.mark.parametrize(
+    "args",
+    [
+        ["--format", "env", "-f", "env"],
+        ["--import-env", "env", "-e", "env"],
+    ],
+)
+def test_duplicate_args(args: list[str]) -> None:
+    """Ensure that duplicate arguments are not accepted."""
+    parse_args({"env": fake_env_format}, [*args, "template"])
