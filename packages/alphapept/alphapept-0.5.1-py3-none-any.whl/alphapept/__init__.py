5a0351aeb6a9aed1eca40a1b85babb93d400f07f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:29 +0000

    Add package alphapept/0.5.1/alphapept-0.5.1-py3-none-any.whl

---
+++ packages/alphapept/alphapept-0.5.1-py3-none-any.whl/alphapept/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.5.1"
+
+__requirements__ = {
+    "": "requirements/requirements.txt",
+    "develop": "requirements/requirements_develop.txt",
+    "gui": "requirements/requirements_gui.txt",
+    "legacy": "requirements/requirements_legacy.txt",
+}
