50e083a378d11f5a7a12e4d6e0e14b61d62c8823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:40 +0000

    Add package pysocklib/0.3.1/pysocklib-0.3.1-py3-none-any.whl

---
+++ packages/pysocklib/pysocklib-0.3.1-py3-none-any.whl/socketlib/services/abstract_service.py
@@ -0,0 +1,73 @@
+import abc
+import logging
+import queue
+import threading
+from typing import Any, Callable, Optional
+
+
+class AbstractService(abc.ABC):
+    """ Abstract base class for all services.
+
+        To add a new service implement the _handle_message method.
+
+        A service consists of an input queue and an output queue. The purpose
+        of a service is to apply some function to the inputs to obtain
+        the outputs that can then be processed by another service or send to a receptor.
+
+        Most services are meant to run indefinitely, and so they do not run in the
+        main thread. However, a custom function to terminate the service when needed
+        can be used, and the service can also run in the main thread if necessary.
+    """
+    def __init__(
+            self,
+            in_queue: Optional[queue.Queue] = None,
+            out_queue: Optional[queue.Queue] = None,
+            stop: Callable[[], bool] = lambda: False,
+            events: Optional[dict[str, threading.Event]] = None,
+            logger: Optional[logging.Logger] = None,
+    ):
+        # Main thread of this service
+        self._in = in_queue if in_queue is not None else queue.Queue()
+        self._out = out_queue if out_queue is not None else queue.Queue()
+        self._events = events if events is not None else dict()
+        self._logger = logger
+
+        self._process_thread = threading.Thread(
+            target=self._handle_message,
+            daemon=True
+        )
+        self._stop = stop
+
+    @property
+    def process_thread(self) -> threading.Thread:
+        return self._process_thread
+
+    @property
+    def in_queue(self) -> queue.Queue:
+        return self._in
+
+    @property
+    def out_queue(self) -> queue.Queue:
+        return self._out
+
+    @abc.abstractmethod
+    def _handle_message(self):
+        raise NotImplementedError
+
+    def start_main_thread(self):
+        """ Starts this service in the main thread. """
+        self._handle_message()
+
+    def start(self) -> None:
+        self._process_thread.start()
+        if self._logger is not None:
+            self._logger.debug(f"Started {self.__class__.__name__}")
+
+    def join(self) -> None:
+        self._process_thread.join()
+
+    def shutdown(self) -> None:
+        self._stop = lambda: True
+        self.join()
+        if self._logger is not None:
+            self._logger.debug(f"Shutting down {self.__class__.__name__}")
