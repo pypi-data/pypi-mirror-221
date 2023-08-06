c8c7ef905775d2e51dc0f1da6c6ff203285d8acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:40 +0000

    Add package SmartDriveML/0.0.12/SmartDriveML-0.0.12-py3-none-any.whl

---
+++ packages/SmartDriveML/SmartDriveML-0.0.12-py3-none-any.whl/SmartDriveML/priceAnalysis.py
@@ -0,0 +1,7 @@
+def fetchCost():
+    print("Cost from Clous is fetched successfully")
+    return "fetchCost Executed Successfully"
+
+def fetchFramework():
+    print("Selected Framework successfully")
+    return "fetchFramework Executed Successfully"
