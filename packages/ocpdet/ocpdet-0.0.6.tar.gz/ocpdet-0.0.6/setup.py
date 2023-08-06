d3034f97e7304db6685a831a9dc22e8f3ba460cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:14 +0000

    Add package ocpdet/0.0.6/ocpdet-0.0.6.tar.gz

---
+++ packages/ocpdet/ocpdet-0.0.6.tar.gz/ocpdet-0.0.6/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ocpdet",
+    version="0.0.6",
+    author="Victor Khamesi",
+    author_email="victor.khamesi21@imperial.ac.uk",
+    description="A Python package for online changepoint detection, implementing state-of-the-art algorithms and a novel approach.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vkhamesi/ocpdet",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "numpy",
+        "tqdm",
+        "tensorflow",
+        "scipy"
+    ],
+    python_requires='>=3.6',
+)
