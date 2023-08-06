92302d4a81d356b05808d1a8c8eeba0414fc6053
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:37:58 +0000

    Add package unitpy/0.0.9/unitpy-0.0.9.tar.gz

---
+++ packages/unitpy/unitpy-0.0.9.tar.gz/unitpy-0.0.9/src/unitpy/utils/equation_formating.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+from unitpy.config import config
+
+
+def equation_formater(dict_: dict[str, int | float]) -> str:
+
+    if config.units.format_symbols:
+        string = format_with_symbols(dict_)
+    else:
+        string = format_with_power(dict_)
+
+    return string
+
+
+def format_with_power(dict_: dict[str, int | float]) -> str:
+    return config.units.multiplication_seperator.join([f"{k}**{v}" for k, v in dict_.items() if v != 0])
+
+
+def format_with_symbols(dict_: dict) -> str:
+    numerator = []
+    denominator = []
+    for k, v in dict_.items():
+        if v >= 0:
+            numerator.append((k, v))
+        else:
+            denominator.append((k, v))
+
+    if numerator:
+        string = format_integer_numerator(numerator)
+    else:
+        return format_with_power(dict_)
+
+    if denominator:
+        string += config.units.division_seperator + format_integer_denominator(denominator)
+
+    return string
+
+
+def format_integer_numerator(numerator) -> str:
+    string = config.units.multiplication_seperator.join(format_term(k, power) for k, power in numerator if power != 0)
+
+    if config.units.integer_format_numerator_parenthesis and len(numerator) > 1:
+        string = "(" + string + ")"
+
+    return string
+
+
+def format_integer_denominator(denominator) -> str:
+    string = config.units.multiplication_seperator.join(format_term(k, -1 * power) for k, power in denominator if
+                                                        power != 0)
+
+    if config.units.integer_format_denominator_parenthesis and len(denominator) > 1:
+        string = "(" + string + ")"
+
+    return string
+
+
+def format_term(label: str, power: int | float) -> str:
+    if power == 0:
+        return ""
+
+    if abs(power) == 1:
+        return label
+
+    return f"{label}**{power}"
