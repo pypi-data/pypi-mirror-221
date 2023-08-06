cde4bf9635766f56fa166eca2eb218f664705527
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:06 +0000

    Add package numerous-api-grpc/0.9.0/numerous_api_grpc-0.9.0.tar.gz

---
+++ packages/numerous-api-grpc/numerous_api_grpc-0.9.0.tar.gz/numerous_api_grpc-0.9.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='numerous_api_grpc',
+    version='0.9.0',
+    description='Numerous Data Port gRPC generated Python module',
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+    package_dir={'': 'generated/python'},
+    packages=[''],
+)
