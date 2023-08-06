2ba80e2c3d2ef940860645112602c5b77cd2cd60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:14 +0000

    Add package playcode/0.0.5/playcode-0.0.5-py3-none-any.whl

---
+++ packages/playcode/playcode-0.0.5-py3-none-any.whl/playcode/password_make.py
@@ -0,0 +1,11 @@
+import random
+
+
+def password_make(length):
+   print("Hello")
+
+
+
+
+if __name__ == "__main__":
+    password_make(10)
---
+++ packages/playcode/playcode-0.0.5-py3-none-any.whl/playcode/test.py
@@ -0,0 +1,4 @@
+from playcode import password_make
+
+pw = password_make.password_make(15)
+print(pw)
