bd5ae8a1c377fc21d69fd79f0463cec4a37b5e37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:25 +0000

    Add package zood/0.7.19/zood-0.7.19.tar.gz

---
+++ packages/zood/zood-0.7.19.tar.gz/zood-0.7.19/zood/config/js/global_js_configuration.js
@@ -0,0 +1,43 @@
+
+// 美化选择框
+// - [ ] xxx
+// - [x] aaa
+var inputs = document.getElementsByTagName('input')
+for(var i=0;i<inputs.length;i++) {
+    inputs[i].removeAttribute('disabled')
+    inputs[i].onclick = function() {
+        return false;
+    }
+}
+
+var markdown_part = document.querySelector(".markdown-body");
+
+
+var currentUrl = window.location.href.slice(0, -1);
+var dirTree = document.querySelector(".dir-tree");
+var links = dirTree.querySelectorAll("a");
+// 主题保持
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme !== null) {
+    if (savedTheme === 'light') {
+        markdown_part.className = 'markdown-body markdown-light'
+    } else {
+        markdown_part.className = 'markdown-body markdown-dark'
+    }
+}
+// 如果保存的主题存在,则设置当前主题为保存的主题
+links.forEach(function(link) {
+  if (link.href === currentUrl) {
+    link.scrollIntoView({block: 'center', inline:'nearest', container: dirTree });
+    if (savedTheme) {
+        if (savedTheme == 'dark') {
+            link.classList.add("link-active-dark");
+        } else {
+            link.classList.add("link-active");
+        }
+    } else {
+        link.classList.add("link-active");
+    }
+  }
+});
+
