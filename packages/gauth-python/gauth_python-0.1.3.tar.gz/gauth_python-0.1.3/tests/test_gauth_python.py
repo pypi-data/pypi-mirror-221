3cf14d27e965d6d41a786ad95b0c800eb3244679
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:14 +0000

    Add package gauth-python/0.1.3/gauth_python-0.1.3.tar.gz

---
+++ packages/gauth-python/gauth_python-0.1.3.tar.gz/gauth_python-0.1.3/tests/test_gauth_python.py
@@ -0,0 +1,9 @@
+from gauth_python import requests
+import gauth_python
+
+def test_function():
+    assert requests.code_issuance("exampleString", "exampleString")
+    assert requests.token_issuance("exampleString","exampleString","exampleString","exampleString")
+    assert requests.token_reissuance()
+    assert requests.user_info()
+
