20cbe128a93b29c0890063c06a179a040061c303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:19 +0000

    Add package Widget-Controller/2.0/Widget_Controller-2.0.tar.gz

---
+++ packages/Widget-Controller/Widget_Controller-2.0.tar.gz/Widget Controller-2.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+readme = open("./README.md", "r")
+
+setup(
+    name="Widget Controller",
+    packages=["WC"],
+    version="2.0",
+    description="Una libreria para crear facilmente UI (traducido de Tkinter y otros)",
+    long_description=readme.read(),
+    long_description_content_type="text/markdown",
+    author="Z3R0_GT",
+    author_email="contac.es.z3r0.gt@gmail.com",
+    url="https://github.com/Z3R0GT/WC",
+    download_url="https://github.com/Z3R0GT/WC/releases/tag/BUILD-2.0",
+    keywords=["ui", "UI", "Tkinter", "logging", "build"],
+    classifiers=[ ],
+    license="OTHER",
+    include_package_data=True
+)
