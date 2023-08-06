491d444d40c161c8a09cf4d786f18b5c2459fc4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:02 +0000

    Add package ftmq/0.1.5/ftmq-0.1.5.tar.gz

---
+++ packages/ftmq/ftmq-0.1.5.tar.gz/ftmq-0.1.5/ftmq/types.py
@@ -0,0 +1,20 @@
+from typing import Any, Generator, Literal, TypeAlias
+
+from nomenklatura.entity import CE
+
+from .enums import Properties, Schemata
+
+# a string-keyed dict
+SDict: TypeAlias = dict[str, Any]
+
+# property multi-value
+Value: TypeAlias = list[str]
+
+# composite entity generator
+CEGenerator: TypeAlias = Generator[CE, None, None]
+
+StrGenerator: TypeAlias = Generator[str, None, None]
+BytesGenerator: TypeAlias = Generator[bytes, None, None]
+
+Schemata: TypeAlias = Literal[*[s.name for s in Schemata]]
+Properties: TypeAlias = Literal[*[p.name for p in Properties]]
